975e6bde70b944044b2bb41bc3a5fc280e7c441d
Jerry Bruwes <jbruwes@icloud.com>
2021-12-08 16:09:10 +0300

    Update data.js

--- cms/sources/views/templateViews/data.js
+++ cms/sources/views/templateViews/data.js
@@ -1,20 +1,18 @@
 import { JetView } from "webix-jet";
 
 export default class DataView extends JetView {
-  config() {
-    return {
-      id: "data",
-      view: "datatable",
-      select: "row",
-      columns: [
-        { id: "data", editor: "text", header: "data-", fillspace: true },
-        { id: "value", editor: "text", header: "value", fillspace: true },
-      ],
-      editable: true,
-      on: {
-        "data->onStoreUpdated": () =>
-          this.getParentView().redraw(this.getParentView()),
-      },
-    };
-  }
+  config = () => ({
+    id: "data",
+    view: "datatable",
+    select: "row",
+    columns: [
+      { id: "data", editor: "text", header: "data-", fillspace: true },
+      { id: "value", editor: "text", header: "value", fillspace: true },
+    ],
+    editable: true,
+    on: {
+      "data->onStoreUpdated": () =>
+        this.getParentView().redraw(this.getParentView()),
+    },
+  });
 }
