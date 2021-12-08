3198d4ad04d0b27278ef7e540a88c6378eee503b
Jerry Bruwes <jbruwes@icloud.com>
2021-12-08 16:09:37 +0300

    Update top.js

--- cms/sources/views/top.js
+++ cms/sources/views/top.js
@@ -5,92 +5,84 @@ import * as webix from "webix";
  * Ксласс описания формы заголовка начальной страницы
  */
 export default class TopView extends JetView {
-  /**
-   * This method returns the initial UI configuration of a view
-   *
-   * @returns {object} The initial UI configuration of a view
-   */
-  config() {
-    const ui = {
-      rows: [
-        {
-          id: "toolbar",
-          view: "toolbar",
-          height: 56,
-          // css: "webix_dark",
-          cols: [
-            {
-              view: "icon",
-              icon: "mdi mdi-menu",
-              click: () => {
-                $$("sidebar").toggle();
+  config = () => ({
+    rows: [
+      {
+        id: "toolbar",
+        view: "toolbar",
+        height: 56,
+        // css: "webix_dark",
+        cols: [
+          {
+            view: "icon",
+            icon: "mdi mdi-menu",
+            click: () => {
+              $$("sidebar").toggle();
+            },
+          },
+          {
+            view: "label",
+            label: '<i class="rocket icon"></i> KosmoS3 CMS',
+          },
+        ],
+      },
+      {
+        cols: [
+          {
+            view: "sidebar",
+            collapsed: true,
+            id: "sidebar",
+            // css: "webix_dark",
+            data: [],
+            click: (id) => {
+              if (id === "signout") {
+                webix.delay(() => {
+                  this.app.authenticationData = null;
+                  this.show("signin");
+                  this.resetSidebar();
+                });
+              } else this.show(id);
+            },
+          },
+          {
+            type: "wide",
+            padding: 2,
+            css: "app_layout",
+            rows: [
+              {
+                $subview: true,
               },
-            },
-            {
-              view: "label",
-              label: '<i class="rocket icon"></i> KosmoS3 CMS',
-            },
-          ],
-        },
-        {
-          cols: [
-            {
-              view: "sidebar",
-              collapsed: true,
-              id: "sidebar",
-              // css: "webix_dark",
-              data: [],
-              click: (id) => {
-                if (id === "signout") {
-                  webix.delay(() => {
-                    this.app.authenticationData = null;
-                    this.show("signin");
-                    this.resetSidebar();
-                  });
-                } else this.show(id);
-              },
-            },
-            {
-              type: "wide",
-              padding: 2,
-              css: "app_layout",
-              rows: [
-                {
-                  $subview: true,
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    };
-    return ui;
-  }
+            ],
+          },
+        ],
+      },
+    ],
+  });
 
   /**
    * Сброс сайдбара в состояние до логина
    */
-  resetSidebar = () => {
-    $$("sidebar").clearAll();
-    if ($$("play")) $$("toolbar").removeView("play");
-    $$("sidebar").add({
+  resetSidebar() {
+    this.$$("sidebar").clearAll();
+    if (this.$$("play")) this.$$("toolbar").removeView("play");
+    this.$$("sidebar").add({
       id: "signin",
       icon: "mdi mdi-login-variant",
       value: "Sign In",
     });
-    $$("sidebar").add({
+    this.$$("sidebar").add({
       id: "about",
       icon: "mdi mdi-information-outline",
       value: "About",
     });
-    $$("sidebar").select("signin");
-  };
+    this.$$("sidebar").select("signin");
+  }
 
   /**
    * The method is called once when a view is rendered
    */
   init() {
-    $$("sidebar")
+    this.$$("sidebar")
       .getPopup()
       .attachEvent("onBeforeShow", () => {
         return false;
