3bd1fb26180e075fd3694c5e5ccc6c7c493c241e
Jerry Bruwes <jbruwes@icloud.com>
2022-01-04 13:37:54 +0300

    Update init.js

--- cms/sources/workers/init.js
+++ cms/sources/workers/init.js
@@ -15,47 +15,49 @@ onmessage = async ({
 }) => {
   const io = new S3(pAccessKeyId, pSecretAccessKey, pBucketName, pRegion);
   try {
-    io.headObject("index.json");
+    await io.headObject("index.json");
   } catch (err) {
     const id = new Date().valueOf();
-    io.putObject(
+    await io.putObject(
       "index.json",
       "application/json",
       `{"link":"","text":"","date":"","image":"","visible":true,"value":"${pBucketName}","id":${id}}`
     );
-    io.putObject(`${id}.htm`, "text/html", "");
+    await io.putObject(`${id}.htm`, "text/html", "");
   }
   try {
-    io.headObject("index.cdn.json");
+    await io.headObject("index.cdn.json");
   } catch (err) {
-    io.putObject("index.cdn.json", "application/json", "[]");
+    await io.putObject("index.cdn.json", "application/json", "[]");
   }
   try {
-    io.headObject("index.js");
+    await io.headObject("index.js");
   } catch (err) {
-    io.putObject(
+    await io.putObject(
       "index.js",
       "application/javascript",
       "function init(){try{}catch(e){}}"
     );
   }
   try {
-    io.headObject("index.css");
+    await io.headObject("index.css");
   } catch (err) {
-    io.putObject("index.css", "text/css", "");
+    await io.putObject("index.css", "text/css", "");
   }
   try {
-    io.headObject("index.cdn.css");
+    await io.headObject("index.cdn.css");
   } catch (err) {
-    io.putObject("index.cdn.css", "text/css", "");
+    await io.putObject("index.cdn.css", "text/css", "");
   }
   try {
-    io.headObject("index.htm");
+    await io.headObject("index.htm");
   } catch (err) {
-    io.putObject(
+    await io.putObject(
       "index.htm",
       "text/html",
-      (await (await fetch("index.htm", { cache: "no-store" })).text()).replace(
+      (
+        await (await fetch("index.htm", { cache: "no-store" })).text()
+      ).replace(
         "{pusher}",
         '<div data-static="" class="ui container" style="z-index:1"><div id="content" style="margin:0px;flex:1 1 auto"><main></main></div></div>'
       )
