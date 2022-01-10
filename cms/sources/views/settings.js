29a5db4bcf8a103bc1123dc7e21ed328082805c2
Jerry Bruwes <jbruwes@icloud.com>
2022-01-10 18:27:48 +0300

    Update settings.js

--- cms/sources/views/settings.js
+++ cms/sources/views/settings.js
@@ -55,7 +55,7 @@ export default class SettingsView extends JetView {
             {
               view: "uploader",
               id: "uploader",
-              value: "Upload Icon",
+              value: "Upload ico",
               multiple: false,
               autosend: false,
               name: "files",
@@ -65,6 +65,44 @@ export default class SettingsView extends JetView {
             {
               view: "list",
               id: "bglist",
+              type: "uploader",
+              template:
+                "{common.removeIcon()}{common.percent()}{common.fileName()}",
+              autoheight: true,
+              borderless: true,
+            },
+            {
+              view: "uploader",
+              id: "svgUploader",
+              value: "Upload svg",
+              multiple: false,
+              autosend: false,
+              name: "files",
+              link: "svgBglist",
+              accept: "image/svg+xml",
+            },
+            {
+              view: "list",
+              id: "svgBglist",
+              type: "uploader",
+              template:
+                "{common.removeIcon()}{common.percent()}{common.fileName()}",
+              autoheight: true,
+              borderless: true,
+            },
+            {
+              view: "uploader",
+              id: "pngUploader",
+              value: "Upload png",
+              multiple: false,
+              autosend: false,
+              name: "files",
+              link: "pngBglist",
+              accept: "image/png",
+            },
+            {
+              view: "list",
+              id: "pngBglist",
               type: "uploader",
               template:
                 "{common.removeIcon()}{common.percent()}{common.fileName()}",
@@ -88,29 +126,29 @@ export default class SettingsView extends JetView {
   config = () => this.#config;
 
   /**
-   *
+   * @param uploader
+   * @param filename
    */
-  async init() {
+  async doIcon(uploader, filename) {
     try {
-      await this.app.io.headObject("favicon.ico");
+      await this.app.io.headObject(filename);
       if ($$("sidebar").getSelectedId() === "settings") {
-        $$("uploader").files.data.clearAll();
-        $$("uploader").addFile(
+        uploader.files.data.clearAll();
+        uploader.addFile(
           {
-            name: "favicon.ico",
-            sname: "favicon.ico",
+            name: filename,
+            sname: filename,
           },
           0
         );
       }
     } catch (err) {
-      // console.log(err);
+      // webix.message({text:err.message,type:"error"});
     } finally {
-      $$("uploader").attachEvent("onAfterFileAdd", async (pFile) => {
+      uploader.attachEvent("onAfterFileAdd", async (pFile) => {
         const file = pFile;
-        // file.file.sname = "favicon.ico";
         try {
-          await this.app.io.putObject("favicon.ico", file.file.type, file.file);
+          await this.app.io.putObject(filename, file.file.type, file.file);
           webix.message("Settings save complete");
         } catch (err) {
           webix.message({
@@ -119,9 +157,9 @@ export default class SettingsView extends JetView {
           });
         }
       });
-      $$("uploader").files.attachEvent("onAfterDelete", async () => {
+      uploader.files.attachEvent("onAfterDelete", async () => {
         try {
-          await this.app.io.deleteObject("favicon.ico");
+          await this.app.io.deleteObject(filename);
           webix.message("Settings save complete");
         } catch (err) {
           webix.message({
@@ -131,6 +169,15 @@ export default class SettingsView extends JetView {
         }
       });
     }
+  }
+
+  /**
+   *
+   */
+  async init() {
+    this.doIcon($$("uploader"), "favicon.ico");
+    this.doIcon($$("pngUploader"), "icon.png");
+    this.doIcon($$("svgUploader"), "icon.svg");
     try {
       this.prop = JSON.parse(await this.app.io.getObject("index.json"));
       if (this.prop[0].yandex) $$("yandex").setValue(this.prop[0].yandex);
