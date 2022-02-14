abf4b23b47e468f937cf8fffcb3e5f16d2bc987f
Jerry Bruwes <jbruwes@icloud.com>
2022-02-14 18:54:28 +0300

    Create main.js

---
+++ pkg/main.js
@@ -0,0 +1,14 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import { app, BrowserWindow } from "electron";
+import serve from "electron-serve";
+
+const loadURL = serve({ directory: "." });
+(async () => {
+  await app.whenReady();
+  const mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 768,
+  });
+  await loadURL(mainWindow);
+})();
