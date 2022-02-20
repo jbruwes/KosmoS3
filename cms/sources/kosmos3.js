8b53432d3d579bd84f6644787bda2b08d96af3e4
Jerry Bruwes <jbruwes@icloud.com>
2022-02-20 09:58:22 +0300

    Update kosmos3.js

--- cms/sources/kosmos3.js
+++ cms/sources/kosmos3.js
@@ -6,7 +6,7 @@ import "./styles/app.css";
 import "fomantic-ui-css/components/icon.css";
 import "@mdi/font/css/materialdesignicons.css";
 import { JetApp, EmptyRouter } from "webix-jet";
-import * as webix from "webix";
+import * as webix from "webix/webix.min";
 
 /**
  *
