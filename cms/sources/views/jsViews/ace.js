3297ef77893467ef7a9ac415af832f3c5aa03b9b
Jerry Bruwes <jbruwes@icloud.com>
2022-02-21 08:01:58 +0300

    Update ace.js

--- cms/sources/views/jsViews/ace.js
+++ cms/sources/views/jsViews/ace.js
@@ -15,6 +15,7 @@ export default class AceView extends JetView {
    */
   destroy() {
     if (this.#session) this.#session.removeAllListeners("change");
+    this.#session = null;
   }
 
   /**
@@ -48,50 +49,47 @@ export default class AceView extends JetView {
   async cb(text) {
     const timeoutId = [];
     const that = this;
+    const editor = await $$("ace-js").getEditor(true);
     if (this.app) {
-      const editor = await $$("ace-js").getEditor(true);
-      if (this.app) {
-        this.#session = editor.getSession();
-        this.#session.setUseWrapMode(true);
-        this.#session.setValue(
-          text
-            .replace(/^function init\(\){try{/, "")
-            .replace(/}catch\(e\){}}$/, ""),
-          -1
-        );
-        this.#session.on("change", () => {
-          timeoutId.push(
-            webix.delay(
-              async () => {
-                timeoutId.pop();
-                if (!timeoutId.length) {
+      this.#session = editor.getSession();
+      this.#session.setUseWrapMode(true);
+      this.#session.setValue(
+        text
+          .replace(/^function init\(\){try{/, "")
+          .replace(/}catch\(e\){}}$/, ""),
+        -1
+      );
+      this.#session.on("change", () => {
+        timeoutId.push(
+          webix.delay(
+            async () => {
+              timeoutId.pop();
+              if (!timeoutId.length) {
+                try {
+                  await this.app.io.putObject(
+                    "index.js",
+                    "application/javascript",
+                    `function init(){try{${$$("ace-js")
+                      .getEditor()
+                      .getValue()}}catch(e){}}`
+                  );
+                  if (this.app) webix.message("JS save complete");
+                } catch (err) {
                   if (this.app)
-                    try {
-                      await this.app.io.putObject(
-                        "index.js",
-                        "application/javascript",
-                        `function init(){try{${$$("ace-js")
-                          .getEditor()
-                          .getValue()}}catch(e){}}`
-                      );
-                      if (this.app) webix.message("JS save complete");
-                    } catch (err) {
-                      if (this.app)
-                        webix.message({
-                          text: err.message,
-                          type: "error",
-                        });
-                    }
+                    webix.message({
+                      text: err.message,
+                      type: "error",
+                    });
                 }
-              },
-              that,
-              [],
-              1000
-            )
-          );
-        });
-        editor.resize();
-      }
+              }
+            },
+            that,
+            [],
+            1000
+          )
+        );
+      });
+      editor.resize();
     }
   }
 
@@ -99,12 +97,11 @@ export default class AceView extends JetView {
    *
    */
   async main() {
-    if (this.app)
-      try {
-        const indexJs = await this.app.io.getObject("index.js");
-        if (this.app) this.cb(indexJs);
-      } catch (err) {
-        if (this.app) this.cb("");
-      }
+    try {
+      const indexJs = await this.app.io.getObject("index.js");
+      if (this.app) this.cb(indexJs);
+    } catch (err) {
+      if (this.app) this.cb("");
+    }
   }
 }
