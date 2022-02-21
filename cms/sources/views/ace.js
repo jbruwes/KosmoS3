d1f3766031a9fa9fb1d41ccd37b8926da61cc734
Jerry Bruwes <jbruwes@icloud.com>
2022-02-21 13:58:11 +0300

    Update ace.js

--- cms/sources/views/ace.js
+++ cms/sources/views/ace.js
@@ -1,14 +1,32 @@
 import { JetView } from "webix-jet";
-import * as webix from "webix";
+import * as webix from "webix/webix.min";
 import "../ace";
 
 /**
  *
  */
 export default class AceView extends JetView {
-  #config = null;
+  #config;
+
+  #editor;
+
+  #session;
 
   #timeoutId = [];
+
+  /**
+   *
+   */
+  destroy() {
+    if (this.#session) {
+      this.#session.removeAllListeners("changeAnnotation");
+      this.#session.removeAllListeners("change");
+    }
+    this.#config = null;
+    this.#editor = null;
+    this.#session = null;
+    this.#timeoutId = null;
+  }
 
   /**
    * @param app
@@ -30,49 +48,50 @@ export default class AceView extends JetView {
   /**
    *
    */
-  async init() {
-    const editor = await this.getRoot().getEditor(true);
-    const session = editor.getSession();
-    session.on("changeAnnotation", function onChangeAnnotation() {
-      const annotations = session.getAnnotations() || [];
-      const len = annotations.length;
-      let i = len;
-      while (i) {
-        i -= 1;
-        if (/doctype first\. Expected/.test(annotations[i].text))
-          annotations.splice(i, 1);
-      }
-      if (len > annotations.length) session.setAnnotations(annotations);
-    });
-    session.setUseWrapMode(true);
+  init() {
+    this.main();
   }
-
-  /**
-   */
-  aceChange = () => {
-    this.#timeoutId.push(
-      webix.delay(
-        () => {
-          this.#timeoutId.pop();
-          if (!this.#timeoutId.length)
-            $$("tinymce").setValue(this.getRoot().getEditor().getValue());
-        },
-        this,
-        [],
-        1000
-      )
-    );
-  };
 
   /**
    * @param html
    */
   async setValue(html) {
-    const editor = await this.getRoot().getEditor(true);
-    const session = editor.getSession();
-    session.off("change", this.aceChange, this);
-    session.setValue(html, -1);
-    session.on("change", this.aceChange, this);
-    editor.resize();
+    const aceChange = () => {
+      this.#timeoutId.push(
+        webix.delay(
+          () => {
+            this.#timeoutId.pop();
+            if (!this.#timeoutId.length)
+              $$("tinymce").setValue(this.#editor.getValue());
+          },
+          this,
+          [],
+          1000
+        )
+      );
+    };
+    this.#session.off("change", aceChange);
+    this.#session.setValue(html, -1);
+    this.#session.on("change", aceChange);
+    this.#editor.resize();
+  }
+
+  async main() {
+    this.#editor = await this.getRoot().getEditor(true);
+    if (this.app) {
+      this.#session = this.#editor.getSession();
+      this.#session.on("changeAnnotation", () => {
+        const annotations = this.#session.getAnnotations() || [];
+        const len = annotations.length;
+        let i = len;
+        while (i) {
+          i -= 1;
+          if (/doctype first\. Expected/.test(annotations[i].text))
+            annotations.splice(i, 1);
+        }
+        if (len > annotations.length) this.#session.setAnnotations(annotations);
+      });
+      this.#session.setUseWrapMode(true);
+    }
   }
 }
