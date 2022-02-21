49e022009b8eb3014e7a39e05a1428d0cc8d7245
Jerry Bruwes <jbruwes@icloud.com>
2022-02-21 13:59:39 +0300

    Update cdntoolbar.js

--- cms/sources/views/jsViews/cdntoolbar.js
+++ cms/sources/views/jsViews/cdntoolbar.js
@@ -7,6 +7,13 @@ export default class CdnToolbarView extends JetView {
   #config;
 
   /**
+   *
+   */
+  destroy() {
+    this.#config = null;
+  }
+
+  /**
    * @param app
    */
   constructor(app) {
