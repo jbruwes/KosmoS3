5b79bab095fe0c8b918612775871d4fedf420089
Jerry Bruwes <jbruwes@icloud.com>
2022-04-02 17:37:58 +0300

    Update onhashchange.js

--- fel/sources/modules/onhashchange.js
+++ fel/sources/modules/onhashchange.js
@@ -22,35 +22,11 @@ jarallaxVideo();
  *
  * @param {*[]} usrScripts Массив промисов пользовательских подгружаемых
  *  скриптов
- * @param {object.<*>} scripts Объект с промисами подгружаемых скриптов
+ * @param {Object<*>} scripts Объект с промисами подгружаемых скриптов
  * @param {object} index Структура сайта
  * @param {string} sel Dom путь
  */
 export default function onhashchange(usrScripts, scripts, index, sel) {
-  /**
-   * Запуск пользовательского скрипта
-   */
-  function initCall() {
-    init.call(index);
-  }
-  /**
-   * Вспомогательная ф-ция для преобразования объекта в массив
-   *
-   * @param {*} value Текущее обещание
-   * @returns {*} Возвращаемое текущее обещание
-   */
-  function map(value) {
-    return value;
-  }
-  /**
-   * Проверка наличия ф-ции init и
-   * запуск ожидания загрузки пользовательских скриптов
-   */
-  function runUsrCode() {
-    if (typeof init === "function") {
-      $.when(...$.merge($.map(scripts, map), usrScripts)).done(initCall);
-    }
-  }
   const pSel = sel || "#content";
   carousel(index, pSel);
   deck(index, pSel);
@@ -74,5 +50,12 @@ export default function onhashchange(usrScripts, scripts, index, sel) {
   $("img").on("load", AOS.refresh);
   AOS.refresh();
   jarallax(document.querySelectorAll(".jarallax"));
-  $.when(scripts.indexJs, scripts.indexCdnJson).done(runUsrCode);
+  $.when(
+    ...$.merge(
+      $.map(scripts, (value) => value),
+      usrScripts
+    )
+  ).done(() => {
+    if (typeof init === "function") init.call(index);
+  });
 }
