02d8bf73ae14d9b331f92e1e73763cd0bb4045bc
Jerry Bruwes <jbruwes@icloud.com>
2022-04-30 18:14:31 +0300

    Update html.js

--- cms/sources/workers/html.js
+++ cms/sources/workers/html.js
@@ -7,39 +7,41 @@
  */
 export default async function html(pPath, pHtml, pIo, pNode) {
   let lHtml = pHtml;
-  lHtml = lHtml.replace(/#base#/g, "/");
+  lHtml = lHtml.replace(/{{ base }}/g, "/");
   lHtml = lHtml.replace(
-    /#title#/g,
+    /{{ title }}/g,
     (pNode.title ? pNode.title : pNode.value).replace(/"/g, "&quot;")
   );
   lHtml = lHtml.replace(
-    /#description#/g,
+    /{{ description }}/g,
     pNode.description ? pNode.description.replace(/"/g, "&quot;") : ""
   );
   lHtml = lHtml.replace(
-    /#yandex#/g,
+    /{{ yandex }}/g,
     pNode.yandex ? pNode.yandex.replace(/"/g, "&quot;") : ""
   );
   lHtml = lHtml.replace(
-    /#google#/g,
+    /{{ google }}/g,
     pNode.google ? pNode.google.replace(/"/g, "&quot;") : ""
   );
   const lImage = pNode.image;
   lHtml = lHtml.replace(
-    /#image#/g,
+    /{{ image }}/g,
     lImage ? `https://${pIo.getBucket()}/${encodeURI(lImage)}` : ""
   );
-  if (pNode.metrika) lHtml = lHtml.replace(/#metrika#/g, pNode.metrika);
+  if (pNode.metrika) lHtml = lHtml.replace(/{{ metrika }}/g, pNode.metrika);
   else
     lHtml = lHtml.replace(
       /<script id="yandex"[^>]*>([\s\S]*?)<\/script>/gi,
       ""
     );
-  if (pNode.analytics) lHtml = lHtml.replace(/#analytics#/g, pNode.analytics);
+  if (pNode.analytics)
+    lHtml = lHtml.replace(/{{ analytics }}/g, pNode.analytics);
   else
-    lHtml = lHtml
-      .replace(/<script id="google"[^>]*>([\s\S]*?)<\/script>/gi, "")
-      .replace(/<script id="analytics"[^>]*>([\s\S]*?)<\/script>/gi, "");
+    lHtml = lHtml.replace(
+      /<script id="google"[^>]*>([\s\S]*?)<\/script>/gi,
+      ""
+    );
   let lHtm = "";
   try {
     lHtm = await pIo.getObject(`${pNode.id}.htm`);
@@ -54,14 +56,20 @@ export default async function html(pPath, pHtml, pIo, pNode) {
       await pIo.putObject(
         `${lUrl}index.html`,
         "text/html",
-        lHtml.replace(/#url#/g, `https://${pIo.getBucket()}/${encodeURI(lUrl)}`)
+        lHtml.replace(
+          /{{ url }}/g,
+          `https://${pIo.getBucket()}/${encodeURI(lUrl)}`
+        )
       );
     }
     const lPath = pPath ? `${pPath}/` : "";
     await pIo.putObject(
       `${lPath}index.html`,
       "text/html",
-      lHtml.replace(/#url#/g, `https://${pIo.getBucket()}/${encodeURI(lPath)}`)
+      lHtml.replace(
+        /{{ url }}/g,
+        `https://${pIo.getBucket()}/${encodeURI(lPath)}`
+      )
     );
   }
 }
