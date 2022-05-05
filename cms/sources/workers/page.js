d51d22f9e716b075bb4bc708f4e04beaaa95798f
Jerry Bruwes <jbruwes@icloud.com>
2022-05-05 14:25:56 +0300

    Update page.js

--- cms/sources/workers/page.js
+++ cms/sources/workers/page.js
@@ -5,14 +5,16 @@ import S3 from "../s3";
 import html from "./html";
 
 /**
- * @param root0
- * @param root0.data
- * @param root0.data.pAccessKeyId
- * @param root0.data.pSecretAccessKey
- * @param root0.data.pBucketName
- * @param root0.data.pRegion
- * @param root0.data.pId
- * @param root0.data.pEndpoint
+ * Рендеринг страницы
+ *
+ * @param {object} root0 Сообщение
+ * @param {object} root0.data Данные сообщения
+ * @param {string} root0.data.pAccessKeyId Идентификатор
+ * @param {string} root0.data.pSecretAccessKey Ключ доступа
+ * @param {string} root0.data.pBucketName Название корзины
+ * @param {string} root0.data.pRegion Регион
+ * @param {string} root0.data.pEndpoint Точка входа
+ * @param {string} root0.data.pId Идентификатор страницы
  */
 onmessage = async ({
   data: {
