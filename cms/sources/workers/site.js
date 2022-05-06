2989097df559c7490205b7db5feb96afeae4c271
Jerry Bruwes <jbruwes@icloud.com>
2022-05-06 16:05:07 +0300

    Update site.js

--- cms/sources/workers/site.js
+++ cms/sources/workers/site.js
@@ -42,7 +42,11 @@ onmessage = async ({
       );
     lNode.path.shift();
     lNode.path = lNode.path.join("/");
-    await html(lNode.path, lHtml, io, lNode);
+    lNode.yandex = lJson.yandex;
+    lNode.google = lJson.google;
+    lNode.metrika = lJson.metrika;
+    lNode.analytics = lJson.analytics;
+    await html(lHtml, io, lNode);
     i += 1;
     if (i < lMapLength) setTimeout(run);
     else
@@ -51,14 +55,16 @@ onmessage = async ({
         "application/xml",
         `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${lMap
           .map((e) => {
-            const lUrl = e.url
+            let lUrl = e.url
               ? decodeURI(e.url.trim().replace(/^\/+|\/+$/g, "")).replace(
                   / /g,
                   "_"
                 )
-              : e.path;
-            return `<url><loc>https://${pBucketName}/${
-              lUrl ? `${lUrl}/` : ""
+              : "";
+            lUrl = lUrl || e.path;
+            lUrl = lUrl ? `${lUrl}/` : "";
+            return `<url><loc>https://${pBucketName}${
+              lUrl ? `/${encodeURI(lUrl)}` : ""
             }</loc>${e.lastmod ? `<lastmod>${e.lastmod}</lastmod>` : ""}${
               e.changefreq ? `<changefreq>${e.changefreq}</changefreq>` : ""
             }${e.priority ? `<priority>${e.priority}</priority>` : ""}</url>`;
