40168fe9967c59b82c26fe16d276e35f478b0475
Jerry Bruwes <jbruwes@icloud.com>
2022-05-18 16:49:33 +0300

    Update index.d.ts

--- fel/sources/typings/index.d.ts
+++ fel/sources/typings/index.d.ts
@@ -2,3 +2,42 @@ declare module "jsel";
 declare module "glightbox";
 declare module "jarallax";
 declare function init(): void;
+
+declare interface Branch {
+  yandex: string;
+  google: string;
+  metrika: string;
+  analytics: string;
+
+  value: string;
+  id: number | string;
+  visible: boolean;
+  date: string;
+  image: string;
+  $count: number;
+  $parent: number | string;
+  $level: number;
+  open: boolean;
+  lastmod: string;
+  changefreq: string;
+  priority: number;
+  description: string;
+  keywords: string;
+  url: string;
+  title: string;
+  icon: string;
+  path: string;
+  data: IBranch[];
+}
+
+declare interface Url {
+  url: string;
+  id: numberl;
+}
+
+declare interface Data {
+  index: Branch;
+  urls: Url[];
+  content: string;
+  context: import("page").Context;
+}
