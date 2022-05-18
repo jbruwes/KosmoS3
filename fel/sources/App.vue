beecb7e101b0a060f7194226cea79e24fe15f629
Jerry Bruwes <jbruwes@icloud.com>
2022-05-18 16:54:57 +0300

    Update App.vue

--- fel/sources/App.vue
+++ fel/sources/App.vue
@@ -1,7 +1,8 @@
-<script>
+<script lang="ts">
 import { defineComponent, nextTick } from "vue";
 import { jarallax, jarallaxVideo } from "jarallax";
 import page from "page";
+import type { Context } from "page";
 import jsel from "jsel";
 import AOS from "aos";
 import GLightbox from "glightbox";
@@ -25,21 +26,21 @@ export default defineComponent({
   /**
    * Инициализация данных приложения
    *
-   * @returns {object} Объект data
+   * @returns {Data} Объект data
    */
-  data: () => ({
-    index: {},
+  data: (): Data => ({
+    index: <Branch>{},
     urls: [],
     content: "",
-    context: { routePath: null },
+    context: <Context>{},
   }),
   computed: {
     /**
      * Пользовательские скрипты
      *
-     * @returns {Array} Массив обещаний по зпгрузке пользовательских скриптов
+     * @returns {Promise<HTMLScriptElement>[]} Массив обещаний по зпгрузке пользовательских скриптов
      */
-    scripts() {
+    scripts(): Promise<HTMLScriptElement>[] {
       return [...this.urls, { url: "index.js" }]
         .filter((script) => script.url)
         .map((script) => this.$loadScript(script.url));
@@ -47,29 +48,31 @@ export default defineComponent({
     /**
      * Текущий объект
      *
-     * @returns {object} Текущий объект для загрузки
+     * @returns {Branch} Текущий объект для загрузки
      */
-    node() {
+    node(): Branch | undefined {
       return this.plainIndex.find(
-        (e) =>
+        (e: Branch) =>
           e.path === this.context.routePath || e.url === this.context.routePath
       );
     },
     /**
      * Плоский индекс
      *
-     * @returns {object} Плоский индекс для поиска
+     * @returns {Branch[]} Плоский индекс для поиска
      */
-    plainIndex() {
+    plainIndex(): Branch[] {
       const plainIndex = jsel(this.index).selectAll("//*[@id]");
-      plainIndex.forEach((node) => {
+      plainIndex.forEach((node: Branch) => {
         const lNode = node;
-        lNode.path = jsel(this.index).selectAll(
+        let path = jsel(this.index).selectAll(
           `//*[@id="${lNode.id}"]/ancestor-or-self::*[@id]`
         );
-        lNode.path = lNode.path.map((e) => e.value.trim().replace(/\s/g, "_"));
-        lNode.path.shift();
-        lNode.path = `/${lNode.path.join("/")}`;
+        path = path.map((e: Branch) =>
+          e.value.trim().replace(/\s/g, "_")
+        );
+        path.shift();
+        lNode.path = `/${path.join("/")}`;
         lNode.url = lNode.url
           ? `/${lNode.url.trim().replace(/^\/+|\/+$/g, "")}`
           : "";
@@ -83,43 +86,45 @@ export default defineComponent({
      */
     async node() {
       let html = "";
-      try {
-        const response = await fetch(
-          `${encodeURIComponent(this.node.id)}.htm`,
-          {
-            cache: "no-store",
-          }
-        );
-        html = response.status === 200 ? await response.text() : "";
-      } finally {
-        document.title = (
-          this.node.title ? this.node.title : this.node.value
-        ).replace(/"/g, "&quot;");
-        const lUrl = this.node.url || this.context.routePath;
-        [
-          ['meta[name="description"]', this.node.description],
-          ['meta[name="keywords"]', this.node.keywords],
-          ['meta[property="og:title"]', document.title],
-          ['meta[property="og:description"]', this.node.description],
+      if (this.node)
+        try {
+          const response = await fetch(
+            `${encodeURIComponent(this.node.id)}.htm`,
+            {
+              cache: "no-store",
+            }
+          );
+          html = response.status === 200 ? await response.text() : "";
+        } finally {
+          document.title = (
+            this.node.title ? this.node.title : this.node.value
+          ).replace(/"/g, "&quot;");
+          const lUrl = this.node.url || this.context.routePath;
           [
-            'meta[property="og:url"]',
-            `${window.location.origin}${
-              lUrl === "/" ? "" : `${encodeURI(lUrl)}/`
-            }`,
-          ],
-          [
-            'meta[property="og:image"]',
-            this.node.image
-              ? `${window.location.origin}/${this.node.image}`
-              : "",
-          ],
-        ].forEach((e) => {
-          document.head.querySelector(e[0]).content = e[1]
-            ? e[1].replace(/"/g, "&quot;")
-            : "";
-        });
-        this.content = html;
-      }
+            ['meta[name="description"]', this.node.description],
+            ['meta[name="keywords"]', this.node.keywords],
+            ['meta[property="og:title"]', document.title],
+            ['meta[property="og:description"]', this.node.description],
+            [
+              'meta[property="og:url"]',
+              `${window.location.origin}${
+                lUrl === "/" ? "" : `${encodeURI(lUrl)}/`
+              }`,
+            ],
+            [
+              'meta[property="og:image"]',
+              this.node.image
+                ? `${window.location.origin}/${this.node.image}`
+                : "",
+            ],
+          ].forEach((e) => {
+            const element = document.head.querySelector(
+              e[0]
+            ) as HTMLMetaElement;
+            element.content = e[1] ? e[1].replace(/"/g, "&quot;") : "";
+          });
+          this.content = html;
+        }
     },
     /**
      * При изменении контента на странице обновляем компоненты
@@ -136,7 +141,7 @@ export default defineComponent({
     index() {
       if (!window.frameElement) {
         page();
-        this.plainIndex.forEach((node) => {
+        this.plainIndex.forEach((node: Branch) => {
           if (node.url) page(node.url, this.route);
           page(node.path, this.route);
         });
@@ -160,9 +165,9 @@ export default defineComponent({
     /**
      * Обработка роутинга
      *
-     * @param {object} context Объект роутинга
+     * @param {Context} context Объект роутинга
      */
-    route(context) {
+    route(context: Context) {
       this.context = context;
     },
     /**
@@ -170,7 +175,7 @@ export default defineComponent({
      *
      * @param {string} pSel Селектор
      */
-    async onhashchange(pSel = "#content") {
+    async onhashchange(pSel: string = "#content") {
       carousel(this.index, pSel);
       deck(this.index, pSel);
       cardgrid(this.index, pSel);
