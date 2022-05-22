2e897e83d3532347363b44ee5737cd1370ccb720
Jerry Bruwes <jbruwes@icloud.com>
2022-05-22 15:24:32 +0300

    Update breadcrumbs.js

--- fel/sources/modules/breadcrumbs.js
+++ fel/sources/modules/breadcrumbs.js
@@ -91,8 +91,8 @@ export default function breadcrumbs(index, sel) {
     if (dataChildren.length) {
       try {
         $(this)
-          .removeData("auto")
-          .removeAttr("data-auto")
+          // .removeData("auto")
+          // .removeAttr("data-auto")
           .render(dataChildren, {
             "a.step": {
               "i<-": {
