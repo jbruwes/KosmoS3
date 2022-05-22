83fdb3ee67cebe24efef3de702c18a56b970babe
Jerry Bruwes <jbruwes@icloud.com>
2022-05-22 15:24:38 +0300

    Update cardgrid.js

--- fel/sources/modules/cardgrid.js
+++ fel/sources/modules/cardgrid.js
@@ -60,8 +60,8 @@ export default function cardgrid(index, sel) {
     if (dataChildren.length) {
       try {
         $(this)
-          .removeData("auto")
-          .removeAttr("data-auto")
+          // .removeData("auto")
+          // .removeAttr("data-auto")
           .render(dataChildren, {
             "div.column": {
               "i<-": {
