c54b8971b9dab66110456ee24c0cc6fc65b05d25
Jerry Bruwes <jbruwes@icloud.com>
2022-05-23 13:51:05 +0300

    Update pagination.js

--- fel/sources/modules/pagination.js
+++ fel/sources/modules/pagination.js
@@ -118,6 +118,7 @@ export default function pagination(index, sel) {
       try {
         $(this)
           .removeData("auto")
+          .removeAttr("data-auto")
           .render(dataChildren, {
             "a.item:empty": {
               "i<-": {
