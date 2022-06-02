73a1bd597270b99ca166e5e7343bf464e2f19bf2
Jerry Bruwes <jbruwes@icloud.com>
2022-06-02 16:10:50 +0200

    Update init.js

--- cms/sources/workers/init.js
+++ cms/sources/workers/init.js
@@ -62,7 +62,7 @@ onmessage = async ({
       io.putObject(
         "index.htm",
         "text/html",
-        '<div class="container mx-auto static flex flex-auto flex-col inset-0 pointer-events-none" style="z-index:1"><div id="content" style="margin:0px;flex:1 1 auto"><article v-if="!content"></article><article v-else v-html="content"></article></div></div>'
+        '<div class="v-container py-0 position-static" style="z-index:1"><div id="content" style="margin:0px;flex:1 1 auto"><article v-if="!content"></article><article v-else v-html="content"></article></div></div>'
         // '<div data-static="" class="ui container" style="z-index:1"><div id="content" style="margin:0px;flex:1 1 auto"><article v-if="!content"></article><article v-else><v-runtime-template :template="content"></v-runtime-template></article></div></div>'
       )
     );
