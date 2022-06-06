667e5f21ad9de0a4bf739239879f7eb68bb8aacf
Jerry Bruwes <jbruwes@icloud.com>
2022-06-06 16:10:34 +0200

    Update VNavigationDrawerK3.vue

--- fel/sources/modules/VNavigationDrawerK3.vue
+++ fel/sources/modules/VNavigationDrawerK3.vue
@@ -3,28 +3,49 @@
     <v-list-item>
       <v-list-item-content>
         <v-list-item-title class="text-h6">
-          {{ index2 ? index2.value: "Title!" }}
+          {{ mainTitle }}
         </v-list-item-title>
-        <v-list-item-subtitle> {{ store.index ? store.index.value: "Subtitle" }} </v-list-item-subtitle>
+        <v-list-item-subtitle> {{ mainDescription }} </v-list-item-subtitle>
       </v-list-item-content>
     </v-list-item>
     <v-divider></v-divider>
+    <v-list nav dense>
+      <v-list-item
+        v-for="(item, i) in items"
+        :key="i"
+        :value="item"
+        active-color="primary"
+      >
+        <v-list-item-avatar>
+          <v-icon :icon="item.icon"></v-icon>
+        </v-list-item-avatar>
+
+        <v-list-item-title v-text="item.text"></v-list-item-title>
+      </v-list-item>
+    </v-list>
   </v-navigation-drawer>
 </template>
 
 <script>
-import * as store from "../stores/store.js";
-import * as index2 from "../stores/index.js";
+import { storeToRefs } from "pinia";
+import defineStore from "../stores/core.js";
 export default {
-  data: () => ({
-    test: "Title",
-    store,
-    index2,
-  }),
-  watch: {
-    store() {
-      console.log("watch ", this.store.index);
-    },
+  setup() {
+    const core = defineStore();
+    const { mainTitle, mainDescription } = storeToRefs(core);
+    return { mainTitle, mainDescription };
   },
+  data: () => ({
+    selectedItem: 0,
+    items: [
+      { text: "My Files", icon: "mdi-folder" },
+      { text: "Shared with me", icon: "mdi-account-multiple" },
+      { text: "Starred", icon: "mdi-star" },
+      { text: "Recent", icon: "mdi-history" },
+      { text: "Offline", icon: "mdi-check-circle" },
+      { text: "Uploads", icon: "mdi-upload" },
+      { text: "Backups", icon: "mdi-cloud-upload" },
+    ],
+  }),
 };
 </script>
