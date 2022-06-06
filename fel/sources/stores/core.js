eb7190ea8434a323c2193961f5f47c5172ecec73
Jerry Bruwes <jbruwes@icloud.com>
2022-06-06 16:10:25 +0200

    Create core.js

---
+++ fel/sources/stores/core.js
@@ -0,0 +1,67 @@
+import { defineStore } from "pinia";
+import jsel from "jsel";
+
+export default defineStore("core", {
+  /**
+   * @returns {Object} Переменные состояния
+   */
+  state: () => ({
+    index: undefined,
+    context: undefined,
+  }),
+  getters: {
+    mainTitle() {
+      if (this.index)
+        return this.index.title ? this.index.title : this.index.value;
+      else return "";
+    },
+    mainDescription() {
+      if (this.index) return this.index.description;
+      else return "";
+    },
+    /**
+     * Плоский индекс
+     *
+     * @returns {object[]} Плоский индекс для поиска
+     */
+    plainIndex() {
+      const plainIndex = jsel(this.index).selectAll("//*[@id]");
+      plainIndex.forEach((node) => {
+        const lNode = node;
+        let path = jsel(this.index).selectAll(
+          `//*[@id="${lNode.id}"]/ancestor-or-self::*[@id]`
+        );
+        path = path.map((e) => e.value.trim().replace(/\s/g, "_"));
+        path.shift();
+        lNode.path = `/${path.join("/")}`;
+        lNode.url = lNode.url
+          ? `/${lNode.url.trim().replace(/^\/+|\/+$/g, "")}`
+          : "";
+      });
+      return plainIndex;
+    },
+    /**
+     * Текущий объект
+     *
+     * @returns {object} Текущий объект для загрузки
+     */
+    node() {
+      return this.plainIndex.find((e) =>
+        this.context
+          ? e.path === this.context.routePath ||
+            e.url === this.context.routePath
+          : undefined
+      );
+    },
+  },
+  actions: {
+    /**
+     * Загрузка данных в index
+     */
+    async initIndex() {
+      [this.index] = await (
+        await fetch("index.json", { cache: "no-store" })
+      ).json();
+    },
+  },
+});
