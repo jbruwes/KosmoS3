373d58c75e9614405190c8c91c21b94fddc220ad
Jerry Bruwes <jbruwes@icloud.com>
2022-06-08 16:18:37 +0200

    Update VNavigationDrawerK3.vue

--- fel/sources/modules/VNavigationDrawerK3.vue
+++ fel/sources/modules/VNavigationDrawerK3.vue
@@ -1,51 +1,45 @@
 <template>
   <v-navigation-drawer app temporary>
     <v-list-item>
+      <v-list-item-avatar>
+        <v-icon :icon="`mdi-${index.icon}`" size="x-large"></v-icon>
+      </v-list-item-avatar>
       <v-list-item-content>
         <v-list-item-title class="text-h6">
-          {{ mainTitle }}
+          {{ index.title ? index.title : index.value }}
         </v-list-item-title>
-        <v-list-item-subtitle> {{ mainDescription }} </v-list-item-subtitle>
+        <v-list-item-subtitle> {{ index.description }} </v-list-item-subtitle>
       </v-list-item-content>
     </v-list-item>
     <v-divider></v-divider>
     <v-list nav dense>
       <v-list-item
-        v-for="(item, i) in items"
+        v-for="(item, i) in index.data"
         :key="i"
         :value="item"
         active-color="primary"
       >
         <v-list-item-avatar>
-          <v-icon :icon="item.icon"></v-icon>
+          <v-icon :icon="`mdi-${item.icon}`" size="large"></v-icon>
         </v-list-item-avatar>
-
-        <v-list-item-title v-text="item.text"></v-list-item-title>
+        <v-list-item-title>{{
+          item.title ? item.title : item.value
+        }}</v-list-item-title>
       </v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
-
 <script>
 import { storeToRefs } from "pinia";
 import defineStore from "../stores/core.js";
 export default {
   setup() {
     const core = defineStore();
-    const { mainTitle, mainDescription } = storeToRefs(core);
-    return { mainTitle, mainDescription };
+    const { index } = storeToRefs(core);
+    return { index };
   },
   data: () => ({
     selectedItem: 0,
-    items: [
-      { text: "My Files", icon: "mdi-folder" },
-      { text: "Shared with me", icon: "mdi-account-multiple" },
-      { text: "Starred", icon: "mdi-star" },
-      { text: "Recent", icon: "mdi-history" },
-      { text: "Offline", icon: "mdi-check-circle" },
-      { text: "Uploads", icon: "mdi-upload" },
-      { text: "Backups", icon: "mdi-cloud-upload" },
-    ],
   }),
 };
 </script>
