00e04f4d66b4bc1c71ebb7089c8a3298f918f6fb
Jerry Bruwes <jbruwes@icloud.com>
2022-06-09 16:05:53 +0200

    Update VNavigationDrawerK3.vue

--- fel/sources/modules/VNavigationDrawerK3.vue
+++ fel/sources/modules/VNavigationDrawerK3.vue
@@ -1,31 +1,28 @@
 <template>
   <v-navigation-drawer app temporary>
-    <v-list-item>
-      <v-list-item-avatar>
-        <v-icon :icon="`mdi-${index.icon}`" size="x-large"></v-icon>
-      </v-list-item-avatar>
-      <v-list-item-content>
-        <v-list-item-title class="text-h6">
-          {{ index.title ? index.title : index.value }}
-        </v-list-item-title>
-        <v-list-item-subtitle> {{ index.description }} </v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-    <v-divider></v-divider>
-    <v-list nav dense>
+    <v-list>
       <v-list-item
-        v-for="(item, i) in index.data"
-        :key="i"
-        :value="item"
-        active-color="primary"
+        :prepend-icon="`mdi-${index.icon}`"
+        :title="title(index)"
+        :subtitle="index.description"
+        href="/"
       >
-        <v-list-item-avatar>
-          <v-icon :icon="`mdi-${item.icon}`" size="large"></v-icon>
-        </v-list-item-avatar>
-        <v-list-item-title>{{
-          item.title ? item.title : item.value
-        }}</v-list-item-title>
-      </v-list-item>
+      </v-list-item
+    ></v-list>
+    <v-divider></v-divider>
+    <v-list nav>
+      <template v-for="(item, i) in index.data" :key="i" :value="item">
+        <v-list-item
+          v-if="item.visible"
+          :prepend-icon="`mdi-${item.icon}`"
+          :title="title(item)"
+          :subtitle="item.description"
+          :href="href(item)"
+          :active="href(item) === decodeURI(window.location.pathname)"
+          active-color="primary"
+        >
+        </v-list-item
+      ></template>
     </v-list>
   </v-navigation-drawer>
 </template>
@@ -41,5 +38,9 @@ export default {
   data: () => ({
     selectedItem: 0,
   }),
+  methods: {
+    href: (item) => (item.href ? item.href : item.path),
+    title: (item) => (item.title ? item.title : item.value),
+  },
 };
 </script>
