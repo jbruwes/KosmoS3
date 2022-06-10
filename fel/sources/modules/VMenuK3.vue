cf1eee5160ee95a84144c492a597a5c066a751c9
Jerry Bruwes <jbruwes@icloud.com>
2022-06-10 13:58:29 +0200

    Create VMenuK3.vue

---
+++ fel/sources/modules/VMenuK3.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-menu close-on-click location="start">
+    <template v-slot:activator="{ props }">
+      <v-btn icon="mdi-dots-vertical" v-bind="props"> </v-btn>
+    </template>
+    <v-list nav>
+      <template v-for="(item, i) in items" :key="i" :value="item">
+        <v-list-subheader
+          v-if="item.visible"
+          :prepend-icon="`mdi-${item.icon}`"
+          :title="title(item)"
+        >
+        </v-list-subheader></template
+    ></v-list>
+  </v-menu>
+</template>
+<script>
+import { storeToRefs } from "pinia";
+import defineStore from "../stores/core.js";
+export default {
+  setup() {
+    const core = defineStore();
+    const { index, siblings, routePath } = storeToRefs(core);
+    const { href, title } = core;
+    return { index, siblings, routePath, href, title };
+  },
+  computed: {
+    items() {
+      return !this.routePath || this.routePath === "/"
+        ? this.index.data
+        : this.siblings;
+    },
+  },
+};
+</script>
