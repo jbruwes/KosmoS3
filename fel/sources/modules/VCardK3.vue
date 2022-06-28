d24fe2e1f5a43afe0129c81c24b97b8961b51d27
Jerry Bruwes <jbruwes@icloud.com>
2022-06-28 16:24:26 +0200

    Create VCardK3.vue

---
+++ fel/sources/modules/VCardK3.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-card
+    width="400"
+    title="This is a title"
+    subtitle="This is a subtitle"
+    text="This is content"
+    prepend-icon="mdi-home"
+  >
+  </v-card>
+</template>
