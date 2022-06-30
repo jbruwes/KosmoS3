d46013f6a0b77acf99da7c4e4d7075e0d0ca02ee
Jerry Bruwes <jbruwes@icloud.com>
2022-06-30 17:26:29 +0200

    Update VCardK3.vue

--- fel/sources/modules/VCardK3.vue
+++ fel/sources/modules/VCardK3.vue
@@ -1,10 +1,33 @@
 <template>
-  <v-card
-    width="400"
-    title="This is a title"
-    subtitle="This is a subtitle"
-    text="This is content"
-    prepend-icon="mdi-home"
-  >
-  </v-card>
+  <v-hover v-slot="{ isHovering, props }">
+    <v-card v-bind="props" width="290px">
+      <v-img :src="img"></v-img>
+      <v-card-title>{{ title }}</v-card-title>
+      <v-card-subtitle>{{ subtitle }}</v-card-subtitle>
+      <v-card-text>{{ text }}</v-card-text>
+      <v-overlay
+        :model-value="isHovering"
+        contained
+        class="align-center justify-center"
+      >
+        <v-btn
+          variant="outlined"
+          size="x-large"
+          color="white"
+          :icon="icon"
+        ></v-btn>
+      </v-overlay>
+    </v-card>
+  </v-hover>
 </template>
+<script>
+export default {
+  props: {
+    title: String,
+    subtitle: String,
+    text: String,
+    icon: String,
+    img: String,
+  },
+};
+</script>
