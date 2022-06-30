787ed5e4729075d929869c12bac936d593e62ebf
Jerry Bruwes <jbruwes@icloud.com>
2022-06-30 17:26:13 +0200

    Update content.js

--- cms/sources/views/content.js
+++ cms/sources/views/content.js
@@ -168,7 +168,7 @@ export default class ContentView extends JetView {
           ],
           CUSTOM_ELEMENT_HANDLING: {
             tagNameCheck: /^v-/,
-            attributeNameCheck: null,
+            attributeNameCheck: /\w+/,
             allowCustomizedBuiltInElements: true,
           },
         })
