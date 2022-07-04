a94ebb90ed5e7db8b1899f0ef8dd853a1e977ae6
Jerry Bruwes <jbruwes@icloud.com>
2022-07-04 16:08:28 +0200

    Create VCardItemK3.vue

---
+++ fel/sources/modules/VCardItemK3.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card-k3
+    :title="item ? getTitle(item) : ''"
+    :subtitle="
+      item
+        ? new Date(item.date ? item.date : item.lastmod).toLocaleDateString()
+        : ''
+    "
+    :text="item ? item.description : ''"
+    :icon="item ? `mdi-${item.icon}` : ''"
+    :img="item ? item.image : ''"
+    :href="item ? getPath(item) : ''"
+  ></v-card-k3>
+</template>
+<script>
+import { mapActions } from "pinia";
+import core from "../stores/core.js";
+import VCardK3 from "./VCardK3.vue";
+export default {
+  props: {
+    item: Object,
+  },
+  components: {
+    VCardK3,
+  },
+  methods: {
+    ...mapActions(core, ["getTitle", "getPath"]),
+  },
+};
+</script>
