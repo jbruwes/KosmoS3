5b88dc18fc69e5c35621d9e578278984f3b2ca76
Jerry Bruwes <jbruwes@icloud.com>
2022-07-06 14:19:51 +0200

    Update VCardItemK3.vue

--- fel/sources/modules/VCardItemK3.vue
+++ fel/sources/modules/VCardItemK3.vue
@@ -1,15 +1,25 @@
 <template>
   <v-card-k3
-    :title="item ? getTitle(item) : ''"
-    :subtitle="
-      item
-        ? new Date(item.date ? item.date : item.lastmod).toLocaleDateString()
-        : ''
+    :title="
+      JSON.parse(titleOff) ? undefined : item ? getTitle(item) : undefined
     "
-    :text="item ? item.description : ''"
-    :icon="item ? `mdi-${item.icon}` : ''"
-    :img="item ? item.image : ''"
-    :href="href"
+    :subtitle="
+      JSON.parse(dateOff)
+        ? undefined
+        : item
+        ? new Date(item.date ? item.date : item.lastmod).toLocaleDateString()
+        : undefined
+    "
+    :text="
+      JSON.parse(descriptionOff)
+        ? undefined
+        : item
+        ? item.description
+        : undefined
+    "
+    :icon="item ? `mdi-${item.icon}` : 'mdi-open-in-new'"
+    :image="JSON.parse(imageOff) ? undefined : item ? item.image : undefined"
+    :href="JSON.parse(hrefOff) ? undefined : href"
   ></v-card-k3>
 </template>
 <script>
@@ -17,7 +27,14 @@ import { mapState, mapActions } from "pinia";
 import core from "../stores/core.js";
 import VCardK3 from "./VCardK3.vue";
 export default {
-  props: { item: Object },
+  props: {
+    item: Object,
+    dateOff: Boolean,
+    titleOff: Boolean,
+    imageOff: Boolean,
+    hrefOff: Boolean,
+    descriptionOff: Boolean,
+  },
   components: { VCardK3 },
   computed: {
     ...mapState(core, ["routePath"]),
