2c2c2b77fd2bb64fa0e803b89e0e87606a0beded
Jerry Bruwes <jbruwes@icloud.com>
2022-07-08 15:35:58 +0200

    Update VCardItemK3.vue

--- fel/sources/modules/VCardItemK3.vue
+++ fel/sources/modules/VCardItemK3.vue
@@ -1,26 +1,46 @@
 <template>
   <v-card-k3
     :animate="animate"
+    :class="class"
+    :width="width"
     :title="
-      JSON.parse(titleOff) ? undefined : item ? getTitle(item) : undefined
+      title
+        ? title
+        : JSON.parse(titleOff)
+        ? undefined
+        : item
+        ? getTitle(item)
+        : undefined
     "
     :subtitle="
-      JSON.parse(dateOff)
+      date
+        ? date
+        : JSON.parse(dateOff)
         ? undefined
         : item
         ? new Date(item.date ? item.date : item.lastmod).toLocaleDateString()
         : undefined
     "
     :text="
-      JSON.parse(descriptionOff)
+      description
+        ? description
+        : JSON.parse(descriptionOff)
         ? undefined
         : item
         ? item.description
         : undefined
     "
-    :icon="item ? `mdi-${item.icon}` : 'mdi-open-in-new'"
-    :image="JSON.parse(imageOff) ? undefined : item ? item.image : undefined"
-    :href="JSON.parse(hrefOff) ? undefined : href"
+    :icon="icon ? icon : item ? `mdi-${item.icon}` : undefined"
+    :image="
+      image
+        ? image
+        : JSON.parse(imageOff)
+        ? undefined
+        : item
+        ? item.image
+        : undefined
+    "
+    :href="href ? href : JSON.parse(hrefOff) ? undefined : url"
   ></v-card-k3>
 </template>
 <script>
@@ -31,16 +51,24 @@ export default {
   props: {
     item: Object,
     animate: String,
+    class: String,
+    width: Number,
+    icon: String,
+    date: String,
     dateOff: Boolean,
+    title: String,
     titleOff: Boolean,
+    image: String,
     imageOff: Boolean,
+    href: String,
     hrefOff: Boolean,
+    description: String,
     descriptionOff: Boolean,
   },
   components: { VCardK3 },
   computed: {
     ...mapState(core, ["routePath"]),
-    href() {
+    url() {
       let href = this.item ? this.getPath(this.item) : "";
       return href === this.routePath ? "" : href;
     },
