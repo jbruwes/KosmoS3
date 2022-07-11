9943188a50d1c7632baed408f133f4b34ab10cd8
Jerry Bruwes <jbruwes@icloud.com>
2022-07-11 21:05:03 +0200

    Update VCardItemK3.vue

--- fel/sources/modules/VCardItemK3.vue
+++ fel/sources/modules/VCardItemK3.vue
@@ -54,7 +54,7 @@ export default {
   computed: {
     ...mapState(core, ["routePath"]),
     url() {
-      const href = this.item ? this.getPath(this.item) : "";
+      const href = this.getPath(this.item);
       return href === this.routePath ? "" : href;
     },
   },
