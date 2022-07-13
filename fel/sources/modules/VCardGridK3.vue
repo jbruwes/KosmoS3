b80e3b6de6c128907d9202aab6db428d7e19a601
Jerry Bruwes <jbruwes@icloud.com>
2022-07-13 16:25:44 +0200

    Update VCardGridK3.vue

--- fel/sources/modules/VCardGridK3.vue
+++ fel/sources/modules/VCardGridK3.vue
@@ -7,7 +7,7 @@
             :item="item"
             :animate="animate"
             :class="class"
-            :width="width || '100%'"
+            :width="width"
             :icon="icon"
             :date="date"
             :title="title"
@@ -16,25 +16,6 @@
             :description="description"
           ></v-card-item-k3></v-sheet></v-col></v-row
   ></v-container>
-
-  <!--v-container>
-    <v-row v-for="chunk in itemChunks" no-gutters>
-      <v-col v-for="item in chunk" cols="12">
-        <v-card-item-k3
-          :item="item"
-          :animate="animate"
-          :class="class"
-          :width="width"
-          :icon="icon"
-          :date="date"
-          :title="title"
-          :image="image"
-          :href="href"
-          :description="description"
-        ></v-card-item-k3>
-      </v-col>
-    </v-row>
-  </v-container-->
 </template>
 <script>
 import _ from "lodash";
@@ -43,24 +24,38 @@ import core from "../stores/core.js";
 import VCardItemK3 from "./VCardItemK3.vue";
 export default {
   props: {
-    path: String,
     animate: String,
     class: String,
-    width: Number,
+    width: { default: "100%", type: Number },
     icon: String,
     date: String,
     title: String,
     image: String,
     href: String,
     description: String,
+    cols: { default: 4, type: Number },
+    deep: Boolean,
+    length: Number,
+    reveal: Boolean,
+    sort: String,
+    path: String,
+    children: Boolean,
   },
   components: { VCardItemK3 },
   computed: {
     items() {
-      return this.getItems(null, null, null, null, this.path, null, "*");
+      return this.getItems(
+        this.deep,
+        this.length,
+        this.reveal,
+        this.sort,
+        this.path,
+        this.children,
+        "*[@id]"
+      );
     },
     itemChunks() {
-      return _.chunk(Object.values(this.items), 4);
+      return _.chunk(Object.values(this.items), this.cols);
     },
   },
   methods: { ...mapActions(core, ["getItems"]) },
