a9e61d7a4a1723abeb904ec09ded8c2cd8132e3f
Jerry Bruwes <jbruwes@icloud.com>
2022-07-18 19:45:57 +0200

    Update VCardK3.vue

--- fel/sources/modules/VCardK3.vue
+++ fel/sources/modules/VCardK3.vue
@@ -4,13 +4,28 @@
       v-animate-onscroll.repeat="animate"
       v-bind="props"
       :width="width"
+      :height="height"
       :class="class"
     >
-      <v-img :src="image"></v-img>
+      <v-img :src="image" :aspect-ratio="16 / 9" cover
+        ><v-expand-transition
+          ><div
+            v-if="isHovering"
+            class="d-flex transition-fast-in-fast-out align-center justify-center v-overlay__scrim v-overlay--absolute fill-height"
+          >
+            <v-btn
+              variant="outlined"
+              size="x-large"
+              color="white"
+              :icon="`mdi-${icon}`"
+              :href="href"
+            ></v-btn></div></v-expand-transition
+      ></v-img>
       <v-card-title>{{ title }}</v-card-title>
       <v-card-subtitle>{{ subtitle }}</v-card-subtitle>
       <v-card-text>{{ text }}</v-card-text>
-      <v-overlay
+
+      <!--v-overlay
         :model-value="isHovering"
         contained
         class="align-center justify-center"
@@ -22,7 +37,7 @@
           :icon="`mdi-${icon}`"
           :href="href"
         ></v-btn>
-      </v-overlay>
+      </v-overlay-->
     </v-card>
   </v-hover>
 </template>
@@ -31,7 +46,8 @@ export default {
   props: {
     animate: { default: "animate__animated animate__flipInY", type: String },
     class: String,
-    width: { default: 290, type: Number },
+    width: { default: 275.99, type: Number },
+    height: Number,
     title: String,
     subtitle: String,
     text: String,
