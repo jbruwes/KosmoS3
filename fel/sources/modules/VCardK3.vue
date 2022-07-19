6f6677a7f2b108812de8df3f0c051ceb388969d8
Jerry Bruwes <jbruwes@icloud.com>
2022-07-19 16:16:36 +0200

    Update VCardK3.vue

--- fel/sources/modules/VCardK3.vue
+++ fel/sources/modules/VCardK3.vue
@@ -24,7 +24,6 @@
       <v-card-title>{{ title }}</v-card-title>
       <v-card-subtitle>{{ subtitle }}</v-card-subtitle>
       <v-card-text>{{ text }}</v-card-text>
-
       <!--v-overlay
         :model-value="isHovering"
         contained
