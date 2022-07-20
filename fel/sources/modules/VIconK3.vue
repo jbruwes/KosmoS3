cb51117f70501cb621ff35bad7717ee5a90d2dbf
Jerry Bruwes <jbruwes@icloud.com>
2022-07-20 16:13:47 +0200

    Update VIconK3.vue

--- fel/sources/modules/VIconK3.vue
+++ fel/sources/modules/VIconK3.vue
@@ -1,20 +1,35 @@
 <template>
-  <v-btn icon :stacked="true">
-    <v-icon> mdi-tools </v-icon>
-    <span> Tools </span>
-  </v-btn>
-  <!--v-hover v-slot="{ isHovering, props }">
+  <v-hover v-slot="{ isHovering, props }">
     <v-card
+      variant="plain"
       v-animate-onscroll.repeat="animate"
       v-bind="props"
       :width="width"
+      :height="height"
       :class="class"
+      :href="href"
     >
-      <v-img :src="image"></v-img>
-      <v-card-title>{{ title }}</v-card-title>
-      <v-card-subtitle>{{ subtitle }}</v-card-subtitle>
-      <v-card-text>{{ text }}</v-card-text>
-      <v-overlay
+      <v-img :src="image" :aspect-ratio="16 / 9" cover
+        ><v-expand-transition
+          ><div
+            v-if="isHovering"
+            class="d-flex transition-fast-in-fast-out align-center justify-center v-overlay__scrim v-overlay--absolute fill-height"
+          >
+            <v-btn
+              variant="outlined"
+              size="x-large"
+              color="white"
+              :icon="`mdi-${icon}`"
+              :href="href"
+            ></v-btn></div></v-expand-transition
+      ></v-img>
+      <v-card-item
+        :title="title"
+        :subtitle="subtitle"
+        :prependIcon="`mdi-${icon}`"
+      ></v-card-item>
+      <v-card-text v-if="text">{{ text }}</v-card-text>
+      <!--v-overlay
         :model-value="isHovering"
         contained
         class="align-center justify-center"
@@ -26,9 +41,9 @@
           :icon="`mdi-${icon}`"
           :href="href"
         ></v-btn>
-      </v-overlay>
+      </v-overlay-->
     </v-card>
-  </v-hover-->
+  </v-hover>
 </template>
 <script>
 export default {
