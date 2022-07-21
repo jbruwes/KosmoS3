fb8fcdde5398e9bd7e5654b70752f726cf35c819
Jerry Bruwes <jbruwes@icloud.com>
2022-07-21 16:08:47 +0200

    Create VImageCardSingleK3.vue

---
+++ fel/sources/modules/VImageCardSingleK3.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-card-single-k3 type="image"></v-card-single-k3>
+</template>
+<script>
+import VCardSingleK3 from "./VCardSingleK3.vue";
+export default {
+  components: { VCardSingleK3 },
+};
+</script>
