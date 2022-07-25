8385e4fc5968cd7699ab09ba918f79cfe42b2dcd
Jerry Bruwes <jbruwes@icloud.com>
2022-07-25 16:19:16 +0200

    Update VCardSingleK3.vue

--- fel/sources/modules/VCardSingleK3.vue
+++ fel/sources/modules/VCardSingleK3.vue
@@ -1,41 +1,9 @@
 <template>
-  <v-card-item-k3
-    :item="item"
-    :animate="animate"
-    :class="class"
-    :width="width"
-    :icon="icon"
-    :date="date"
-    :title="title"
-    :image="image"
-    :href="href"
-    :description="description"
-  ></v-card-item-k3>
+  <v-item-k3 type="image"></v-item-k3>
 </template>
 <script>
-import { mapActions } from "pinia";
-import core from "../stores/core.js";
-import VCardItemK3 from "./VCardItemK3.vue";
+import VItemK3 from "./VItemK3.vue";
 export default {
-  props: {
-    path: String,
-    animate: String,
-    class: String,
-    width: Number,
-    icon: String,
-    date: String,
-    title: String,
-    image: String,
-    href: String,
-    description: String,
-  },
-  components: { VCardItemK3 },
-  computed: {
-    item() {
-      const items = this.getItems(null, null, null, null, this.path);
-      return items.length ? items[0] : undefined;
-    },
-  },
-  methods: { ...mapActions(core, ["getItems"]) },
+  components: { VItemK3 },
 };
 </script>
