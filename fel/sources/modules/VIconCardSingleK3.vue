31a3ad8c1c990de9ee8a828a28bfb1439f92482d
Jerry Bruwes <jbruwes@icloud.com>
2022-07-25 16:19:29 +0200

    Delete VIconCardSingleK3.vue

--- fel/sources/modules/VIconCardSingleK3.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-card-single-k3 type="icon"></v-card-single-k3>
-</template>
-<script>
-import VCardSingleK3 from "./VCardSingleK3.vue";
-export default {
-  components: { VCardSingleK3 },
-};
-</script>
