44ef93a7ebef11924b94268914c12c2640910586
Jerry Bruwes <jbruwes@icloud.com>
2022-07-27 16:06:21 +0200

    Create VSlideK3.vue

---
+++ fel/sources/modules/VSlideK3.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-slide-group show-arrows>
+    <v-slide-group-item v-for="item in items">
+      <v-item-k3
+        :item="item"
+        :animate="animate"
+        :class="class"
+        :width="width"
+        :height="height"
+        :icon="icon"
+        :date="date"
+        :title="title"
+        :image="image"
+        :href="href"
+        :description="description"
+        :type="type"
+      ></v-item-k3>
+    </v-slide-group-item>
+  </v-slide-group>
+</template>
+<script>
+import { mapActions } from "pinia";
+import core from "../stores/core.js";
+import VItemK3 from "./VItemK3.vue";
+export default {
+  props: {
+    animate: String,
+    class: { default: "ma-4", type: String },
+    width: Number,
+    height: Number,
+    icon: String,
+    date: String,
+    title: String,
+    image: String,
+    href: String,
+    description: String,
+    deep: Boolean,
+    length: Number,
+    reveal: Boolean,
+    sort: String,
+    path: String,
+    children: Boolean,
+    type: String,
+  },
+  components: { VItemK3 },
+  computed: {
+    items() {
+      return this.getItems(
+        this.deep,
+        this.length,
+        this.reveal,
+        this.sort,
+        this.path,
+        this.children,
+        "*[@id]"
+      );
+    },
+  },
+  methods: { ...mapActions(core, ["getItems"]) },
+};
+</script>
