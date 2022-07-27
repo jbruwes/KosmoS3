c8b842e2680f9a3d8048d652f5ef01ac28f37dac
Jerry Bruwes <jbruwes@icloud.com>
2022-07-27 16:05:52 +0200

    Update VCardSlideK3.vue

--- fel/sources/modules/VCardSlideK3.vue
+++ fel/sources/modules/VCardSlideK3.vue
@@ -1,57 +1,9 @@
 <template>
-  <v-slide-group show-arrows>
-    <v-slide-group-item v-for="item in items">
-      <v-item-k3
-        :item="item"
-        :animate="animate"
-        :class="class"
-        :width="width"
-        :icon="icon"
-        :date="date"
-        :title="title"
-        :image="image"
-        :href="href"
-        :description="description"
-      ></v-item-k3>
-    </v-slide-group-item>
-  </v-slide-group>
+  <v-slide-k3 type="card"></v-slide-k3>
 </template>
 <script>
-import { mapActions } from "pinia";
-import core from "../stores/core.js";
-import VItemK3 from "./VItemK3.vue";
+import VSlideK3 from "./VSlideK3.vue";
 export default {
-  props: {
-    animate: String,
-    class: { default: "ma-4", type: String },
-    width: Number,
-    icon: String,
-    date: String,
-    title: String,
-    image: String,
-    href: String,
-    description: String,
-    deep: Boolean,
-    length: Number,
-    reveal: Boolean,
-    sort: String,
-    path: String,
-    children: Boolean,
-  },
-  components: { VItemK3 },
-  computed: {
-    items() {
-      return this.getItems(
-        this.deep,
-        this.length,
-        this.reveal,
-        this.sort,
-        this.path,
-        this.children,
-        "*[@id]"
-      );
-    },
-  },
-  methods: { ...mapActions(core, ["getItems"]) },
+  components: { VSlideK3 },
 };
 </script>
