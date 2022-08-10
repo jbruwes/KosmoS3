c61c1c9969ea4a1280537601508007ea3d78117c
Jerry Bruwes <jbruwes@icloud.com>
2022-08-10 17:14:01 +0200

    Update init.js

--- cms/sources/workers/init.js
+++ cms/sources/workers/init.js
@@ -62,8 +62,8 @@ onmessage = async ({
       io.putObject(
         "index.htm",
         "text/html",
-        // '<div class="v-container py-0 position-static" style="z-index:1"><div id="content" style="margin:0px;flex:1 1 auto"><article v-if="!data"></article><article v-else v-html="data"></article></div></div>'
-        '<div class="v-container py-0 position-static" style="z-index:1"><div id="content" style="margin:0px;flex:1 1 auto"><article v-if="!data"></article><article v-else><v-runtime-template :parent="this" :template="data"></v-runtime-template></article></div></div>'
+        // '<div class="v-container py-0 position-static" style="z-index:1"><div id="content" style="margin:0px;flex:1 1 auto"><article v-if="!template"></article><article v-else v-html="template"></article></div></div>'
+        '<div class="v-container py-0 position-static" style="z-index:1"><div id="content" style="margin:0px;flex:1 1 auto"><article v-if="!template"></article><article v-else><v-runtime-template :parent="this" :template="template"></v-runtime-template></article></div></div>'
       )
     );
   if (put.length)
