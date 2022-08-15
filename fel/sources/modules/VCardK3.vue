6f89a785f95c0c67ed58e12692a4faa731e94dd5
Jerry Bruwes <jbruwes@icloud.com>
2022-08-15 17:45:26 +0200

    Update VCardK3.vue

--- fel/sources/modules/VCardK3.vue
+++ fel/sources/modules/VCardK3.vue
@@ -1,9 +1,25 @@
 <template>
-  <v-item-k3 type="card"></v-item-k3>
+  <v-item-k3 #="slot"
+    ><v-card-template-k3
+      :animate="slot.animate"
+      :class="slot.class"
+      :width="slot.width"
+      :height="slot.height"
+      :title="slot.title"
+      :icon="slot.icon"
+      :image="slot.image"
+      :href="slot.href"
+      :path="slot.path"
+      :item="slot.item"
+      :date="slot.date"
+      :description="slot.description"
+    ></v-card-template-k3
+  ></v-item-k3>
 </template>
 <script>
 import VItemK3 from "./VItemK3.vue";
+import VCardTemplateK3 from "./VCardTemplateK3.vue";
 export default {
-  components: { VItemK3 },
+  components: { VItemK3, VCardTemplateK3 },
 };
 </script>
