e66b0226e00e62ef28f87f276ae6d39322a8f5e2
Jerry Bruwes <jbruwes@icloud.com>
2022-08-17 16:54:46 +0200

    Update VCardSlideK3.vue

--- fel/sources/modules/VCardSlideK3.vue
+++ fel/sources/modules/VCardSlideK3.vue
@@ -1,9 +1,16 @@
 <template>
-  <v-slide-k3 type="card"></v-slide-k3>
+  <v-slide-k3 #="slot"
+    ><v-card-k3
+      class="ma-4"
+      :animate="slot.animate"
+      :item="slot.item"
+    ></v-card-k3
+  ></v-slide-k3>
 </template>
 <script>
 import VSlideK3 from "./VSlideK3.vue";
+import VCardK3 from "./VCardK3.vue";
 export default {
-  components: { VSlideK3 },
+  components: { VSlideK3, VCardK3 },
 };
 </script>
