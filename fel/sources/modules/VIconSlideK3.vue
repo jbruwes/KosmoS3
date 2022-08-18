7f38905377637422960268f87026e448ab5a5bda
Jerry Bruwes <jbruwes@icloud.com>
2022-08-18 17:14:21 +0200

    Update VIconSlideK3.vue

--- fel/sources/modules/VIconSlideK3.vue
+++ fel/sources/modules/VIconSlideK3.vue
@@ -1,9 +1,17 @@
 <template>
-  <v-slide-k3 type="icon"></v-slide-k3>
+  <v-slide-k3 #="slot"
+    ><v-icon-k3
+      class="ma-4"
+      :animate="slot.animate"
+      :item="slot.item"
+      :width="slot.width"
+    ></v-icon-k3
+  ></v-slide-k3>
 </template>
 <script>
 import VSlideK3 from "./VSlideK3.vue";
+import VIconK3 from "./VIconK3.vue";
 export default {
-  components: { VSlideK3 },
+  components: { VSlideK3, VIconK3 },
 };
 </script>
