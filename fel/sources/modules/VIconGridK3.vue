a56c970c9ef1d2641b1fd31fc2d588d21d7a7600
Jerry Bruwes <jbruwes@icloud.com>
2022-08-18 17:14:14 +0200

    Update VIconGridK3.vue

--- fel/sources/modules/VIconGridK3.vue
+++ fel/sources/modules/VIconGridK3.vue
@@ -1,9 +1,16 @@
 <template>
-  <v-grid-k3 type="icon"></v-grid-k3>
+  <v-grid-k3 #="slot"
+    ><v-icon-k3
+      class="fill-height ma-auto"
+      :animate="slot.animate"
+      :item="slot.item"
+    ></v-icon-k3
+  ></v-grid-k3>
 </template>
 <script>
 import VGridK3 from "./VGridK3.vue";
+import VIconK3 from "./VIconK3.vue";
 export default {
-  components: { VGridK3 },
+  components: { VGridK3, VIconK3 },
 };
 </script>
