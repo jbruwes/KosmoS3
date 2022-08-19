320bde00a4174d40ea629de1fefc9fbcdc206e4c
Jerry Bruwes <jbruwes@icloud.com>
2022-08-19 12:17:43 +0200

    Create VTemplateIconK3.vue

---
+++ fel/sources/modules/VTemplateIconK3.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card
+    v-animate-onscroll.repeat="animate"
+    :height="height"
+    :width="width"
+    :minWidth="minWidth"
+    :minHeight="minHeight"
+    :class="class"
+    :href="href"
+    variant="plain"
+  >
+    <div class="text-center">
+      <v-icon
+        :icon="icon"
+        style="font-size: calc(var(--v-icon-size-multiplier) * 4em);"
+      ></v-icon>
+    </div>
+    <v-card-item :title="title" class="justify-center text-center"
+      ><v-card-subtitle v-if="subtitle"
+        ><v-chip
+          size="x-small"
+          label
+          variant="outlined"
+          prepend-icon="mdi-calendar"
+          :text="subtitle"
+        ></v-chip></v-card-subtitle
+    ></v-card-item>
+    <v-card-text v-if="text" class="text-center">{{ text }}</v-card-text>
+  </v-card>
+</template>
+<script>
+export default {
+  props: {
+    animate: { default: "animate__animated animate__bounce", type: String },
+    class: String,
+    width: Number,
+    height: Number,
+    minWidth: Number,
+    minHeight: Number,
+    title: String,
+    subtitle: String,
+    text: String,
+    icon: String,
+    href: String,
+  },
+};
+</script>
