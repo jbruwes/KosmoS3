d646968f3ba283aac05fb6a8fcbd2192eb1a0b59
Jerry Bruwes <jbruwes@icloud.com>
2022-08-19 12:18:33 +0200

    Create VGridIconK3.vue

---
+++ fel/sources/modules/VGridIconK3.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-grid-k3 #="slot"
+    ><v-single-icon-k3
+      class="fill-height ma-auto"
+      :animate="slot.animate"
+      :item="slot.item"
+    ></v-single-icon-k3
+  ></v-grid-k3>
+</template>
+<script>
+import VGridK3 from "./VGridK3.vue";
+import VSingleIconK3 from "./VSingleIconK3.vue";
+export default {
+  components: { VGridK3, VSingleIconK3 },
+};
+</script>
