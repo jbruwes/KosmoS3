bab9a9470d6b5e4e37970983d953d2982728ba42
Jerry Bruwes <jbruwes@icloud.com>
2022-08-19 12:18:11 +0200

    Create VSingleIconK3.vue

---
+++ fel/sources/modules/VSingleIconK3.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-single-k3 #="slot"
+    ><v-template-icon-k3
+      :animate="slot.animate"
+      :class="slot.class"
+      :width="slot.width"
+      :height="slot.height"
+      :minWidth="slot.minWidth"
+      :minHeight="slot.minHeight"
+      :title="slot.title"
+      :icon="slot.icon"
+      :href="slot.href"
+      :path="slot.path"
+      :item="slot.item"
+      :subtitle="slot.subtitle"
+      :text="slot.text"
+    ></v-template-icon-k3
+  ></v-single-k3>
+</template>
+<script>
+import VSingleK3 from "./VSingleK3.vue";
+import VTemplateIconK3 from "./VTemplateIconK3.vue";
+export default {
+  components: { VSingleK3, VTemplateIconK3 },
+};
+</script>
