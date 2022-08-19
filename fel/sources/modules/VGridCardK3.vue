0c6d8e6bf14235254679fdad9ae8f71577cfc958
Jerry Bruwes <jbruwes@icloud.com>
2022-08-19 12:18:37 +0200

    Create VGridCardK3.vue

---
+++ fel/sources/modules/VGridCardK3.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-grid-k3 #="slot"
+    ><v-single-card-k3
+      class="fill-height ma-auto"
+      :animate="slot.animate"
+      :item="slot.item"
+    ></v-single-card-k3
+  ></v-grid-k3>
+</template>
+<script>
+import VGridK3 from "./VGridK3.vue";
+import VSingleCardK3 from "./VSingleCardK3.vue";
+export default {
+  components: { VGridK3, VSingleCardK3 },
+};
+</script>
