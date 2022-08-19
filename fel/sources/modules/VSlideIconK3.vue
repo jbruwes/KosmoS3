7f585b381796ba840d9e35bc4ee4091a9eb5e416
Jerry Bruwes <jbruwes@icloud.com>
2022-08-19 12:18:03 +0200

    Create VSlideIconK3.vue

---
+++ fel/sources/modules/VSlideIconK3.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-slide-k3 #="slot"
+    ><v-single-icon-k3
+      class="ma-4"
+      :animate="slot.animate"
+      :item="slot.item"
+      :width="slot.width"
+    ></v-single-icon-k3
+  ></v-slide-k3>
+</template>
+<script>
+import VSlideK3 from "./VSlideK3.vue";
+import VSingleIconK3 from "./VSingleIconK3.vue";
+export default {
+  components: { VSlideK3, VSingleIconK3 },
+};
+</script>
