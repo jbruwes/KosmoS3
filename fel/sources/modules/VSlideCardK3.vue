99853c537fe356201fd7c1418ebe6cd24e6a730b
Jerry Bruwes <jbruwes@icloud.com>
2022-08-19 12:18:06 +0200

    Create VSlideCardK3.vue

---
+++ fel/sources/modules/VSlideCardK3.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-slide-k3 #="slot"
+    ><v-single-card-k3
+      class="ma-4"
+      :animate="slot.animate"
+      :item="slot.item"
+      :width="slot.width"
+    ></v-single-card-k3
+  ></v-slide-k3>
+</template>
+<script>
+import VSlideK3 from "./VSlideK3.vue";
+import VSingleCardK3 from "./VSingleCardK3.vue";
+export default {
+  components: { VSlideK3, VSingleCardK3 },
+};
+</script>
