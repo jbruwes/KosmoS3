ab00625797eb36405e50cd424dca5b07f59192a2
Jerry Bruwes <jbruwes@icloud.com>
2022-08-19 14:20:11 +0200

    Create VSingleBannerK3.vue

---
+++ fel/sources/modules/VSingleBannerK3.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-single-k3 #="slot"
+    ><v-template-banner-k3
+      :animate="slot.animate"
+      :class="slot.class"
+      :width="slot.width"
+      :height="slot.height"
+      :title="slot.title"
+      :icon="slot.icon"
+      :image="slot.image"
+      :href="slot.href"
+      :path="slot.path"
+      :item="slot.item"
+      :subtitle="slot.subtitle"
+      :text="slot.text"
+    ></v-template-banner-k3
+  ></v-single-k3>
+</template>
+<script>
+import VSingleK3 from "./VSingleK3.vue";
+import VTemplateBannerK3 from "./VTemplateBannerK3.vue";
+export default {
+  components: { VSingleK3, VTemplateBannerK3 },
+};
+</script>
