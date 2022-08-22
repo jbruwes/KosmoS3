c5fc2cef67ae5b46b1bd810b1fbb223dad1dc87a
Jerry Bruwes <jbruwes@icloud.com>
2022-08-22 16:46:51 +0200

    Update VSingleK3.vue

--- fel/sources/modules/VSingleK3.vue
+++ fel/sources/modules/VSingleK3.vue
@@ -9,14 +9,14 @@
     :title="
       typeof this.title === 'string'
         ? this.title
-        : this.title === undefined || !!this.title
+        : this.title
         ? this.getTitle(this.theItem)
         : undefined
     "
     :subtitle="
       typeof this.date === 'string'
         ? this.date
-        : (this.date === undefined || !!this.date) &&
+        : this.date &&
           !isNaN(new Date(this.theItem.date || this.theItem.lastmod))
         ? new Date(
             this.theItem.date || this.theItem.lastmod
@@ -26,28 +26,28 @@
     :text="
       typeof this.description === 'string'
         ? this.description
-        : this.description === undefined || !!this.description
+        : this.description
         ? this.theItem.description
         : undefined
     "
     :icon="
       typeof this.icon === 'string'
         ? `mdi-${this.icon}`
-        : this.icon === undefined || !!this.icon
+        : this.icon
         ? `mdi-${this.theItem.icon || 'open-in-new'}`
         : undefined
     "
     :image="
       typeof this.image === 'string'
         ? this.image
-        : this.image === undefined || !!this.image
+        : this.image
         ? this.theItem.image
         : undefined
     "
     :href="
       typeof this.href === 'string'
         ? this.href
-        : this.href === undefined || !!this.href
+        : this.href
         ? this.url
         : undefined
     "
@@ -66,14 +66,14 @@ export default {
     height: Number,
     minWidth: Number,
     minHeight: Number,
-    title: String,
-    icon: String,
-    image: String,
-    href: String,
+    title: { default: true, type: String },
+    icon: { default: true, type: String },
+    image: { default: true, type: String },
+    href: { default: true, type: String },
     path: String,
-    item: Object,
-    date: String,
-    description: String,
+    item: { default: true, type: Object },
+    date: { default: true, type: String },
+    description: { default: true, type: String },
   },
   computed: {
     ...mapState(core, ["routePath"]),
@@ -84,7 +84,7 @@ export default {
     theItem() {
       return typeof this.item === "object"
         ? this.item
-        : this.item === undefined || !!this.item
+        : this.item
         ? this.getItems(null, null, null, null, this.path)[0]
         : undefined;
     },
