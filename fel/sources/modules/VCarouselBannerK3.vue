648a7a38e124850bbf5a44286ccc4cbc6f1e35b5
Jerry Bruwes <jbruwes@icloud.com>
2022-08-22 16:46:38 +0200

    Create VCarouselBannerK3.vue

---
+++ fel/sources/modules/VCarouselBannerK3.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-carousel-k3 #="slot"
+    ><v-single-banner-k3
+      :animate="slot.animate"
+      :item="slot.item"
+    ></v-single-banner-k3
+  ></v-carousel-k3>
+</template>
+<script>
+import VCarouselK3 from "./VCarouselK3.vue";
+import VSingleBannerK3 from "./VSingleBannerK3.vue";
+export default {
+  components: { VCarouselK3, VSingleBannerK3 },
+};
+</script>
