e5ea879c0aab679c83cf37e7562958414cf87b0c
Jerry Bruwes <jbruwes@icloud.com>
2022-08-25 16:36:50 +0200

    Create VListItemK3.vue

---
+++ fel/sources/modules/VListItemK3.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-list-k3 #="slot"
+    ><v-single-item-k3
+      class="ma-4"
+      :animate="slot.animate"
+      :item="slot.item"
+      :width="slot.width"
+    ></v-single-item-k3
+  ></v-list-k3>
+</template>
+<script>
+import VListK3 from "./VListK3.vue";
+import VSingleItemK3 from "./VSingleItemK3.vue";
+export default {
+  components: { VListK3, VSingleItemK3 },
+};
+</script>
