cef8d2ef9c9a367e60e51b25817c0b3220aa5809
Jerry Bruwes <jbruwes@icloud.com>
2022-08-25 16:36:57 +0200

    Create VSingleItemK3.vue

---
+++ fel/sources/modules/VSingleItemK3.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-single-k3 #="slot"
+    ><v-template-item-k3
+      :animate="slot.animate"
+      :class="slot.class"
+      :width="slot.width"
+      :height="slot.height"
+      :title="slot.title"
+      :icon="slot.icon"
+      :href="slot.href"
+      :path="slot.path"
+      :item="slot.item"
+      :subtitle="slot.subtitle"
+      :text="slot.text"
+    ></v-template-item-k3
+  ></v-single-k3>
+</template>
+<script>
+import VSingleK3 from "./VSingleK3.vue";
+import VTemplateItemK3 from "./VTemplateItemK3.vue";
+export default {
+  components: { VSingleK3, VTemplateItemK3 },
+};
+</script>
