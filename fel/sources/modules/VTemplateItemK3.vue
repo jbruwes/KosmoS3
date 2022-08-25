51e961842eeeb5e1a6e88312e35bdfe9d4b6c969
Jerry Bruwes <jbruwes@icloud.com>
2022-08-25 16:37:08 +0200

    Create VTemplateItemK3.vue

---
+++ fel/sources/modules/VTemplateItemK3.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-list-item
+    v-animate-onscroll.repeat="animate"
+    :height="height"
+    :width="width"
+    :class="class"
+    :href="href"
+    :prepend-icon="icon"
+    :title="title"
+    :subtitle="text"
+    ><v-chip
+      v-if="subtitle"
+      size="x-small"
+      label
+      variant="outlined"
+      prepend-icon="mdi-calendar"
+      :text="subtitle"
+      class="mr-1"
+    ></v-chip>
+  </v-list-item>
+</template>
+<script>
+export default {
+  props: {
+    animate: { default: "animate__animated animate__bounce", type: String },
+    class: String,
+    width: Number,
+    height: Number,
+    title: String,
+    subtitle: String,
+    text: String,
+    icon: String,
+    href: String,
+  },
+};
+</script>
