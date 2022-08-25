00303ff533e6e8a7e2793b682eb7281e86a2c92e
Jerry Bruwes <jbruwes@icloud.com>
2022-08-25 16:36:54 +0200

    Create VListK3.vue

---
+++ fel/sources/modules/VListK3.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-list>
+    <template v-for="item in items">
+      <slot :item="item" :animate="animate" :height="height"></slot>
+    </template>
+  </v-list>
+</template>
+<script>
+import { mapActions } from "pinia";
+import core from "../stores/core.js";
+import VSingleK3 from "./VSingleK3.vue";
+export default {
+  props: {
+    animate: String,
+    height: Number,
+    deep: Boolean,
+    length: Number,
+    reveal: Boolean,
+    sort: String,
+    path: String,
+    children: Boolean,
+  },
+  components: { VSingleK3 },
+  computed: {
+    items() {
+      return this.getItems(
+        this.deep,
+        this.length,
+        this.reveal,
+        this.sort,
+        this.path,
+        this.children,
+        "*[@id]"
+      );
+    },
+  },
+  methods: { ...mapActions(core, ["getItems"]) },
+};
+</script>
