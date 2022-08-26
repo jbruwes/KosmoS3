9c2c1c7bfb3d0e49999a8426c16e4dc637f4d006
Jerry Bruwes <jbruwes@icloud.com>
2022-08-26 14:09:52 +0200

    Create VSingleButtonK3.vue

---
+++ fel/sources/modules/VSingleButtonK3.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-single-k3 #="slot"
+    ><v-template-button-k3
+      :animate="slot.animate"
+      :class="slot.class"
+      :width="slot.width"
+      :height="slot.height"
+      :title="slot.title"
+      :icon="slot.icon"
+      :href="slot.href"
+      :path="slot.path"
+      :item="slot.item"
+      :variant="slot.variant"
+    ></v-template-button-k3
+  ></v-single-k3>
+</template>
+<script>
+import VSingleK3 from "./VSingleK3.vue";
+import VTemplateButtonK3 from "./VTemplateButtonK3.vue";
+export default {
+  components: { VSingleK3, VTemplateButtonK3 },
+};
+</script>
