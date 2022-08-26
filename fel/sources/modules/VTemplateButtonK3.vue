e71c1f4a1ef97ba7146ee4bd0dd389881960bd39
Jerry Bruwes <jbruwes@icloud.com>
2022-08-26 14:07:18 +0200

    Create VTemplateButtonK3.vue

---
+++ fel/sources/modules/VTemplateButtonK3.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-btn
+    class="text-none"
+    :variant="variant"
+    v-animate-onscroll.repeat="animate"
+    :height="height"
+    :width="width"
+    :class="class"
+    :href="href"
+    :prepend-icon="icon"
+  >{{ title }}</v-btn>
+</template>
+<script>
+export default {
+  props: {
+    animate: { default: "animate__animated animate__bounce", type: String },
+    class: String,
+    width: Number,
+    height: Number,
+    title: String,
+    icon: String,
+    href: String,
+    variant: { default: "flat", type: String },
+  },
+};
+</script>
