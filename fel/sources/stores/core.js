1b892b388c6063fd56067a77629f33ccbeae8a0d
Jerry Bruwes <jbruwes@icloud.com>
2022-08-29 17:11:06 +0200

    Update core.js

--- fel/sources/stores/core.js
+++ fel/sources/stores/core.js
@@ -1,382 +1,235 @@
+import { ref, computed } from "vue";
 import { defineStore } from "pinia";
+import { useFetch, get, set } from "@vueuse/core";
 import jsel from "jsel";
 
-export default defineStore("core", {
+export default defineStore("core", () => {
+  const tree = ref({});
+  const context = ref({});
+  const routePath = computed(() => get(context).routePath);
   /**
-   * @returns {object} Переменные состояния
+   * Вычисление вектора
+   *
+   * @param {object} item Объект вычисления
+   * @returns {object[]} Вектор
    */
-  state: () => ({
-    tree: Object,
-    context: Object,
-  }),
-  getters: {
-    /**
-     * Плоский индекс
-     *
-     * @returns {object[]} Плоский индекс для поиска
-     */
-    list() {
-      return jsel(this.tree)
-        .selectAll("//*[@id]")
-        .map((node) => {
-          const lNode = node;
-          lNode.path = this.getVector(lNode).map((e) =>
-            e.value.trim().replace(/\s/g, "_").replace(/\//g, "")
-          );
-          lNode.path.shift();
-          lNode.path = lNode.path.join("/");
-          lNode.path = lNode.path ? `/${lNode.path}/` : "/";
-          lNode.href = lNode.url
-            ? lNode.url
-                .trim()
-                .replace(/\s/g, "_")
-                .replace(/^\/+|\/+$/g, "")
-            : "";
-          lNode.href = lNode.href ? `/${lNode.href}/` : "";
-          return lNode;
-        });
-    },
-    /**
-     * Объекты одного уровня с текущим
-     *
-     * @returns {object[]} Массив объектов
-     */
-    siblings() {
-      return this.getSiblings(this.item);
-    },
-    /**
-     * Вычисление массива первого уровня
-     *
-     * @returns {object[]} Массив объектов первого уровня
-     */
-    children() {
-      return this.item.data;
-    },
-    /**
-     * Вычисление массива первого уровня
-     *
-     * @returns {object[]} Массив объектов первого уровня
-     */
-    treeChildren() {
-      return this.tree.data;
-    },
-    /**
-     * Вычисление массива первого уровня
-     *
-     * @returns {object[]} Массив объектов первого уровня
-     */
-    parentChildren() {
-      return this.parent.data;
-    },
-    /**
-     * Вектор до текущего объекта
-     *
-     * @returns {object[]} Вектор
-     */
-    vector() {
-      return this.getVector(this.item);
-    },
-    /**
-     * Родительский объект
-     *
-     * @returns {object} Родитель
-     */
-    parent() {
-      return this.getParent(this.item);
-    },
-    /**
-     * Текущий объект
-     *
-     * @returns {object} Текущий объект для загрузки
-     */
-    item() {
-      return (
-        this.list.find(
-          (e) => e.path === this.routePath || e.href === this.routePath
-        ) || {}
-      );
-    },
-    /**
-     * Id текущего объекта
-     *
-     * @returns {string} Id
-     */
-    id() {
-      return this.item.id;
-    },
-    /**
-     * Заголовок текущего объекта
-     *
-     * @returns {string} Заголовок
-     */
-    title() {
-      return this.getTitle(this.item);
-    },
-    /**
-     * Заголовок дерева
-     *
-     * @returns {string} Заголовок
-     */
-    treeTitle() {
-      return this.getTitle(this.tree);
-    },
-    /**
-     * Заголовок родительского объекта
-     *
-     * @returns {string} Заголовок
-     */
-    parentTitle() {
-      return this.getTitle(this.parent);
-    },
-    /**
-     * Описание текущего объекта
-     *
-     * @returns {string} Описание
-     */
-    description() {
-      return this.item.description;
-    },
-    /**
-     * Ключевые слова текущего объекта
-     *
-     * @returns {string} Ключевые слова
-     */
-    keywords() {
-      return this.item.keywords;
-    },
-    /**
-     * Описание дерева
-     *
-     * @returns {string} Описание
-     */
-    treeDescription() {
-      return this.tree.description;
-    },
-    /**
-     * Описание родительского объекта
-     *
-     * @returns {string} Описание
-     */
-    parentDescription() {
-      return this.parent.description;
-    },
-    /**
-     * Иконка текущего объекта
-     *
-     * @returns {string} Иконка
-     */
-    icon() {
-      return this.item.icon;
-    },
-    /**
-     * Иконка дерева
-     *
-     * @returns {string} Иконка
-     */
-    treeIcon() {
-      return this.tree.icon;
-    },
-    /**
-     * Иконка родительского объекта
-     *
-     * @returns {string} Иконка
-     */
-    parentIcon() {
-      return this.parent.icon;
-    },
-    /**
-     * Путь текущего объекта
-     *
-     * @returns {string} Путь
-     */
-    path() {
-      return this.getPath(this.item);
-    },
-    /**
-     * Путь дерева
-     *
-     * @returns {string} Путь
-     */
-    treePath() {
-      return this.getPath(this.tree);
-    },
-    /**
-     * Путь родительского объекта
-     *
-     * @returns {string} Путь
-     */
-    parentPath() {
-      return this.getPath(this.parent);
-    },
+  const getVector = (item) =>
+    jsel(get(tree)).selectAll(`//*[@id="${item.id}"]/ancestor-or-self::*[@id]`);
 
-    /**
-     * Картинка текущего объекта
-     *
-     * @returns {string} Картинка
-     */
-    image() {
-      return this.item.image;
-    },
-    /**
-     * Картинка дерева
-     *
-     * @returns {string} Картинка
-     */
-    treeImage() {
-      return this.item.tree;
-    },
-    /**
-     * Картинка родительского объекта
-     *
-     * @returns {string} Картинка
-     */
-    parentImage() {
-      return this.parent.image;
-    },
-    /**
-     * Вычисление текущего пути
-     *
-     * @returns {string} Путь
-     */
-    routePath() {
-      return this.context.routePath;
-    },
-  },
-  actions: {
-    /**
-     * Загрузка данных в tree
-     */
-    async initIndex() {
-      [this.tree] = await (
-        await fetch("index.json", { cache: "no-store" })
-      ).json();
-    },
-    /**
-     * Вычисленние пути
-     *
-     * @param {object} item Объект вычисления
-     * @returns {string} Путь
-     */
-    getPath: (item) => (item.href ? item.href : item.path),
-    /**
-     * Вычисление заголовка
-     *
-     * @param {object} item Объект вычисления
-     * @returns {string} Заголовок
-     */
-    getTitle: (item) => (item.title ? item.title : item.value),
-    /**
-     * Вычисление вектора
-     *
-     * @param {object} item Объект вычисления
-     * @returns {object[]} Вектор
-     */
-    getVector(item) {
-      return jsel(this.tree).selectAll(
-        `//*[@id="${item.id}"]/ancestor-or-self::*[@id]`
-      );
-    },
-    /**
-     * Вычисление родителя
-     *
-     * @param {object} item Объект вычисления
-     * @returns {object} Родитель
-     */
-    getParent(item) {
-      return jsel(this.tree).select(`//*[@id="${item.id}"]/../parent::*[@id]`);
-    },
-    /**
-     * Вычисление объектов одного уровня
-     *
-     * @param {object} item Объект вычисления
-     * @returns {object[]} Объекты одного уровня
-     */
-    getSiblings(item) {
-      return jsel(this.tree).selectAll(
-        `//*[@id="${item.id}"]/preceding-sibling::*[@id]|//*[@id="${item.id}"]|//*[@id="${item.id}"]/following-sibling::*[@id]`
-      );
-    },
-    /**
-     * Получение массива дочерних объектов
-     *
-     * @param {(number | boolean)} deep Флаг использования рекурсии
-     *  по дочерним объектам
-     * @param {number} length Количество дочерних объектов для изъятия
-     * @param {(number | boolean)} reveal Флаг указывающий показывать ли
-     *  скрытые объекты
-     * @param {string} sort Флаг указывающий на необходимость
-     *  отсортировать результат
-     * @param {string} path CSV путей до дочерних объектов
-     * @param {(number | boolean)} children Выбирать папки или файлы?
-     * Если путое значение то всё
-     * @param {string} [attr=""] Путь xpath
-     * @param {string} axe Заведует включением параметра xpath axe
-     * @returns {object[]} Массив дочерних объектов
-     */
-    getItems(deep, length, reveal, sort, path, children, attr, axe) {
-      let lChildren = null;
-      const lAttr = attr || "";
-      let dataChildren = [];
-      const dataHashes = (
-        Array.isArray(path) ? path : [path || this.routePath || ""]
-      ).map((value) =>
-        decodeURIComponent(value.trim())
-          .replace(/_/g, " ")
-          .replace(/\/+/g, "/")
-          .replace(/^\/+|\/+$/g, "")
-      );
-      dataHashes.forEach((dataHash) => {
-        try {
-          if (children === undefined || children === null || children === "")
-            lChildren = "";
-          else lChildren = children ? "[*]" : "[not(*)]";
-          $.merge(
-            dataChildren,
-            jsel(this.tree).selectAll(
-              `/*${
-                dataHash
-                  ? `/data/*[@value="${dataHash
-                      .split("/")
-                      .join('"]/data/*[@value="')}"]`
-                  : ""
-              }${lAttr && !axe ? "/data" : ""}${
-                deep && lAttr && !axe ? "/" : ""
-              }${lAttr ? "/" : ""}${axe ? `${axe}::` : ""}${lAttr}${
-                lAttr && !axe && !reveal ? "[@visible=1]" : ""
-              }${lChildren}`
-            )
-          );
-        } catch (e) {
-          // console.log(e.message);
-        }
-      });
-      // if (lAttr && !axe) {
-      //  dataChildren = dataChildren.filter(
-      //    (element) => element.$href.replace(/^\/+|\/+$/g, "") !== hash
-      //  );
-      // }
-      if (
-        length &&
-        !Number.isNaN(Number(length)) &&
-        length > 0 &&
-        length < dataChildren.length
-      ) {
-        dataChildren = dataChildren.slice(0, length);
+  const list = computed(() =>
+    jsel(get(tree))
+      .selectAll("//*[@id]")
+      .map((node) => {
+        const lNode = node;
+        lNode.path = getVector(lNode).map((e) =>
+          e.value.trim().replace(/\s/g, "_").replace(/\//g, "")
+        );
+        lNode.path.shift();
+        lNode.path = lNode.path.join("/");
+        lNode.path = lNode.path ? `/${lNode.path}/` : "/";
+        lNode.href = lNode.url
+          ? lNode.url
+              .trim()
+              .replace(/\s/g, "_")
+              .replace(/^\/+|\/+$/g, "")
+          : "";
+        lNode.href = lNode.href ? `/${lNode.href}/` : "";
+        return lNode;
+      })
+  );
+  /**
+   * Вычисленние пути
+   *
+   * @param {object} item Объект вычисления
+   * @returns {string} Путь
+   */
+  const getPath = (item) => (item.path ? item.path : item.href);
+  /**
+   * Вычисление заголовка
+   *
+   * @param {object} item Объект вычисления
+   * @returns {string} Заголовок
+   */
+  const getTitle = (item) => (item.title ? item.title : item.value);
+  /**
+   * Вычисление родителя
+   *
+   * @param {object} item Объект вычисления
+   * @returns {object} Родитель
+   */
+  const getParent = (item) =>
+    jsel(get(tree)).select(`//*[@id="${item.id}"]/../parent::*[@id]`);
+  /**
+   * Вычисление объектов одного уровня
+   *
+   * @param {object} item Объект вычисления
+   * @returns {object[]} Объекты одного уровня
+   */
+  const getSiblings = (item) =>
+    jsel(get(tree)).selectAll(
+      `//*[@id="${item.id}"]/preceding-sibling::*[@id]|//*[@id="${item.id}"]|//*[@id="${item.id}"]/following-sibling::*[@id]`
+    );
+
+  const item = computed(
+    () =>
+      get(list).find(
+        (e) => e.path === get(routePath) || e.href === get(routePath)
+      ) || {}
+  );
+
+  /**
+   * Загрузка данных в tree
+   */
+  async function initIndex() {
+    const { data } = await useFetch("index.json").json();
+    set(
+      tree,
+      // (await (await fetch("index.json", { cache: "no-store" })).json())[0]
+      get(data)[0]
+    );
+  }
+  /**
+   * Получение массива дочерних объектов
+   *
+   * @param {(number | boolean)} deep Флаг использования рекурсии
+   *  по дочерним объектам
+   * @param {number} length Количество дочерних объектов для изъятия
+   * @param {(number | boolean)} reveal Флаг указывающий показывать ли
+   *  скрытые объекты
+   * @param {string} sort Флаг указывающий на необходимость
+   *  отсортировать результат
+   * @param {string} path CSV путей до дочерних объектов
+   * @param {(number | boolean)} children Выбирать папки или файлы?
+   * Если путое значение то всё
+   * @param {string} [attr=""] Путь xpath
+   * @param {string} axe Заведует включением параметра xpath axe
+   * @returns {object[]} Массив дочерних объектов
+   */
+  function getItems(deep, length, reveal, sort, path, children, attr, axe) {
+    let lChildren = null;
+    const lAttr = attr || "";
+    let dataChildren = [];
+    const dataHashes = (
+      Array.isArray(path) ? path : [path || get(routePath) || ""]
+    ).map((value) =>
+      decodeURIComponent(value.trim())
+        .replace(/_/g, " ")
+        .replace(/\/+/g, "/")
+        .replace(/^\/+|\/+$/g, "")
+    );
+    dataHashes.forEach((dataHash) => {
+      try {
+        if (children === undefined || children === null || children === "")
+          lChildren = "";
+        else lChildren = children ? "[*]" : "[not(*)]";
+        $.merge(
+          dataChildren,
+          jsel(get(tree)).selectAll(
+            `/*${
+              dataHash
+                ? `/data/*[@value="${dataHash
+                    .split("/")
+                    .join('"]/data/*[@value="')}"]`
+                : ""
+            }${lAttr && !axe ? "/data" : ""}${
+              deep && lAttr && !axe ? "/" : ""
+            }${lAttr ? "/" : ""}${axe ? `${axe}::` : ""}${lAttr}${
+              lAttr && !axe && !reveal ? "[@visible=1]" : ""
+            }${lChildren}`
+          )
+        );
+      } catch (e) {
+        // console.log(e.message);
       }
-      switch (sort) {
-        case "random":
-          dataChildren.sort(() => 0.5 - Math.random());
-          break;
-        case "date":
-          dataChildren.sort((a, b) => {
-            if (a.date > b.date) return -1;
-            if (a.date < b.date) return 1;
-            return 0;
-          });
-          break;
-        default:
-      }
-      return dataChildren;
-    },
-  },
+    });
+    // if (lAttr && !axe) {
+    //  dataChildren = dataChildren.filter(
+    //    (element) => element.$href.replace(/^\/+|\/+$/g, "") !== hash
+    //  );
+    // }
+    if (
+      length &&
+      !Number.isNaN(Number(length)) &&
+      length > 0 &&
+      length < dataChildren.length
+    ) {
+      dataChildren = dataChildren.slice(0, length);
+    }
+    switch (sort) {
+      case "random":
+        dataChildren.sort(() => 0.5 - Math.random());
+        break;
+      case "date":
+        dataChildren.sort((a, b) => {
+          if (a.date > b.date) return -1;
+          if (a.date < b.date) return 1;
+          return 0;
+        });
+        break;
+      default:
+    }
+    return dataChildren;
+  }
+
+  const siblings = computed(() => getSiblings(get(item)));
+  const children = computed(() => get(item).data);
+  const treeChildren = computed(() => get(tree).data);
+  const parent = computed(() => getParent(get(item)));
+  const parentChildren = computed(() => get(parent).data);
+  const vector = computed(() => getVector(get(item)));
+  const id = computed(() => get(item).id);
+  const title = computed(() => getTitle(get(item)));
+  const treeTitle = computed(() => getTitle(get(tree)));
+  const parentTitle = computed(() => getTitle(get(parent)));
+  const description = computed(() => get(item).description);
+  const keywords = computed(() => get(item).keywords);
+  const treeDescription = computed(() => get(tree).description);
+  const parentDescription = computed(() => get(parent).description);
+  const icon = computed(() => get(item).icon);
+  const treeIcon = computed(() => get(tree).icon);
+  const parentIcon = computed(() => get(parent).icon);
+  const path = computed(() => getPath(get(item)));
+  const treePath = computed(() => getPath(get(tree)));
+  const parentPath = computed(() => getPath(get(parent)));
+  const image = computed(() => get(item).image);
+  const treeImage = computed(() => get(item).tree);
+  const parentImage = computed(() => get(parent).image);
+
+  return {
+    tree,
+    context,
+    list,
+    siblings,
+    children,
+    treeChildren,
+    parentChildren,
+    vector,
+    parent,
+    item,
+    id,
+    title,
+    treeTitle,
+    parentTitle,
+    description,
+    keywords,
+    treeDescription,
+    parentDescription,
+    icon,
+    treeIcon,
+    parentIcon,
+    path,
+    treePath,
+    parentPath,
+    image,
+    treeImage,
+    parentImage,
+    routePath,
+    initIndex,
+    getPath,
+    getTitle,
+    getVector,
+    getParent,
+    getSiblings,
+    getItems,
+  };
 });
