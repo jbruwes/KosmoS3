4a3267ab194290523a9821a7576ae99746ccb6f6
Jerry Bruwes <jbruwes@icloud.com>
2022-09-06 15:48:26 +0200

    Create VTemplateHeaderK3.vue

---
+++ fel/sources/modules/VTemplateHeaderK3.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-alert
+    :icon="icon"
+    v-animate-onscroll.repeat="animate"
+    :height="height"
+    :width="width"
+    :class="classes"
+    :href="href"
+    :variant="variant"
+    :title="title"
+    ><v-chip
+      v-if="subtitle"
+      size="x-small"
+      class="mr-1"
+      label
+      variant="outlined"
+      prepend-icon="mdi-calendar"
+      :text="subtitle"
+    ></v-chip
+    >{{ text }}</v-alert
+  >
+</template>
+<script>
+export default {
+  props: {
+    animate: { default: "animate__animated animate__bounce", type: String },
+    class: String,
+    width: Number,
+    height: Number,
+    title: String,
+    subtitle: String,
+    text: String,
+    icon: String,
+    href: String,
+    variant: { default: "plain", type: String },
+  },
+};
+</script>
