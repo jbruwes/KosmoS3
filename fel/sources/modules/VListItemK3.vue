654d08416228e1bdbedc53891634533567937a72
Jerry Bruwes <jbruwes@icloud.com>
2022-09-07 12:49:05 +0200

    Update VListItemK3.vue

--- fel/sources/modules/VListItemK3.vue
+++ fel/sources/modules/VListItemK3.vue
@@ -5,6 +5,7 @@
       :item="slot.item"
       :animate="slot.animate"
       :date="slot.date"
+      :description="slot.description"
       :variant="slot.variant"
       :height="slot.height"
       :width="slot.width"
