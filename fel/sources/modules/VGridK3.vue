d00715dbacd305a308bd232c2fd486dfef72600c
Jerry Bruwes <jbruwes@icloud.com>
2022-09-07 15:19:34 +0200

    Update VGridK3.vue

--- fel/sources/modules/VGridK3.vue
+++ fel/sources/modules/VGridK3.vue
@@ -32,6 +32,7 @@ export default {
     sort: String,
     path: String,
     children: { default: undefined, type: Boolean },
+    attr: { default: "*[@id]", type: Boolean },
   },
   computed: {
     items() {
@@ -42,7 +43,7 @@ export default {
         this.sort,
         this.path,
         this.children,
-        "*[@id][string(@image)]"
+        this.attr,
       );
     },
   },
