16197f5e07306c7001947ccba1a32402d63db24e
Jerry Bruwes <jbruwes@icloud.com>
2022-09-07 10:11:33 +0200

    Update VTemplateIconK3.vue

--- fel/sources/modules/VTemplateIconK3.vue
+++ fel/sources/modules/VTemplateIconK3.vue
@@ -31,8 +31,8 @@ export default {
   props: {
     animate: { default: "animate__animated animate__bounce", type: String },
     classes: String,
-    width: Number,
-    height: Number,
+    width: [String, Number],
+    height: [String, Number],
     title: String,
     date: String,
     description: String,
