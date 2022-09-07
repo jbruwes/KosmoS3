bdfbfba871f7c3dd36321072c63468675291c15b
Jerry Bruwes <jbruwes@icloud.com>
2022-09-07 09:35:41 +0200

    Update VSingleBannerK3.vue

--- fel/sources/modules/VSingleBannerK3.vue
+++ fel/sources/modules/VSingleBannerK3.vue
@@ -9,8 +9,8 @@
       :icon="slot.icon"
       :image="slot.image"
       :href="slot.href"
-      :subtitle="slot.subtitle"
-      :text="slot.text"
+      :date="slot.sate"
+      :description="slot.description"
       :variant="slot.variant"
     ></v-template-banner-k3
   ></v-single-k3>
