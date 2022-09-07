c2bba1e360aeaf79ba1a8540257ac13c87463f7e
Jerry Bruwes <jbruwes@icloud.com>
2022-09-07 15:24:17 +0200

    Update VCarouselK3.vue

--- fel/sources/modules/VCarouselK3.vue
+++ fel/sources/modules/VCarouselK3.vue
@@ -28,7 +28,7 @@ export default {
     sort: String,
     path: String,
     children: { default: undefined, type: Boolean },
-    attr: { default: "*[@id]", type: Boolean },
+    xpath: { default: "*[@id]", type: Boolean },
   },
   computed: {
     items() {
@@ -39,7 +39,7 @@ export default {
         this.sort,
         this.path,
         this.children,
-        this.attr,
+        this.xpath,
       );
     },
   },
