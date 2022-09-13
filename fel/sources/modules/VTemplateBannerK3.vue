48a4c341bc9d9814e402f73e9d410447aef0d21f
Jerry Bruwes <jbruwes@icloud.com>
2022-09-13 10:18:03 +0200

    Update VTemplateBannerK3.vue

--- fel/sources/modules/VTemplateBannerK3.vue
+++ fel/sources/modules/VTemplateBannerK3.vue
@@ -1,54 +1,50 @@
 <template>
-  <v-parallax
-    :height="height"
-    :width="width"
-    :src="image"
-    aspect-ratio="1"
-  >
-    <v-row class="fill-height" align="center" justify="center">
-      <v-card
-        class="pa-16 ma-16 text-white"
-        :href="href"
-        :variant="variant"
-        v-animate-onscroll.repeat="animate"
-      >
-        <div class="text-center">
-          <v-icon
-            :icon="icon"
-            style="font-size: calc(var(--v-icon-size-multiplier) * 4em);"
-          ></v-icon>
-        </div>
-        <v-card-item class="justify-center text-center">
-          <div>
-            <div class="text-h3 mb-1" v-if="title">
-              {{ title }}
-            </div>
-            <div v-if="date">
-              <v-chip
-                size="x-small"
-                label
-                variant="outlined"
-                prepend-icon="mdi-calendar"
-                :text="date"
-              ></v-chip>
-            </div>
+  <v-row class="fill-height" align="center" justify="center">
+    <v-card
+      class="pa-16 ma-16 text-white"
+      :href="href"
+      :variant="variant"
+      v-animate-onscroll.repeat="animate"
+    >
+      <div class="text-center">
+        <v-icon
+          :icon="icon"
+          style="font-size: calc(var(--v-icon-size-multiplier) * 4em);"
+        ></v-icon>
+      </div>
+      <v-card-item class="justify-center text-center">
+        <div>
+          <div class="text-h4 mb-1" v-if="title">
+            {{ title }}
           </div>
-        </v-card-item>
-        <v-alert v-if="description" :title="description" variant="text" prominent class="text-center"></v-alert>
-      </v-card>
-    </v-row>
-  </v-parallax>
+          <div v-if="date">
+            <v-chip
+              size="x-small"
+              label
+              variant="outlined"
+              prepend-icon="mdi-calendar"
+              :text="date"
+            ></v-chip>
+          </div>
+        </div>
+      </v-card-item>
+      <v-alert
+        v-if="description"
+        :title="description"
+        variant="text"
+        prominent
+        class="text-center"
+      ></v-alert>
+    </v-card>
+  </v-row>
 </template>
 <script>
 export default {
   props: {
     animate: { default: "animate__animated animate__bounceIn", type: String },
-    width: [String, Number],
-    height: [String, Number],
     title: String,
     date: String,
     description: String,
-    image: String,
     icon: String,
     href: String,
     variant: { default: "tonal", type: String },
