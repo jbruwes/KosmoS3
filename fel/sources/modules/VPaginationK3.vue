a863c0c9b6e12218bcc8b2c40d5b03562467b2ed
Jerry Bruwes <jbruwes@icloud.com>
2022-09-15 10:58:53 +0200

    Update VPaginationK3.vue

--- fel/sources/modules/VPaginationK3.vue
+++ fel/sources/modules/VPaginationK3.vue
@@ -5,7 +5,7 @@
 <script setup>
 import { storeToRefs } from "pinia";
 import { ref, computed, watch } from "vue";
-import { get, set, computedWithControl } from "@vueuse/core";
+import { get, set, watchTriggerable } from "@vueuse/core";
 import page from "page";
 import core from "~/core.js";
 const props = defineProps({
@@ -23,27 +23,25 @@ const store = core();
 const { id } = storeToRefs(store);
 const { getItems, getHref } = store;
 const itemIndex = ref(0);
-const items = computedWithControl(
-  () => get(id),
-  () =>
-    getItems(
-      props.deep,
-      props.length,
-      props.reveal,
-      props.sort,
-      props.path,
-      props.children,
-      props.xpath,
-      props.axe
-    )
+const items = computed(() =>
+  getItems(
+    props.deep,
+    props.length,
+    props.reveal,
+    props.sort,
+    props.path,
+    props.children,
+    props.xpath,
+    props.axe
+  )
 );
 const itemsLength = computed(() => get(items).length);
-watch(items, (newItems) => {
+const { trigger } = watchTriggerable(items, (newItems) => {
   const index = 1 + newItems.findIndex((item) => item.id === get(id));
   if (index) set(itemIndex, index);
 });
 watch(itemIndex, (newItemIndex, oldItemIndex) => {
   if (oldItemIndex) page(getHref(get(items)[newItemIndex - 1]));
 });
-if (get(id)) items.trigger();
+if (get(id)) trigger();
 </script>
