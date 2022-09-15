fb8b99ed9e0623644e7a10298cac827a75b68438
Jerry Bruwes <jbruwes@icloud.com>
2022-09-15 08:57:24 +0200

    Update VPaginationK3.vue

--- fel/sources/modules/VPaginationK3.vue
+++ fel/sources/modules/VPaginationK3.vue
@@ -2,60 +2,48 @@
   <v-pagination v-model="itemIndex" :length="itemsLength" :total-visible="7">
   </v-pagination>
 </template>
-<script>
+<script setup>
 import { storeToRefs } from "pinia";
 import { ref, computed, watch } from "vue";
-import { get, set } from "@vueuse/core";
+import { get, set, computedWithControl } from "@vueuse/core";
 import page from "page";
 import core from "~/core.js";
-export default {
-  props: {
-    variant: String,
-    deep: Boolean,
-    length: Number,
-    reveal: Boolean,
-    sort: String,
-    path: String,
-    children: { default: undefined, type: Boolean },
-    xpath: { default: "*/*[@id]", type: String },
-    axe: { default: "parent", type: String },
-  },
-  setup(props) {
-    const store = core();
-    const { id } = storeToRefs(store);
-    const { getItems, getHref } = store;
-    const itemIndex = ref(0);
-    const items = computed(() => {
-      let ret = getItems(
-        props.deep,
-        props.length,
-        props.reveal,
-        props.sort,
-        props.path,
-        props.children,
-        props.xpath,
-        props.axe
-      );
-      if (!ret.length)
-        ret = getItems(
-          props.deep,
-          props.length,
-          props.reveal,
-          props.sort,
-          props.path,
-          props.children,
-          "*[@id]",
-          "self"
-        );
-      const index = 1 + ret.findIndex((e) => e.id === get(id));
-      if (index) set(itemIndex, index);
-      return ret;
-    });
-    const itemsLength = computed(() => get(items).length);
-    watch(itemIndex, (newIndex, oldIndex) => {
-      if (oldIndex) page(getHref(get(items)[newIndex - 1]));
-    });
-    return { itemIndex, itemsLength };
-  },
-};
+const props = defineProps({
+  variant: String,
+  deep: Boolean,
+  length: Number,
+  reveal: Boolean,
+  sort: String,
+  path: String,
+  children: { default: undefined, type: Boolean },
+  xpath: { default: "*/*[@id]", type: String },
+  axe: { default: "parent", type: String },
+});
+const store = core();
+const { id } = storeToRefs(store);
+const { getItems, getHref } = store;
+const itemIndex = ref(0);
+const items = computedWithControl(
+  () => get(id),
+  () =>
+    getItems(
+      props.deep,
+      props.length,
+      props.reveal,
+      props.sort,
+      props.path,
+      props.children,
+      props.xpath,
+      props.axe
+    )
+);
+const itemsLength = computed(() => get(items).length);
+watch(items, (newItems) => {
+  const index = 1 + newItems.findIndex((item) => item.id === get(id));
+  if (index) set(itemIndex, index);
+});
+watch(itemIndex, (newItemIndex, oldItemIndex) => {
+  if (oldItemIndex) page(getHref(get(items)[newItemIndex - 1]));
+});
+if (get(id)) items.trigger();
 </script>
