de8c70057453996a22fd718bbb967a76c3a77e9f
Jerry Bruwes <jbruwes@icloud.com>
2022-09-19 16:53:16 +0200

    Create VSingleBannerRippleK3.vue

---
+++ fel/sources/modules/VSingleBannerRippleK3.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-single-k3 #="slot"
+    ><v-vanta-ripple-k3 :height="slot.height" :width="slot.width"
+      ><v-template-banner-k3
+        :animate="slot.animate"
+        :classes="slot.classes"
+        :title="slot.title"
+        :icon="slot.icon"
+        :href="slot.href"
+        :date="slot.date"
+        :description="slot.description"
+        :variant="slot.variant"
+      ></v-template-banner-k3></v-vanta-ripple-k3
+  ></v-single-k3>
+</template>
+<script setup>
+import VSingleK3 from "./VSingleK3.vue";
+import VVantaRippleK3 from "./VVantaRippleK3.vue";
+import VTemplateBannerK3 from "./VTemplateBannerK3.vue";
+</script>
