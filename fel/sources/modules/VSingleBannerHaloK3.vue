a9d8eb43032a27b949c4c9243befd5a0f093b30b
Jerry Bruwes <jbruwes@icloud.com>
2022-09-19 16:53:06 +0200

    Create VSingleBannerHaloK3.vue

---
+++ fel/sources/modules/VSingleBannerHaloK3.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-single-k3 #="slot"
+    ><v-vanta-halo-k3 :height="slot.height" :width="slot.width"
+      ><v-template-banner-k3
+        :animate="slot.animate"
+        :classes="slot.classes"
+        :title="slot.title"
+        :icon="slot.icon"
+        :href="slot.href"
+        :date="slot.date"
+        :description="slot.description"
+        :variant="slot.variant"
+      ></v-template-banner-k3></v-vanta-halo-k3
+  ></v-single-k3>
+</template>
+<script setup>
+import VSingleK3 from "./VSingleK3.vue";
+import VVantaHaloK3 from "./VVantaHaloK3.vue";
+import VTemplateBannerK3 from "./VTemplateBannerK3.vue";
+</script>
