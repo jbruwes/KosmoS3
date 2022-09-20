e3d913d8b5e4bb504e7d63e2f89e76911b6e9b9e
Jerry Bruwes <jbruwes@icloud.com>
2022-09-20 16:06:38 +0200

    Update VSingleButtonK3.vue

--- fel/sources/modules/VSingleButtonK3.vue
+++ fel/sources/modules/VSingleButtonK3.vue
@@ -17,10 +17,10 @@
 import VSingleK3 from "./VSingleK3.vue";
 import VTemplateButtonK3 from "./VTemplateButtonK3.vue";
 const props = defineProps({
-  classes: { default: undefined, type: String },
-  animate: { default: undefined, type: String },
-  width: { default: undefined, type: [String, Number] },
-  height: { default: undefined, type: [String, Number] },
-  variant: { default: undefined, type: String },
+  classes: String,
+  animate: String,
+  width: [String, Number],
+  height: [String, Number],
+  variant: String,
 });
 </script>
