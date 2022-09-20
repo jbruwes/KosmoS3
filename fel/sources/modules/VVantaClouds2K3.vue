46ed2e7934855c5be8e0774e0433855d4e9a8caf
Jerry Bruwes <jbruwes@icloud.com>
2022-09-20 14:26:17 +0200

    Update VVantaClouds2K3.vue

--- fel/sources/modules/VVantaClouds2K3.vue
+++ fel/sources/modules/VVantaClouds2K3.vue
@@ -7,6 +7,21 @@ import p5 from "p5";
 import CLOUDS2 from "vanta/dist/vanta.clouds2.min";
 import { ref, onMounted, onBeforeUnmount } from "vue";
 import { get, useResizeObserver } from "@vueuse/core";
+const props = defineProps({
+  mouseControls: { default: true, type: Boolean },
+  touchControls: { default: true, type: Boolean },
+  gyroControls: { default: false, type: Boolean },
+  minHeight: { default: 200, type: Number },
+  minWidth: { default: 200, type: Number },
+  scale: { default: 1, type: Number },
+  scaleMobile: { default: 4, type: Number },
+  backgroundColor: { default: 0x000000, type: Number },
+  skyColor: { default: 0x5ca6ca, type: Number },
+  cloudColor: { default: 0x334d80, type: Number },
+  lightColor: { default: 0xffffff, type: Number },
+  speed: { default: 1.0, type: Number },
+  texturePath: { default: "noise.png", type: String },
+});
 const vantaRef = ref(null);
 let vantaEffect;
 onMounted(() => {
@@ -14,7 +29,19 @@ onMounted(() => {
     el: get(vantaRef).$el,
     THREE: THREE,
     p5: p5,
-    texturePath: "noise.png",
+    mouseControls: props.mouseControls,
+    touchControls: props.touchControls,
+    gyroControls: props.gyroControls,
+    minHeight: props.minHeight,
+    minWidth: props.minWidth,
+    scale: props.scale,
+    scaleMobile: props.scaleMobile,
+    backgroundColor: props.backgroundColor,
+    skyColor: props.skyColor,
+    cloudColor: props.cloudColor,
+    lightColor: props.lightColor,
+    speed: props.speed,
+    texturePath: props.texturePath,
   });
 });
 useResizeObserver(vantaRef, () => {
