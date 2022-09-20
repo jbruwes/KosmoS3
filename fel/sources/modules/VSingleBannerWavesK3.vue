3cee7f5a883915cb5bc6b5bc48e7e0d04db8fda6
Jerry Bruwes <jbruwes@icloud.com>
2022-09-20 15:11:53 +0200

    Update VSingleBannerWavesK3.vue

--- fel/sources/modules/VSingleBannerWavesK3.vue
+++ fel/sources/modules/VSingleBannerWavesK3.vue
@@ -1,6 +1,20 @@
 <template>
   <v-single-k3 #="slot"
-    ><v-vanta-waves-k3 :height="height" :width="width"
+    ><v-vanta-waves-k3
+      :height="height"
+      :width="width"
+      :mouseControls="mouseControls"
+      :touchControls="touchControls"
+      :gyroControls="gyroControls"
+      :minHeight="minHeight"
+      :minWidth="minWidth"
+      :scale="scale"
+      :scaleMobile="scaleMobile"
+      :color="color"
+      :shininess="shininess"
+      :waveHeight="waveHeight"
+      :waveSpeed="waveSpeed"
+      :zoom="zoom"
       ><v-template-banner-k3
         :animate="animate"
         :classes="classes"
@@ -23,5 +37,17 @@ const props = defineProps({
   width: { default: undefined, type: [String, Number] },
   height: { default: undefined, type: [String, Number] },
   variant: { default: undefined, type: String },
+  mouseControls: { default: undefined, type: Boolean },
+  touchControls: { default: undefined, type: Boolean },
+  gyroControls: { default: undefined, type: Boolean },
+  minHeight: { default: undefined, type: Number },
+  minWidth: { default: undefined, type: Number },
+  scale: { default: undefined, type: Number },
+  scaleMobile: { default: undefined, type: Number },
+  color: { default: undefined, type: Number },
+  shininess: { default: undefined, type: Number },
+  waveHeight: { default: undefined, type: Number },
+  waveSpeed: { default: undefined, type: Number },
+  zoom: { default: undefined, type: Number },
 });
 </script>
