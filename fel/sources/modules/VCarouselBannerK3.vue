c1266277716a67a6f20b1f4bd607f0dc183b0af2
Jerry Bruwes <jbruwes@icloud.com>
2022-09-20 14:22:55 +0200

    Update VCarouselBannerK3.vue

--- fel/sources/modules/VCarouselBannerK3.vue
+++ fel/sources/modules/VCarouselBannerK3.vue
@@ -1,17 +1,27 @@
 <template>
-  <v-carousel-k3 #="slot" selector="*[@id][string(@image)]">
+  <v-carousel-k3
+    #="slot"
+    selector="*[@id][string(@image)]"
+    :width="width"
+    :height="height"
+  >
     <v-single-banner-k3
+      :classes="classes"
       :item="slot.item"
-      :animate="slot.animate"
+      :animate="animate"
       :date="slot.date"
-      :variant="slot.variant"
+      :variant="variant"
     ></v-single-banner-k3
   ></v-carousel-k3>
 </template>
-<script>
+<script setup>
 import VCarouselK3 from "./VCarouselK3.vue";
 import VSingleBannerK3 from "./VSingleBannerK3.vue";
-export default {
-  components: { VCarouselK3, VSingleBannerK3 },
-};
+const props = defineProps({
+  classes: { default: undefined, type: String },
+  animate: { default: undefined, type: String },
+  width: { default: undefined, type: [String, Number] },
+  height: { default: undefined, type: [String, Number] },
+  variant: { default: undefined, type: String },
+});
 </script>
