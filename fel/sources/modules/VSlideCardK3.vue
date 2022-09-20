6a4a3c83b50c822398637690dedd933f84ec4bb9
Jerry Bruwes <jbruwes@icloud.com>
2022-09-20 14:25:58 +0200

    Update VSlideCardK3.vue

--- fel/sources/modules/VSlideCardK3.vue
+++ fel/sources/modules/VSlideCardK3.vue
@@ -1,21 +1,25 @@
 <template>
   <v-slide-k3 #="slot" selector="*[@id][string(@image)]"
     ><v-single-card-k3
-      classes="ma-4"
+      :classes="classes"
       :item="slot.item"
-      :animate="slot.animate"
+      :animate="animate"
       :date="slot.date"
       :description="slot.description"
-      :variant="slot.variant"
-      :height="slot.height"
-      :width="slot.width"
+      :variant="variant"
+      :height="height"
+      :width="width"
     ></v-single-card-k3
   ></v-slide-k3>
 </template>
-<script>
+<script setup>
 import VSlideK3 from "./VSlideK3.vue";
 import VSingleCardK3 from "./VSingleCardK3.vue";
-export default {
-  components: { VSlideK3, VSingleCardK3 },
-};
+const props = defineProps({
+  classes: { default: "ma-4", type: String },
+  animate: { default: undefined, type: String },
+  width: { default: undefined, type: [String, Number] },
+  height: { default: undefined, type: [String, Number] },
+  variant: { default: undefined, type: String },
+});
 </script>
