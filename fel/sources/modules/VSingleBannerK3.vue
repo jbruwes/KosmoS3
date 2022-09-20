ddb8442ecac5c97cc466faea748aad39ca40d720
Jerry Bruwes <jbruwes@icloud.com>
2022-09-20 14:24:46 +0200

    Update VSingleBannerK3.vue

--- fel/sources/modules/VSingleBannerK3.vue
+++ fel/sources/modules/VSingleBannerK3.vue
@@ -1,27 +1,31 @@
 <template>
   <v-single-k3 #="slot"
     ><v-parallax
-      :height="slot.height"
-      :width="slot.width"
+      :height="height"
+      :width="width"
       :src="slot.image"
       :eager="true"
       :aspect-ratio="1"
       ><v-template-banner-k3
-        :animate="slot.animate"
-        :classes="slot.classes"
+        :animate="animate"
+        :classes="classes"
         :title="slot.title"
         :icon="slot.icon"
         :href="slot.href"
         :date="slot.date"
         :description="slot.description"
-        :variant="slot.variant"
+        :variant="variant"
       ></v-template-banner-k3></v-parallax
   ></v-single-k3>
 </template>
-<script>
+<script setup>
 import VSingleK3 from "./VSingleK3.vue";
 import VTemplateBannerK3 from "./VTemplateBannerK3.vue";
-export default {
-  components: { VSingleK3, VTemplateBannerK3 },
-};
+const props = defineProps({
+  classes: { default: undefined, type: String },
+  animate: { default: undefined, type: String },
+  width: { default: undefined, type: [String, Number] },
+  height: { default: undefined, type: [String, Number] },
+  variant: { default: undefined, type: String },
+});
 </script>
