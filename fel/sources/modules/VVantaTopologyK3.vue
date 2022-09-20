9ee19914115e16d7f56927d3e972fda98423b64a
Jerry Bruwes <jbruwes@icloud.com>
2022-09-20 14:26:39 +0200

    Update VVantaTopologyK3.vue

--- fel/sources/modules/VVantaTopologyK3.vue
+++ fel/sources/modules/VVantaTopologyK3.vue
@@ -7,6 +7,17 @@ import p5 from "p5";
 import TOPOLOGY from "vanta/dist/vanta.topology.min";
 import { ref, onMounted, onBeforeUnmount } from "vue";
 import { get, useResizeObserver } from "@vueuse/core";
+const props = defineProps({
+  mouseControls: { default: true, type: Boolean },
+  touchControls: { default: true, type: Boolean },
+  gyroControls: { default: false, type: Boolean },
+  minHeight: { default: 200, type: Number },
+  minWidth: { default: 200, type: Number },
+  scale: { default: 1, type: Number },
+  scaleMobile: { default: 1, type: Number },
+  color: { default: 0x89964e, type: Number },
+  backgroundColor: { default: 0x002222, type: Number },
+});
 const vantaRef = ref(null);
 let vantaEffect;
 onMounted(() => {
@@ -14,9 +25,18 @@ onMounted(() => {
     el: get(vantaRef).$el,
     THREE: THREE,
     p5: p5,
+    mouseControls: props.mouseControls,
+    touchControls: props.touchControls,
+    gyroControls: props.gyroControls,
+    minHeight: props.minHeight,
+    minWidth: props.minWidth,
+    scale: props.scale,
+    scaleMobile: props.scaleMobile,
+    color: props.color,
+    backgroundColor: props.backgroundColor,
   });
 });
-useResizeObserver(vantaRef, () => {
+useResizeObserver(vantaRef, async () => {
   if (vantaEffect) vantaEffect.resize();
 });
 onBeforeUnmount(() => {
