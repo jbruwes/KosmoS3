8cd89b02d7f40c1956a24cd189f31f76a00d2535
Jerry Bruwes <jbruwes@icloud.com>
2022-09-20 15:11:41 +0200

    Update VSingleBannerRingsK3.vue

--- fel/sources/modules/VSingleBannerRingsK3.vue
+++ fel/sources/modules/VSingleBannerRingsK3.vue
@@ -1,6 +1,17 @@
 <template>
   <v-single-k3 #="slot"
-    ><v-vanta-rings-k3 :height="height" :width="width"
+    ><v-vanta-rings-k3
+      :height="height"
+      :width="width"
+      :mouseControls="mouseControls"
+      :touchControls="touchControls"
+      :gyroControls="gyroControls"
+      :minHeight="minHeight"
+      :minWidth="minWidth"
+      :scale="scale"
+      :scaleMobile="scaleMobile"
+      :backgroundColor="backgroundColor"
+      :color="color"
       ><v-template-banner-k3
         :animate="animate"
         :classes="classes"
@@ -23,5 +34,14 @@ const props = defineProps({
   width: { default: undefined, type: [String, Number] },
   height: { default: undefined, type: [String, Number] },
   variant: { default: undefined, type: String },
+  mouseControls: { default: undefined, type: Boolean },
+  touchControls: { default: undefined, type: Boolean },
+  gyroControls: { default: undefined, type: Boolean },
+  minHeight: { default: undefined, type: Number },
+  minWidth: { default: undefined, type: Number },
+  scale: { default: undefined, type: Number },
+  scaleMobile: { default: undefined, type: Number },
+  backgroundColor: { default: undefined, type: Number },
+  color: { default: undefined, type: Number },
 });
 </script>
