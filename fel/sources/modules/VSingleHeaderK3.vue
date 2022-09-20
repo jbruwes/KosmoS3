657a73ce9c8a0b0a4af0aacbad2ad71b8e02f4d3
Jerry Bruwes <jbruwes@icloud.com>
2022-09-20 14:25:31 +0200

    Update VSingleHeaderK3.vue

--- fel/sources/modules/VSingleHeaderK3.vue
+++ fel/sources/modules/VSingleHeaderK3.vue
@@ -1,23 +1,27 @@
 <template>
   <v-single-k3 #="slot"
     ><v-template-header-k3
-      :animate="slot.animate"
-      :classes="slot.classes"
-      :width="slot.width"
-      :height="slot.height"
+      :animate="animate"
+      :classes="classes"
+      :width="width"
+      :height="height"
       :title="slot.title"
       :icon="slot.icon"
       :href="slot.href"
       :date="slot.date"
       :description="slot.description"
-      :variant="slot.variant"
+      :variant="variant"
     ></v-template-header-k3
   ></v-single-k3>
 </template>
-<script>
+<script setup>
 import VSingleK3 from "./VSingleK3.vue";
 import VTemplateHeaderK3 from "./VTemplateHeaderK3.vue";
-export default {
-  components: { VSingleK3, VTemplateHeaderK3 },
-};
+const props = defineProps({
+  classes: { default: undefined, type: String },
+  animate: { default: undefined, type: String },
+  width: { default: undefined, type: [String, Number] },
+  height: { default: undefined, type: [String, Number] },
+  variant: { default: undefined, type: String },
+});
 </script>
