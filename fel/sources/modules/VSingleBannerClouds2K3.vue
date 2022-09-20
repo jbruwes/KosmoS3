ca4cb3d806c974409ba9f5ea83bc7e53aa95a2a2
Jerry Bruwes <jbruwes@icloud.com>
2022-09-20 15:11:20 +0200

    Update VSingleBannerClouds2K3.vue

--- fel/sources/modules/VSingleBannerClouds2K3.vue
+++ fel/sources/modules/VSingleBannerClouds2K3.vue
@@ -1,6 +1,21 @@
 <template>
   <v-single-k3 #="slot"
-    ><v-vanta-clouds2-k3 :height="height" :width="width"
+    ><v-vanta-clouds2-k3
+      :height="height"
+      :width="width"
+      :mouseControls="mouseControls"
+      :touchControls="touchControls"
+      :gyroControls="gyroControls"
+      :minHeight="minHeight"
+      :minWidth="minWidth"
+      :scale="scale"
+      :scaleMobile="scaleMobile"
+      :backgroundColor="backgroundColor"
+      :skyColor="skyColor"
+      :cloudColor="cloudColor"
+      :lightColor="lightColor"
+      :speed="speed"
+      :texturePath="texturePath"
       ><v-template-banner-k3
         :animate="animate"
         :classes="classes"
@@ -23,5 +38,18 @@ const props = defineProps({
   width: { default: undefined, type: [String, Number] },
   height: { default: undefined, type: [String, Number] },
   variant: { default: undefined, type: String },
+  mouseControls: { default: undefined, type: Boolean },
+  touchControls: { default: undefined, type: Boolean },
+  gyroControls: { default: undefined, type: Boolean },
+  minHeight: { default: undefined, type: Number },
+  minWidth: { default: undefined, type: Number },
+  scale: { default: undefined, type: Number },
+  scaleMobile: { default: undefined, type: Number },
+  backgroundColor: { default: undefined, type: Number },
+  skyColor: { default: undefined, type: Number },
+  cloudColor: { default: undefined, type: Number },
+  lightColor: { default: undefined, type: Number },
+  speed: { default: undefined, type: Number },
+  texturePath: { default: undefined, type: String },
 });
 </script>
