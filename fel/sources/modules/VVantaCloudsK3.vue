c9508beb2f407b8d702c9126473d0238268e0139
Jerry Bruwes <jbruwes@icloud.com>
2022-09-20 14:26:19 +0200

    Update VVantaCloudsK3.vue

--- fel/sources/modules/VVantaCloudsK3.vue
+++ fel/sources/modules/VVantaCloudsK3.vue
@@ -7,6 +7,26 @@ import p5 from "p5";
 import CLOUDS from "vanta/dist/vanta.clouds.min";
 import { ref, onMounted, onBeforeUnmount } from "vue";
 import { get, useResizeObserver } from "@vueuse/core";
+const props = defineProps({
+  mouseControls: { default: true, type: Boolean },
+  touchControls: { default: true, type: Boolean },
+  gyroControls: { default: false, type: Boolean },
+  minHeight: { default: 200, type: Number },
+  minWidth: { default: 200, type: Number },
+  scale: { default: 1, type: Number },
+  scaleMobile: { default: 1, type: Number },
+  backgroundColor: { default: 0xffffff, type: Number },
+  skyColor: { default: 0x68b8d7, type: Number },
+  cloudColor: { default: 0xadc1de, type: Number },
+  cloudShadowColor: { default: 0x183550, type: Number },
+  sunColor: { default: 0xff9919, type: Number },
+  sunGlareColor: { default: 0xff6633, type: Number },
+  sunlightColor: { default: 0xff9933, type: Number },
+  scale: { default: 3, type: Number },
+  scaleMobile: { default: 12, type: Number },
+  speed: { default: 1, type: Number },
+  mouseEase: { default: true, type: Boolean },
+});
 const vantaRef = ref(null);
 let vantaEffect;
 onMounted(() => {
@@ -14,6 +34,24 @@ onMounted(() => {
     el: get(vantaRef).$el,
     THREE: THREE,
     p5: p5,
+    mouseControls: props.mouseControls,
+    touchControls: props.touchControls,
+    gyroControls: props.gyroControls,
+    minHeight: props.minHeight,
+    minWidth: props.minWidth,
+    scale: props.scale,
+    scaleMobile: props.scaleMobile,
+    backgroundColor: props.backgroundColor,
+    skyColor: props.skyColor,
+    cloudColor: props.cloudColor,
+    cloudShadowColor: props.cloudShadowColor,
+    sunColor: props.sunColor,
+    sunGlareColor: props.sunGlareColor,
+    sunlightColor: props.sunlightColor,
+    scale: props.scale,
+    scaleMobile: props.scaleMobile,
+    speed: props.speed,
+    mouseEase: props.mouseEase,
   });
 });
 useResizeObserver(vantaRef, () => {
