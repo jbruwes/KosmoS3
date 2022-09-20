360525c59a24a9a77622863366c65bd7f5577632
Jerry Bruwes <jbruwes@icloud.com>
2022-09-20 14:26:32 +0200

    Update VVantaRingsK3.vue

--- fel/sources/modules/VVantaRingsK3.vue
+++ fel/sources/modules/VVantaRingsK3.vue
@@ -7,6 +7,17 @@ import p5 from "p5";
 import RINGS from "vanta/dist/vanta.rings.min";
 import { ref, onMounted, onBeforeUnmount } from "vue";
 import { get, useResizeObserver } from "@vueuse/core";
+const props = defineProps({
+  mouseControls: { default: true, type: Boolean },
+  touchControls: { default: true, type: Boolean },
+  gyroControls: { default: false, type: Boolean },
+  minHeight: { default: 200, type: Number },
+  minWidth: { default: 200, type: Number },
+  scale: { default: 1, type: Number },
+  scaleMobile: { default: 1, type: Number },
+  backgroundColor: { default: 0x202428, type: Number },
+  color: { default: 0x88ff00, type: Number },
+});
 const vantaRef = ref(null);
 let vantaEffect;
 onMounted(() => {
@@ -14,6 +25,15 @@ onMounted(() => {
     el: get(vantaRef).$el,
     THREE: THREE,
     p5: p5,
+    mouseControls: props.mouseControls,
+    touchControls: props.touchControls,
+    gyroControls: props.gyroControls,
+    minHeight: props.minHeight,
+    minWidth: props.minWidth,
+    scale: props.scale,
+    scaleMobile: props.scaleMobile,
+    backgroundColor: props.backgroundColor,
+    color: props.color,
   });
 });
 useResizeObserver(vantaRef, () => {
