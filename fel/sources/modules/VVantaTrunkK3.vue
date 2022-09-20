f3762b64d828aae5dc7bd00aec4aec37b31c660b
Jerry Bruwes <jbruwes@icloud.com>
2022-09-20 14:26:42 +0200

    Update VVantaTrunkK3.vue

--- fel/sources/modules/VVantaTrunkK3.vue
+++ fel/sources/modules/VVantaTrunkK3.vue
@@ -7,6 +7,19 @@ import p5 from "p5";
 import TRUNK from "vanta/dist/vanta.trunk.min";
 import { ref, onMounted, onBeforeUnmount } from "vue";
 import { get, useResizeObserver } from "@vueuse/core";
+const props = defineProps({
+  mouseControls: { default: true, type: Boolean },
+  touchControls: { default: true, type: Boolean },
+  gyroControls: { default: false, type: Boolean },
+  minHeight: { default: 200, type: Number },
+  minWidth: { default: 200, type: Number },
+  scale: { default: 1, type: Number },
+  scaleMobile: { default: 1, type: Number },
+  color: { default: 0x98465f, type: Number },
+  backgroundColor: { default: 0x222426, type: Number },
+  spacing: { default: 0, type: Number },
+  chaos: { default: 1, type: Number },
+});
 const vantaRef = ref(null);
 let vantaEffect;
 onMounted(() => {
@@ -14,6 +27,17 @@ onMounted(() => {
     el: get(vantaRef).$el,
     THREE: THREE,
     p5: p5,
+    mouseControls: props.mouseControls,
+    touchControls: props.touchControls,
+    gyroControls: props.gyroControls,
+    minHeight: props.minHeight,
+    minWidth: props.minWidth,
+    scale: props.scale,
+    scaleMobile: props.scaleMobile,
+    color: props.color,
+    backgroundColor: props.backgroundColor,
+    spacing: props.spacing,
+    chaos: props.chaos,
   });
 });
 useResizeObserver(vantaRef, () => {
