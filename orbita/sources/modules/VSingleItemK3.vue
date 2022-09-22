55befcdebab82d7765ab2416a5b7a00397528db0
Jerry Bruwes <jbruwes@icloud.com>
2022-09-22 15:04:01 +0200

    Create VSingleItemK3.vue

---
+++ orbita/sources/modules/VSingleItemK3.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-single-k3 #="slot"
+    ><v-template-item-k3
+      :animate="animate"
+      :classes="classes"
+      :width="width"
+      :height="height"
+      :title="slot.title"
+      :icon="slot.icon"
+      :href="slot.href"
+      :date="slot.date"
+      :description="slot.description"
+      :variant="variant"
+    ></v-template-item-k3
+  ></v-single-k3>
+</template>
+<script setup>
+import VSingleK3 from "./VSingleK3.vue";
+import VTemplateItemK3 from "./VTemplateItemK3.vue";
+const props = defineProps({
+  classes: String,
+  animate: String,
+  width: [String, Number],
+  height: [String, Number],
+  variant: String,
+});
+</script>
