36a6ea222e2739ef6d9882d7124bef0dd101004d
Jerry Bruwes <jbruwes@icloud.com>
2022-09-22 15:03:23 +0200

    Create VSingleBannerDotsK3.vue

---
+++ orbita/sources/modules/VSingleBannerDotsK3.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-single-k3 #="slot"
+    ><v-vanta-dots-k3
+      :height="height"
+      :width="width"
+      :mouseControls="mouseControls"
+      :touchControls="touchControls"
+      :gyroControls="gyroControls"
+      :minHeight="minHeight"
+      :minWidth="minWidth"
+      :scale="scale"
+      :scaleMobile="scaleMobile"
+      :color="color"
+      :color2="color2"
+      :backgroundColor="backgroundColor"
+      :size="size"
+      :spacing="spacing"
+      :showLines="showLines"
+      ><v-template-banner-k3
+        :animate="animate"
+        :classes="classes"
+        :title="slot.title"
+        :icon="slot.icon"
+        :href="slot.href"
+        :date="slot.date"
+        :description="slot.description"
+        :variant="variant"
+      ></v-template-banner-k3></v-vanta-dots-k3
+  ></v-single-k3>
+</template>
+<script setup>
+import VSingleK3 from "./VSingleK3.vue";
+import VVantaDotsK3 from "./VVantaDotsK3.vue";
+import VTemplateBannerK3 from "./VTemplateBannerK3.vue";
+const props = defineProps({
+  classes: String,
+  animate: String,
+  width: [String, Number],
+  height: [String, Number],
+  variant: String,
+  mouseControls: { default: undefined, type: Boolean },
+  touchControls: { default: undefined, type: Boolean },
+  gyroControls: { default: undefined, type: Boolean },
+  minHeight: Number,
+  minWidth: Number,
+  scale: Number,
+  scaleMobile: Number,
+  color: Number,
+  color2: Number,
+  backgroundColor: Number,
+  size: Number,
+  spacing: Number,
+  showLines: { default: undefined, type: Boolean },
+});
+</script>
