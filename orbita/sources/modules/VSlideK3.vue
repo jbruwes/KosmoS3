769ee20ecfc10a6b6931968d2ba0716a74c25234
Jerry Bruwes <jbruwes@icloud.com>
2022-09-22 15:04:10 +0200

    Create VSlideK3.vue

---
+++ orbita/sources/modules/VSlideK3.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-slide-group show-arrows>
+    <v-slide-group-item v-for="(item, i) in items" :key="i">
+      <slot
+        :item="item"
+        :date="date"
+        :description="description"
+      ></slot>
+    </v-slide-group-item>
+  </v-slide-group>
+</template>
+<script>
+import { mapActions } from "pinia";
+import core from "~/core.js";
+export default {
+  props: {
+    date: { default: false, type: [Boolean, String] },
+    description: { default: true, type: [Boolean, String] },
+    deep: { default: undefined, type: Boolean },
+    length: Number,
+    reveal: { default: undefined, type: Boolean },
+    sort: String,
+    path: String,
+    children: { default: undefined, type: Boolean },
+    selector: { default: "*[@id]", type: String },
+    axe: String,
+  },
+  computed: {
+    items() {
+      return this.getItems(
+        this.deep,
+        this.length,
+        this.reveal,
+        this.sort,
+        this.path,
+        this.children,
+        this.selector,
+        this.axe,
+      );
+    },
+  },
+  methods: { ...mapActions(core, ["getItems"]) },
+};
+</script>
