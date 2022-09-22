4658b6e09b1bf718b7e36a86d225d7d71645dd2c
Jerry Bruwes <jbruwes@icloud.com>
2022-09-22 15:04:19 +0200

    Create VTemplateHeaderK3.vue

---
+++ orbita/sources/modules/VTemplateHeaderK3.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-alert
+    :icon="icon"
+    v-animate-onscroll.repeat="animate"
+    :height="height"
+    :width="width"
+    :class="classes"
+    :href="href"
+    :variant="variant"
+    :title="title"
+    prominent
+    ><v-chip
+      v-if="date"
+      size="x-small"
+      class="mr-1"
+      label
+      variant="outlined"
+      prepend-icon="mdi-calendar"
+      :text="date"
+    ></v-chip
+    >{{ description }}</v-alert
+  >
+</template>
+<script>
+export default {
+  props: {
+    animate: { default: "animate__animated animate__bounce", type: String },
+    classes: String,
+    width: [String, Number],
+    height: [String, Number],
+    title: String,
+    date: String,
+    description: String,
+    icon: String,
+    href: String,
+    variant: { default: "plain", type: String },
+  },
+};
+</script>
