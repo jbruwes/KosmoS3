b04e53ad527cc986f6ffb12f3f6508c3b075bc39
Jerry Bruwes <jbruwes@icloud.com>
2022-09-22 15:03:04 +0200

    Create VMenuK3.vue

---
+++ orbita/sources/modules/VMenuK3.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-menu close-on-click location="start">
+    <template v-slot:activator="{ props }">
+      <v-btn icon="mdi-dots-vertical" v-bind="props"> </v-btn>
+    </template>
+    <v-list nav>
+      <template v-for="(item, i) in items" :key="i">
+        <v-list-subheader
+          v-if="item.visible"
+          :prepend-icon="`mdi-${item.icon}`"
+          :title="getTitle(item)"
+        >
+        </v-list-subheader></template
+    ></v-list>
+  </v-menu>
+</template>
+<script>
+import { mapState, mapActions } from "pinia";
+import core from "~/core.js";
+export default {
+  computed: {
+    items() {
+      return !this.routePath || this.routePath === "/"
+        ? this.treeChildren
+        : this.siblings;
+    },
+    ...mapState(core, ["treeChildren", "siblings", "routePath"]),
+  },
+  methods: { ...mapActions(core, ["getTitle"]) },
+};
+</script>
