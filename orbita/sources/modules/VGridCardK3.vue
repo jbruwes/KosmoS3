9322c52ba65db2af7981ec974c0d2798fac44ea6
Jerry Bruwes <jbruwes@icloud.com>
2022-09-22 15:02:53 +0200

    Create VGridCardK3.vue

---
+++ orbita/sources/modules/VGridCardK3.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-grid-k3 #="slot" selector="*[@id][string(@image)]"
+    ><v-single-card-k3
+      :classes="classes"
+      :item="slot.item"
+      :animate="animate"
+      :date="slot.date"
+      :description="slot.description"
+      :variant="variant"
+      :height="height"
+      :width="width"
+    ></v-single-card-k3
+  ></v-grid-k3>
+</template>
+<script setup>
+import VGridK3 from "./VGridK3.vue";
+import VSingleCardK3 from "./VSingleCardK3.vue";
+const props = defineProps({
+  classes: { default: "fill-height ma-auto", type: String },
+  animate: String,
+  width: [String, Number],
+  height: [String, Number],
+  variant: String,
+});
+</script>
