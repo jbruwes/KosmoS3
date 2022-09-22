41155d75d7bfaabe09d9de9ad6a3f5d9b63ac0f8
Jerry Bruwes <jbruwes@icloud.com>
2022-09-22 15:02:51 +0200

    Create VCarouselK3.vue

---
+++ orbita/sources/modules/VCarouselK3.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-carousel cycle show-arrows="hover" :height="height" :width="width">
+    <v-carousel-item v-for="(item, i) in items" :key="i" :ripple="true">
+      <slot
+        :item="item"
+        :date="date"
+        :description="description"
+      ></slot>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+<script>
+import { mapActions } from "pinia";
+import core from "~/core.js";
+export default {
+  props: {
+    date: { default: false, type: [Boolean, String] },
+    description: { default: true, type: [Boolean, String] },
+    deep: { default: undefined, type: Boolean },
+    length: Number,
+    reveal: { default: undefined, type: Boolean },
+    sort: String,
+    path: String,
+    children: { default: undefined, type: Boolean },
+    selector: { default: "*[@id]", type: String },
+    axe: String,
+  },
+  computed: {
+    items() {
+      return this.getItems(
+        this.deep,
+        this.length,
+        this.reveal,
+        this.sort,
+        this.path,
+        this.children,
+        this.selector,
+        this.axe
+      );
+    },
+  },
+  methods: { ...mapActions(core, ["getItems"]) },
+};
+</script>
