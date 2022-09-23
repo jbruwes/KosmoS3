ebd219fc0040f56422d93d27bb5e2a7f6f430d2c
Jerry Bruwes <jbruwes@icloud.com>
2022-09-23 13:57:16 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,49 @@
 <template>
   <v-app>
+    <v-app-bar app>
+      <v-app-bar-nav-icon
+        variant="text"
+        @click.stop="drawer = !drawer"
+      ></v-app-bar-nav-icon
+      ><v-toolbar-title text="kosmoS3"></v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer
+      app
+      v-model="drawer"
+      :temporary="mobile"
+      :expand-on-hover="!mobile"
+      :rail="!mobile"
+    >
+      <v-list nav :items="items"></v-list>
+    </v-navigation-drawer>
     <v-main>
-      <HelloWorld />
+      <v-container fluid class="fill-height">
+        <router-view></router-view>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-
-  components: {
-    HelloWorld,
+<script setup>
+import { ref } from "vue";
+import { useDisplay } from "vuetify";
+import { get } from "@vueuse/core";
+const { mobile } = useDisplay();
+const drawer = ref(!get(mobile));
+const items = ref([
+  {
+    title: "Home",
+    props: {
+      prependIcon: "mdi-home-outline",
+      to: "/",
+    },
   },
-
-  data: () => ({
-    //
-  }),
-};
+  {
+    title: "About",
+    props: {
+      prependIcon: "mdi-information-outline",
+      to: "/about",
+    },
+  },
+]);
 </script>
