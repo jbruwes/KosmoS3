365fd842c2de4da28ef1ef130ff1c3a5869d5d98
Jerry Bruwes <jbruwes@icloud.com>
2022-09-23 13:56:53 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
+import { createRouter, createMemoryHistory } from "vue-router";
 import { loadFonts } from "./plugins/webfontloader";
+
+import HomeView from "./components/HomeView.vue";
+import AboutView from "./components/AboutView.vue";
 
 loadFonts();
 
-createApp(App).use(vuetify).mount("#app");
+createApp(App)
+  .use(vuetify)
+  .use(
+    createRouter({
+      history: createMemoryHistory(),
+      routes: [
+        { path: "/", component: HomeView },
+        { path: "/about", component: AboutView },
+      ],
+    })
+  )
+  .mount("#app");
