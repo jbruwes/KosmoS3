82776b2747eef6f88081c2af27c18d9ccf27f366
Jerry Bruwes <jbruwes@icloud.com>
2022-09-26 19:08:58 +0200

    Update HomeView.vue

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,82 +1,164 @@
 <template>
   <v-card :image="require('../assets/bg.jpg')" class="fill-height"
     ><v-row class="fill-height" align="center" justify="center"
-      ><v-card class="pa-8 ma-8" :width="640" color="#ffffffee">
-        <v-row
-          ><v-col cols="12" md="6">
-            <div class="text-subtitle-1 text-medium-emphasis mb-1">WEBSITE</div>
-            <v-select
-              label="select saved credential"
-              prepend-inner-icon="mdi-content-save"
-              variant="underlined"
-            ></v-select>
-            <v-text-field
-              label="domain"
-              placeholder="example.com"
-              prepend-inner-icon="mdi-web"
-              variant="underlined"
-            ></v-text-field>
-            <v-text-field
-              label="access key id"
-              prepend-inner-icon="mdi-key"
-              variant="underlined"
-            ></v-text-field>
-            <v-text-field
-              :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-              :type="visible ? 'text' : 'password'"
-              label="secret access key"
-              prepend-inner-icon="mdi-lock"
-              @click:append-inner="visible = !visible"
-              variant="underlined"
-            ></v-text-field> </v-col
-          ><v-col cols="12" md="6">
-            <div class="text-subtitle-1 text-medium-emphasis mb-1">CLOUD</div>
-            <v-select
-              label="cloud provider"
-              prepend-inner-icon="mdi-cloud"
-              variant="underlined"
-              :items="providers"
-              v-model="provider"
-            ></v-select
-            ><v-text-field
-              label="region"
-              prepend-inner-icon="mdi-flag"
-              variant="underlined"
-            ></v-text-field
-            ><v-text-field
-              label="endpoint url"
-              prepend-inner-icon="mdi-database"
-              variant="underlined"
-            ></v-text-field
-            ><v-text-field
-              label="website endpoint url"
-              prepend-inner-icon="mdi-server-network"
-              variant="underlined"
-            ></v-text-field> </v-col
-        ></v-row>
-        <v-row
-          ><v-col cols="12" md="6"
-            ><v-switch
-              label="remember credentials"
-              v-model="remember"
-              color="info"
-            ></v-switch></v-col
-        ></v-row>
-        <v-row
-          ><v-col cols="12"
-            ><v-btn block color="blue" size="large" variant="tonal">
-              Log In
-            </v-btn></v-col
-          ></v-row
+      ><v-card class="pa-8 ma-8" :width="640" color="#ffffffee"
+        ><v-form ref="form" v-model="valid" lazy-validation>
+          <v-row
+            ><v-col cols="12" md="6">
+              <div class="text-subtitle-1 text-medium-emphasis mb-1">
+                WEBSITE
+              </div>
+              <v-select
+                label="select saved credential"
+                prepend-inner-icon="mdi-content-save"
+                variant="underlined"
+                :clearable="true"
+              ></v-select>
+              <v-text-field
+                label="domain"
+                placeholder="example.com"
+                prepend-inner-icon="mdi-web"
+                variant="underlined"
+                :rules="[(v) => !!v || 'Item is required']"
+              ></v-text-field>
+              <v-text-field
+                label="access key id"
+                prepend-inner-icon="mdi-key"
+                variant="underlined"
+                :rules="[(v) => !!v || 'Item is required']"
+              ></v-text-field>
+              <v-text-field
+                :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+                :type="visible ? 'text' : 'password'"
+                label="secret access key"
+                prepend-inner-icon="mdi-lock"
+                variant="underlined"
+                @click:append-inner="visible = !visible"
+                :rules="[(v) => !!v || 'Item is required']"
+              ></v-text-field> </v-col
+            ><v-col cols="12" md="6">
+              <div class="text-subtitle-1 text-medium-emphasis mb-1">CLOUD</div>
+              <v-select
+                v-model="provider"
+                label="cloud provider"
+                prepend-inner-icon="mdi-cloud"
+                variant="underlined"
+                :items="providers"
+                :clearable="true"
+              ></v-select
+              ><v-autocomplete
+                v-model.trim="region"
+                :items="regions"
+                label="region"
+                prepend-inner-icon="mdi-flag"
+                variant="underlined"
+                :disabled="provider === 'yandex'"
+                :hide-no-data="true"
+                :rules="[(v) => !!v || 'Item is required']"
+              ></v-autocomplete
+              ><v-text-field
+                v-model.trim="endpoint"
+                label="endpoint url"
+                prepend-inner-icon="mdi-database"
+                variant="underlined"
+                :disabled="!!provider"
+              ></v-text-field
+              ><v-text-field
+                v-model.trim="wendpoint"
+                label="website endpoint url"
+                prepend-inner-icon="mdi-server-network"
+                variant="underlined"
+                :disabled="!!provider"
+                :rules="[(v) => !!v || 'Item is required']"
+              ></v-text-field> </v-col
+          ></v-row>
+          <v-row
+            ><v-col cols="12" md="6"
+              ><v-switch
+                v-model="remember"
+                label="remember credentials"
+                color="info"
+              ></v-switch></v-col
+          ></v-row>
+          <v-row
+            ><v-col cols="12"
+              ><v-btn
+                block
+                plain
+                size="large"
+                variant="outlined"
+                @click="login"
+              >
+                Log In
+              </v-btn></v-col
+            ></v-row
+          ></v-form
         >
       </v-card></v-row
     ></v-card
   >
 </template>
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
+import { get, set } from "@vueuse/core";
+
 const visible = ref(false);
 const remember = ref(true);
-const providers = ref(["custom...", "aws", "yandex"]);
+const providers = ref(["aws", "yandex"]);
 const provider = ref("");
+const endpoint = ref("");
+const wendpoint = ref("");
+const regions = ref([]);
+const region = ref("");
+const valid = ref(true);
+const form = ref(null);
+watch(provider, (newProvider) => {
+  switch (newProvider) {
+    case "aws":
+      set(regions, [
+        "us-east-2",
+        "us-east-1",
+        "us-west-1",
+        "us-west-2",
+        "af-south-1",
+        "ap-east-1",
+        "ap-southeast-3",
+        "ap-south-1",
+        "ap-northeast-3",
+        "ap-northeast-2",
+        "ap-southeast-1",
+        "ap-southeast-2",
+        "ap-northeast-1",
+        "ca-central-1",
+        "cn-north-1",
+        "cn-northwest-1",
+        "eu-central-1",
+        "eu-west-1",
+        "eu-west-2",
+        "eu-south-1",
+        "eu-west-3",
+        "eu-north-1",
+        "me-south-1",
+        "sa-east-1",
+      ]);
+      set(region, "us-east-1");
+      set(endpoint, "");
+      set(wendpoint, `https://s3.${get(region)}.amazonaws.com`);
+      break;
+    case "yandex":
+      set(regions, ["ru-central1"]);
+      set(region, "ru-central1");
+      set(endpoint, "https://storage.yandexcloud.net");
+      set(wendpoint, "https://website.yandexcloud.net");
+      break;
+    default:
+      set(regions, []);
+      set(region, "");
+      set(endpoint, "");
+      set(wendpoint, "");
+  }
+});
+const login = () => {
+  get(form).validate();
+};
 </script>
