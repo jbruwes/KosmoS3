6b5f7ac7b8860eb2f189ecb8576fd8c6952e49f8
Jerry Bruwes <jbruwes@icloud.com>
2022-09-26 19:09:11 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
 import { createApp } from "vue";
+import { createRouter, createMemoryHistory } from "vue-router";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
-import { createRouter, createMemoryHistory } from "vue-router";
-import { loadFonts } from "./plugins/webfontloader";
-
 import HomeView from "./components/HomeView.vue";
 import AboutView from "./components/AboutView.vue";
-
-loadFonts();
 
 createApp(App)
   .use(vuetify)
