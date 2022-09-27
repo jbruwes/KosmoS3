74a72e56b1ac015d7758307965ec448ec9a33195
Jerry Bruwes <jbruwes@icloud.com>
2022-09-27 08:25:28 +0200

    Update VVantaWavesK3.vue

--- orbita/sources/modules/VVantaWavesK3.vue
+++ orbita/sources/modules/VVantaWavesK3.vue
@@ -7,6 +7,7 @@ import p5 from "p5";
 import WAVES from "vanta/dist/vanta.waves.min";
 import { ref, onMounted, onBeforeUnmount } from "vue";
 import { get, useResizeObserver } from "@vueuse/core";
+
 const props = defineProps({
   mouseControls: { default: true, type: Boolean },
   touchControls: { default: true, type: Boolean },
@@ -26,8 +27,8 @@ let vantaEffect;
 onMounted(() => {
   vantaEffect = WAVES({
     el: get(vantaRef).$el,
-    THREE: THREE,
-    p5: p5,
+    THREE,
+    p5,
     mouseControls: props.mouseControls,
     touchControls: props.touchControls,
     gyroControls: props.gyroControls,
