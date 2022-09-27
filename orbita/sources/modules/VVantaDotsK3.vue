0d6159e66ad4a8c26589182787f33b8201786aa3
Jerry Bruwes <jbruwes@icloud.com>
2022-09-27 08:25:04 +0200

    Update VVantaDotsK3.vue

--- orbita/sources/modules/VVantaDotsK3.vue
+++ orbita/sources/modules/VVantaDotsK3.vue
@@ -7,6 +7,7 @@ import p5 from "p5";
 import DOTS from "vanta/dist/vanta.dots.min";
 import { ref, onMounted, onBeforeUnmount } from "vue";
 import { get, useResizeObserver } from "@vueuse/core";
+
 const props = defineProps({
   mouseControls: { default: true, type: Boolean },
   touchControls: { default: true, type: Boolean },
@@ -27,8 +28,8 @@ let vantaEffect;
 onMounted(() => {
   vantaEffect = DOTS({
     el: get(vantaRef).$el,
-    THREE: THREE,
-    p5: p5,
+    THREE,
+    p5,
     mouseControls: props.mouseControls,
     touchControls: props.touchControls,
     gyroControls: props.gyroControls,
