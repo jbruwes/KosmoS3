70433e99be8e545f90b5e5a220097a612d7b3747
Jerry Bruwes <jbruwes@icloud.com>
2022-09-27 13:01:36 +0200

    Update VTemplateButtonK3.vue

--- orbita/sources/modules/VTemplateButtonK3.vue
+++ orbita/sources/modules/VTemplateButtonK3.vue
@@ -15,12 +15,12 @@
 export default {
   props: {
     animate: { default: "animate__animated animate__bounce", type: String },
-    classes: String,
-    width: [String, Number],
-    height: [String, Number],
-    title: String,
-    icon: String,
-    href: String,
+    classes: { default: undefined, type: String },
+    width: { default: undefined, type: [String, Number] },
+    height: { default: undefined, type: [String, Number] },
+    title: { default: undefined, type: String },
+    icon: { default: undefined, type: String },
+    href: { default: undefined, type: String },
     variant: { default: "flat", type: String },
   },
 };
