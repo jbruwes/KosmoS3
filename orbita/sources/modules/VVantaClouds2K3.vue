8063ebebb4342b4436c529f7ce75d8f09df71d10
Jerry Bruwes <jbruwes@icloud.com>
2022-09-27 08:24:57 +0200

    Update VVantaClouds2K3.vue

--- orbita/sources/modules/VVantaClouds2K3.vue
+++ orbita/sources/modules/VVantaClouds2K3.vue
@@ -7,6 +7,7 @@ import p5 from "p5";
 import CLOUDS2 from "vanta/dist/vanta.clouds2.min";
 import { ref, onMounted, onBeforeUnmount } from "vue";
 import { get, useResizeObserver } from "@vueuse/core";
+
 const props = defineProps({
   mouseControls: { default: true, type: Boolean },
   touchControls: { default: true, type: Boolean },
@@ -27,8 +28,8 @@ let vantaEffect;
 onMounted(() => {
   vantaEffect = CLOUDS2({
     el: get(vantaRef).$el,
-    THREE: THREE,
-    p5: p5,
+    THREE,
+    p5,
     mouseControls: props.mouseControls,
     touchControls: props.touchControls,
     gyroControls: props.gyroControls,
