00c8a32b452076de97517cf7e045037a19c8cd30
Jerry Bruwes <jbruwes@icloud.com>
2022-09-27 13:17:33 +0200

    Update VSingleItemK3.vue

--- orbita/sources/modules/VSingleItemK3.vue
+++ orbita/sources/modules/VSingleItemK3.vue
@@ -18,7 +18,7 @@
 import VSingleK3 from "./VSingleK3.vue";
 import VTemplateItemK3 from "./VTemplateItemK3.vue";
 
-const props = defineProps({
+defineProps({
   classes: { default: undefined, type: String },
   animate: { default: undefined, type: String },
   width: { default: undefined, type: [String, Number] },
