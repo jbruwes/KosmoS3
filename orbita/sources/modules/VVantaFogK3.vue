539247fbaca45fb6627ca2f30a904ed4ac2f4dd3
Jerry Bruwes <jbruwes@icloud.com>
2022-09-27 08:25:07 +0200

    Update VVantaFogK3.vue

--- orbita/sources/modules/VVantaFogK3.vue
+++ orbita/sources/modules/VVantaFogK3.vue
@@ -7,6 +7,7 @@ import p5 from "p5";
 import FOG from "vanta/dist/vanta.fog.min";
 import { ref, onMounted, onBeforeUnmount } from "vue";
 import { get, useResizeObserver } from "@vueuse/core";
+
 const props = defineProps({
   mouseControls: { default: true, type: Boolean },
   touchControls: { default: true, type: Boolean },
@@ -28,8 +29,8 @@ let vantaEffect;
 onMounted(() => {
   vantaEffect = FOG({
     el: get(vantaRef).$el,
-    THREE: THREE,
-    p5: p5,
+    THREE,
+    p5,
     mouseControls: props.mouseControls,
     touchControls: props.touchControls,
     gyroControls: props.gyroControls,
