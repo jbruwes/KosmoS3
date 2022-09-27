6f9c2805c067e52c55ab30a3de92ac87332c2e32
Jerry Bruwes <jbruwes@icloud.com>
2022-09-27 08:25:10 +0200

    Update VVantaGlobeK3.vue

--- orbita/sources/modules/VVantaGlobeK3.vue
+++ orbita/sources/modules/VVantaGlobeK3.vue
@@ -7,6 +7,7 @@ import p5 from "p5";
 import GLOBE from "vanta/dist/vanta.globe.min";
 import { ref, onMounted, onBeforeUnmount } from "vue";
 import { get, useResizeObserver } from "@vueuse/core";
+
 const props = defineProps({
   mouseControls: { default: true, type: Boolean },
   touchControls: { default: true, type: Boolean },
@@ -29,8 +30,8 @@ let vantaEffect;
 onMounted(() => {
   vantaEffect = GLOBE({
     el: get(vantaRef).$el,
-    THREE: THREE,
-    p5: p5,
+    THREE,
+    p5,
     mouseControls: props.mouseControls,
     touchControls: props.touchControls,
     gyroControls: props.gyroControls,
