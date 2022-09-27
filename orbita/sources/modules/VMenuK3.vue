56bddb37e2ef4e93bd89a1d51b228fdfecd77595
Jerry Bruwes <jbruwes@icloud.com>
2022-09-27 12:59:59 +0200

    Update VMenuK3.vue

--- orbita/sources/modules/VMenuK3.vue
+++ orbita/sources/modules/VMenuK3.vue
@@ -16,12 +16,12 @@
 </template>
 <script>
 import { mapState, mapActions } from "pinia";
-import sputnik from "~/sputnik.js";
+import sputnik from "~/sputnik";
 
 export default {
   computed: {
     /**
-     *
+     * @returns {Array} Элементы меню
      */
     items() {
       return !this.routePath || this.routePath === "/"
