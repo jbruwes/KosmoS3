e5370b12bda35ce331164d115770898b19302f80
Jerry Bruwes <jbruwes@icloud.com>
2022-09-29 16:15:00 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import { createRouter, createMemoryHistory } from "vue-router";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import HomeView from "./components/HomeView.vue";
 import AboutView from "./components/AboutView.vue";
+import ContentView from "./components/ContentView.vue";
 
 createApp(App)
   .use(vuetify)
@@ -13,7 +15,9 @@ createApp(App)
       routes: [
         { path: "/", component: HomeView },
         { path: "/about", component: AboutView },
+        { path: "/content", component: ContentView },
       ],
     })
   )
+  .use(createPinia())
   .mount("#app");
