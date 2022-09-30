ef609624dc1536ecddb3e3368558ebe267acfca8
Jerry Bruwes <jbruwes@icloud.com>
2022-09-30 14:20:53 +0200

    Update kosmos3.js

--- src/kosmos3.js
+++ src/kosmos3.js
@@ -1,8 +1,34 @@
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import { get } from "@vueuse/core";
 import { defineStore } from "pinia";
+import S3 from "./s3";
 
 export default defineStore("kosmos3", () => {
-  const io = ref(undefined);
   const auth = ref(false);
-  return { io, auth };
+  const bucket = ref("");
+  const accessKeyId = ref("");
+  const secretAccessKey = ref("");
+  const endpoint = ref("");
+  const wendpoint = ref("");
+  const region = ref("");
+  const s3 = computed(
+    () =>
+      new S3(
+        get(accessKeyId),
+        get(secretAccessKey),
+        get(bucket),
+        get(region),
+        get(endpoint).replace(/\/$/, "")
+      )
+  );
+  return {
+    s3,
+    auth,
+    bucket,
+    accessKeyId,
+    secretAccessKey,
+    endpoint,
+    wendpoint,
+    region,
+  };
 });
