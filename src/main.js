ef3374812c954b665f1539b83f671c6608e69da5
Jerry Bruwes <jbruwes@icloud.com>
2022-09-30 14:21:03 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import vuetify from "./plugins/vuetify";
 import HomeView from "./components/HomeView.vue";
 import AboutView from "./components/AboutView.vue";
 import ContentView from "./components/ContentView.vue";
+import TemplateView from "./components/TemplateView.vue";
+import CssView from "./components/CssView.vue";
+import JsView from "./components/JsView.vue";
+import SettingsView from "./components/SettingsView.vue";
 
 createApp(App)
   .use(vuetify)
@@ -16,6 +20,10 @@ createApp(App)
         { path: "/", component: HomeView },
         { path: "/about", component: AboutView },
         { path: "/content", component: ContentView },
+        { path: "/template", component: TemplateView },
+        { path: "/css", component: CssView },
+        { path: "/js", component: JsView },
+        { path: "/settings", component: SettingsView },
       ],
     })
   )
