607a4c0604af9ea345f14c5d6797fa9680486da7
Jerry Bruwes <jbruwes@icloud.com>
2022-09-30 14:19:23 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,10 @@
         variant="text"
         @click.stop="drawer = !drawer"
       ></v-app-bar-nav-icon
-      ><v-toolbar-title text="kosmoS3"></v-toolbar-title>
+      ><v-toolbar-title
+        ><v-icon icon="mdi-rocket-launch" class="mr-2"></v-icon
+        >kosmoS3</v-toolbar-title
+      >
     </v-app-bar>
     <v-navigation-drawer
       v-model="drawer"
@@ -25,28 +28,84 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed, watch } from "vue";
 import { useDisplay } from "vuetify";
 import { get } from "@vueuse/core";
+import { storeToRefs } from "pinia";
+import { useRouter } from "vue-router";
+import kosmos3 from "./kosmos3";
 
+const router = useRouter();
+const store = kosmos3();
+const { auth } = storeToRefs(store);
 const { mobile } = useDisplay();
 const drawer = ref(!get(mobile));
-const items = ref([
-  {
-    title: "Home",
-    props: {
-      prependIcon: "mdi-home-outline",
-      to: "/",
-    },
-  },
-  {
-    title: "About",
-    props: {
-      prependIcon: "mdi-information-outline",
-      to: "/about",
-    },
-  },
-]);
+const items = computed(() =>
+  get(auth)
+    ? [
+        {
+          title: "Content",
+          props: {
+            prependIcon: "mdi-book-open-page-variant",
+            to: "/content",
+          },
+        },
+        {
+          title: "Template",
+          props: {
+            prependIcon: "mdi-language-html5",
+            to: "/template",
+          },
+        },
+        {
+          title: "CSS",
+          props: {
+            prependIcon: "mdi-language-css3",
+            to: "/css",
+          },
+        },
+        {
+          title: "JavaScript",
+          props: {
+            prependIcon: "mdi-language-javascript",
+            to: "/js",
+          },
+        },
+        {
+          title: "Settings",
+          props: {
+            prependIcon: "mdi-cog",
+            to: "/settings",
+          },
+        },
+        {
+          title: "Logout",
+          props: {
+            prependIcon: "mdi-logout-variant",
+            to: "/",
+          },
+        },
+      ]
+    : [
+        {
+          title: "Login",
+          props: {
+            prependIcon: "mdi-login-variant",
+            to: "/",
+          },
+        },
+        {
+          title: "About",
+          props: {
+            prependIcon: "mdi-information-outline",
+            to: "/about",
+          },
+        },
+      ]
+);
+watch(auth, (newAuth) => {
+  if (newAuth) router.push("/content");
+});
 </script>
 <style>
 html {
