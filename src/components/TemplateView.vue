e7854a19daa86cc16854a5623643d4a680178adb
Jerry Bruwes <jbruwes@icloud.com>
2022-10-04 15:15:07 +0200

    Update TemplateView.vue

--- src/components/TemplateView.vue
+++ src/components/TemplateView.vue
@@ -1,1 +1,10 @@
 <template><div>TemplateView</div></template>
+<script setup>
+import { set } from "@vueuse/core";
+import { storeToRefs } from "pinia";
+import kosmos3 from "@/kosmos3";
+
+const store = kosmos3();
+const { panel } = storeToRefs(store);
+set(panel, null);
+</script>
