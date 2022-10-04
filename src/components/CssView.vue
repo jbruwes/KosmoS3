9e9ff9d92fd52b339f53732978052663009698f5
Jerry Bruwes <jbruwes@icloud.com>
2022-10-04 15:14:52 +0200

    Update CssView.vue

--- src/components/CssView.vue
+++ src/components/CssView.vue
@@ -1,1 +1,10 @@
 <template><div>CssView</div></template>
+<script setup>
+import { set } from "@vueuse/core";
+import { storeToRefs } from "pinia";
+import kosmos3 from "@/kosmos3";
+
+const store = kosmos3();
+const { panel } = storeToRefs(store);
+set(panel, null);
+</script>
