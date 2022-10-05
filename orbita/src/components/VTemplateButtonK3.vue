16c513ae206f813381383ce5d7abef3781e23698
Jerry Bruwes <jbruwes@icloud.com>
2022-10-05 16:09:22 +0200

    Create VTemplateButtonK3.vue

---
+++ orbita/src/components/VTemplateButtonK3.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-btn
+    v-animate-onscroll.repeat="animate"
+    class="text-none"
+    :variant="variant"
+    :height="height"
+    :width="width"
+    :class="classes"
+    :href="href"
+    :prepend-icon="icon"
+    >{{ title }}</v-btn
+  >
+</template>
+<script>
+export default {
+  props: {
+    animate: { default: "animate__animated animate__bounce", type: String },
+    classes: { default: undefined, type: String },
+    width: { default: undefined, type: [String, Number] },
+    height: { default: undefined, type: [String, Number] },
+    title: { default: undefined, type: String },
+    icon: { default: undefined, type: String },
+    href: { default: undefined, type: String },
+    variant: { default: "flat", type: String },
+  },
+};
+</script>
