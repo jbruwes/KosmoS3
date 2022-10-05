8df18fad62578ee10510b4c8f4ad36266dd3ca64
Jerry Bruwes <jbruwes@icloud.com>
2022-10-05 16:08:03 +0200

    Create VListItemK3.vue

---
+++ orbita/src/components/VListItemK3.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-list-k3 #="slot"
+    ><v-single-item-k3
+      :classes="classes"
+      :item="slot.item"
+      :animate="animate"
+      :date="slot.date"
+      :description="slot.description"
+      :variant="variant"
+      :height="height"
+      :width="width"
+    ></v-single-item-k3
+  ></v-list-k3>
+</template>
+<script setup>
+import VListK3 from "./VListK3.vue";
+import VSingleItemK3 from "./VSingleItemK3.vue";
+
+defineProps({
+  classes: { default: "ma-4", type: String },
+  animate: { default: undefined, type: String },
+  width: { default: undefined, type: [String, Number] },
+  height: { default: undefined, type: [String, Number] },
+  variant: { default: undefined, type: String },
+});
+</script>
