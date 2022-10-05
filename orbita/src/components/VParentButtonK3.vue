daa740db3df308b67f4e53aa0acb3dd047c9674c
Jerry Bruwes <jbruwes@icloud.com>
2022-10-05 16:08:17 +0200

    Create VParentButtonK3.vue

---
+++ orbita/src/components/VParentButtonK3.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-single-button-k3 selector="*[@id]" axe="../parent"></v-single-button-k3>
+</template>
+<script setup>
+import VSingleButtonK3 from "./VSingleButtonK3.vue";
+</script>
