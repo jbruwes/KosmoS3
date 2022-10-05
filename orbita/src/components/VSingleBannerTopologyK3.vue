0a8e7f8ed4c2d36e9189f7f6cc5880bad266b51a
Jerry Bruwes <jbruwes@icloud.com>
2022-10-05 16:08:49 +0200

    Create VSingleBannerTopologyK3.vue

---
+++ orbita/src/components/VSingleBannerTopologyK3.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-single-k3 #="slot"
+    ><v-vanta-topology-k3
+      :height="height"
+      :width="width"
+      :mouse-controls="mouseControls"
+      :touch-controls="touchControls"
+      :gyro-controls="gyroControls"
+      :min-height="minHeight"
+      :min-width="minWidth"
+      :scale="scale"
+      :scale-mobile="scaleMobile"
+      :color="color"
+      :background-color="backgroundColor"
+      ><v-template-banner-k3
+        :inner-color="innerColor"
+        :animate="animate"
+        :classes="classes"
+        :title="slot.title"
+        :icon="slot.icon"
+        :href="slot.href"
+        :date="slot.date"
+        :description="slot.description"
+        :variant="variant"
+      ></v-template-banner-k3></v-vanta-topology-k3
+  ></v-single-k3>
+</template>
+<script setup>
+import VSingleK3 from "./VSingleK3.vue";
+import VVantaTopologyK3 from "./VVantaTopologyK3.vue";
+import VTemplateBannerK3 from "./VTemplateBannerK3.vue";
+
+defineProps({
+  classes: { default: undefined, type: String },
+  innerColor: { default: undefined, type: String },
+  animate: { default: undefined, type: String },
+  width: { default: undefined, type: [String, Number] },
+  height: { default: undefined, type: [String, Number] },
+  variant: { default: undefined, type: String },
+  mouseControls: { default: undefined, type: Boolean },
+  touchControls: { default: undefined, type: Boolean },
+  gyroControls: { default: undefined, type: Boolean },
+  minHeight: { default: undefined, type: Number },
+  minWidth: { default: undefined, type: Number },
+  scale: { default: undefined, type: Number },
+  scaleMobile: { default: undefined, type: Number },
+  color: { default: undefined, type: Number },
+  backgroundColor: { default: undefined, type: Number },
+});
+</script>
