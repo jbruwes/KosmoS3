ff7d310129dc8ec8a4414d64082214dc269f186b
Jerry Bruwes <jbruwes@icloud.com>
2022-10-05 16:09:31 +0200

    Create VTemplateItemK3.vue

---
+++ orbita/src/components/VTemplateItemK3.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-list-item
+    v-animate-onscroll.repeat="animate"
+    :height="height"
+    :width="width"
+    :class="classes"
+    :href="href"
+    :prepend-icon="icon"
+    :title="title"
+    :subtitle="description"
+    :variant="variant"
+    ><v-chip
+      v-if="date"
+      size="x-small"
+      label
+      variant="outlined"
+      prepend-icon="mdi-calendar"
+      :text="date"
+      class="mr-1"
+    ></v-chip>
+  </v-list-item>
+</template>
+<script>
+export default {
+  props: {
+    animate: { default: "animate__animated animate__bounce", type: String },
+    classes: { default: undefined, type: String },
+    width: { default: undefined, type: [String, Number] },
+    height: { default: undefined, type: [String, Number] },
+    title: { default: undefined, type: String },
+    date: { default: undefined, type: String },
+    description: { default: undefined, type: String },
+    icon: { default: undefined, type: String },
+    href: { default: undefined, type: String },
+    variant: { default: undefined, type: String },
+  },
+};
+</script>
