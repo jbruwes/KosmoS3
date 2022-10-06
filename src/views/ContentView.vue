a410e77f8233574001df1d44af2a352da6a8b13b
Jerry Bruwes <jbruwes@icloud.com>
2022-10-06 14:36:23 +0200

    Update ContentView.vue

--- src/views/ContentView.vue
+++ src/views/ContentView.vue
@@ -23,220 +23,9 @@
   >
   <v-window v-model="edit" class="tox-tinymce fill-height"
     ><v-window-item class="fill-height" value="visual">
-      <editor
-        v-model="content"
-        :init="{
-          plugins:
-            'preview searchreplace autolink autosave directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount charmap quickbars emoticons',
-          menubar: 'file edit view insert format tools table',
-          toolbar:
-            'undo redo | rlink | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview print | insertfile image media template link anchor codesample | ltr rtl',
-          content_css_cors: true,
-          skin: false,
-          content_css: 'index.cdn.css,index.css',
-          content_style: `${contentUiSkinCss.toString()}\n${contentCss.toString()}`,
-          document_base_url: base,
-          //file_picker_types: 'image media file',
-          quickbars_insert_toolbar: 'template',
-          body_class: 'pa-2 ma-0',
-          valid_elements: '*[*]',
-          protect: [/<v-.+(<\/v-.+?>)/g],
-          branding: false,
-          browser_spellcheck: true,
-          //convert_urls: false,
-          //relative_urls: false,
-          //remove_script_host: false,
-          image_advtab: true,
-          image_caption: true,
-          image_title: true,
-          //images_reuse_filename: true,
-          resize: false,
-          promotion: false,
-          templates: [
-            {
-              title: 'breadcrumbs',
-              description: 'v-breadcrumbs-k3',
-              content: '<v-breadcrumbs-k3></v-breadcrumbs-k3>',
-            },
-            {
-              title: 'carousel banner',
-              description: 'v-carousel-banner-k3',
-              content: '<v-carousel-banner-k3></v-carousel-banner-k3>',
-            },
-            {
-              title: 'grid card',
-              description: 'v-grid-card-k3',
-              content: '<v-grid-card-k3></v-grid-card-k3>',
-            },
-            {
-              title: 'grid icon',
-              description: 'v-grid-icon-k3',
-              content: '<v-grid-icon-k3></v-grid-icon-k3>',
-            },
-            {
-              title: 'list item',
-              description: 'v-list-item-k3',
-              content: '<v-list-item-k3></v-list-item-k3>',
-            },
-            {
-              title: 'pagination',
-              description: 'v-pagination-k3',
-              content: '<v-pagination-k3></v-pagination-k3>',
-            },
-            {
-              title: 'parent button',
-              description: 'v-parent-button-k3',
-              content: '<v-parent-button-k3></v-parent-button-k3>',
-            },
-            {
-              title: 'single banner',
-              description: 'v-single-banner-k3',
-              content: '<v-single-banner-k3></v-single-banner-k3>',
-            },
-            {
-              title: 'single banner birds',
-              description: 'v-single-banner-birds-k3',
-              content: '<v-single-banner-birds-k3></v-single-banner-birds-k3>',
-            },
-            {
-              title: 'single banner cells',
-              description: 'v-single-banner-cells-k3',
-              content: '<v-single-banner-cells-k3></v-single-banner-cells-k3>',
-            },
-            {
-              title: 'single banner clouds',
-              description: 'v-single-banner-clouds-k3',
-              content:
-                '<v-single-banner-clouds-k3></v-single-banner-clouds-k3>',
-            },
-            {
-              title: 'single banner clouds2',
-              description: 'v-single-banner-clouds2-k3',
-              content:
-                '<v-single-banner-clouds2-k3></v-single-banner-clouds2-k3>',
-            },
-            {
-              title: 'single banner dots',
-              description: 'v-single-banner-dots-k3',
-              content: '<v-single-banner-dots-k3></v-single-banner-dots-k3>',
-            },
-            {
-              title: 'single banner fog',
-              description: 'v-single-banner-fog-k3',
-              content: '<v-single-banner-fog-k3></v-single-banner-fog-k3>',
-            },
-            {
-              title: 'single banner globe',
-              description: 'v-single-banner-globe-k3',
-              content: '<v-single-banner-globe-k3></v-single-banner-globe-k3>',
-            },
-            {
-              title: 'single banner halo',
-              description: 'v-single-banner-halo-k3',
-              content: '<v-single-banner-halo-k3></v-single-banner-halo-k3>',
-            },
-            {
-              title: 'single banner net',
-              description: 'v-single-banner-net-k3',
-              content: '<v-single-banner-net-k3></v-single-banner-net-k3>',
-            },
-            {
-              title: 'single banner rings',
-              description: 'v-single-banner-rings-k3',
-              content: '<v-single-banner-rings-k3></v-single-banner-rings-k3>',
-            },
-            {
-              title: 'single banner ripple',
-              description: 'v-single-banner-ripple-k3',
-              content:
-                '<v-single-banner-ripple-k3></v-single-banner-ripple-k3>',
-            },
-            {
-              title: 'single banner topology',
-              description: 'v-single-banner-topology-k3',
-              content:
-                '<v-single-banner-topology-k3></v-single-banner-topology-k3>',
-            },
-            {
-              title: 'single banner trunk',
-              description: 'v-single-banner-trunk-k3',
-              content: '<v-single-banner-trunk-k3></v-single-banner-trunk-k3>',
-            },
-            {
-              title: 'single banner waves',
-              description: 'v-single-banner-waves-k3',
-              content: '<v-single-banner-waves-k3></v-single-banner-waves-k3>',
-            },
-            {
-              title: 'single button',
-              description: 'v-single-button-k3',
-              content: '<v-single-button-k3></v-single-button-k3>',
-            },
-            {
-              title: 'single card',
-              description: 'v-single-card-k3',
-              content: '<v-single-card-k3></v-single-card-k3>',
-            },
-            {
-              title: 'single header',
-              description: 'v-single-header-k3',
-              content: '<v-single-header-k3></v-single-header-k3>',
-            },
-            {
-              title: 'single icon',
-              description: 'v-single-icon-k3',
-              content: '<v-single-icon-k3></v-single-icon-k3>',
-            },
-            {
-              title: 'single item',
-              description: 'v-single-item-k3',
-              content: '<v-single-item-k3></v-single-item-k3>',
-            },
-            {
-              title: 'slide card',
-              description: 'v-slide-card-k3',
-              content: '<v-slide-card-k3></v-slide-card-k3>',
-            },
-            {
-              title: 'slide icon',
-              description: 'v-slide-icon-k3',
-              content: '<v-slide-icon-k3></v-slide-icon-k3>',
-            },
-          ],
-          link_class_list: [
-            {
-              title: 'None',
-              value: '',
-            },
-            {
-              title: 'v-btn',
-              value: 'v-btn',
-            },
-          ],
-          image_class_list: [
-            {
-              title: 'None',
-              value: '',
-            },
-            {
-              title: 'v-img',
-              value: 'v-img',
-            },
-          ],
-          table_class_list: [
-            {
-              title: 'None',
-              value: '',
-            },
-            {
-              title: 'v-table',
-              value: 'v-table',
-            },
-          ],
-          table_cell_class_list: [],
-          table_row_class_list: [],
-        }"
-      ></editor></v-window-item
+      <v-tinymce-editor
+        v-model:value="content"
+      ></v-tinymce-editor></v-window-item
     ><v-window-item class="fill-height" value="source">
       <v-ace-editor
         v-model:value="content"
@@ -258,7 +47,6 @@
 </template>
 
 <script setup>
-import Editor from "@tinymce/tinymce-vue";
 import { VAceEditor } from "vue3-ace-editor";
 import "ace-builds/webpack-resolver";
 import { ref } from "vue";
@@ -266,11 +54,10 @@ import { useDisplay } from "vuetify";
 import { get, set } from "@vueuse/core";
 import { storeToRefs } from "pinia";
 import kosmos3 from "@/kosmos3";
-import contentUiSkinCss from "!!raw-loader!tinymce/skins/ui/oxide/content.min.css"; // eslint-disable-line
-import contentCss from "!!raw-loader!tinymce/skins/content/default/content.min.css"; // eslint-disable-line
+import VTinymceEditor from "@/components/VTinymceEditor.vue";
 
 const store = kosmos3();
-const { base, panel } = storeToRefs(store);
+const { panel } = storeToRefs(store);
 const { mobile } = useDisplay();
 set(panel, !get(mobile));
 const edit = ref("visual");
