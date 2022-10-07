1d0834c02d7995c43b957fc8d22f65a1a3bc995a
Jerry Bruwes <jbruwes@icloud.com>
2022-10-07 12:26:49 +0200

    Create VSourceCode.vue

---
+++ src/components/VSourceCode.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-ace-editor
+    v-model:value="content"
+    lang="html"
+    theme="chrome"
+    style="height: 100%"
+  ></v-ace-editor>
+</template>
+
+<script setup>
+import { VAceEditor } from "vue3-ace-editor";
+import "ace-builds/webpack-resolver";
+import { storeToRefs } from "pinia";
+import kosmos3 from "@/kosmos3";
+
+const store = kosmos3();
+const { content } = storeToRefs(store);
+</script>
