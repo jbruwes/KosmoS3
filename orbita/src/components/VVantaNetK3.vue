53cfac919540feeeeced5f27a4983609d1df5e4e
Jerry Bruwes <jbruwes@icloud.com>
2022-10-10 07:51:45 +0200

    Update VVantaNetK3.vue

--- orbita/src/components/VVantaNetK3.vue
+++ orbita/src/components/VVantaNetK3.vue
@@ -9,19 +9,13 @@ import { ref, onMounted, onBeforeUnmount } from "vue";
 import { get, useResizeObserver } from "@vueuse/core";
 
 const props = defineProps({
-  mouseControls: { default: true, type: Boolean },
-  touchControls: { default: true, type: Boolean },
-  gyroControls: { default: false, type: Boolean },
-  minHeight: { default: 200, type: Number },
-  minWidth: { default: 200, type: Number },
-  scale: { default: 1, type: Number },
-  scaleMobile: { default: 1, type: Number },
-  color: { default: 0xff3f81, type: Number },
-  backgroundColor: { default: 0x23153c, type: Number },
-  points: { default: 10, type: Number },
-  maxDistance: { default: 20, type: Number },
-  spacing: { default: 15, type: Number },
-  showDots: { default: true, type: Boolean },
+  options: {
+    /**
+     * @returns {object} default options
+     */
+    default: () => ({}),
+    type: Object,
+  },
 });
 const vantaRef = ref(null);
 let vantaEffect;
@@ -30,19 +24,7 @@ onMounted(() => {
     el: get(vantaRef).$el,
     THREE,
     p5,
-    mouseControls: props.mouseControls,
-    touchControls: props.touchControls,
-    gyroControls: props.gyroControls,
-    minHeight: props.minHeight,
-    minWidth: props.minWidth,
-    scale: props.scale,
-    scaleMobile: props.scaleMobile,
-    color: props.color,
-    backgroundColor: props.backgroundColor,
-    points: props.points,
-    maxDistance: props.maxDistance,
-    spacing: props.spacing,
-    showDots: props.showDots,
+    ...props.options,
   });
 });
 useResizeObserver(vantaRef, () => {
