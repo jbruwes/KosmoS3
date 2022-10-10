4e90757bd65c207a1a017ef45632623b65945d5a
Jerry Bruwes <jbruwes@icloud.com>
2022-10-10 07:52:07 +0200

    Update VVantaBirdsK3.vue

--- orbita/src/components/VVantaBirdsK3.vue
+++ orbita/src/components/VVantaBirdsK3.vue
@@ -9,24 +9,13 @@ import { ref, onMounted, onBeforeUnmount } from "vue";
 import { get, useResizeObserver } from "@vueuse/core";
 
 const props = defineProps({
-  mouseControls: { default: true, type: Boolean },
-  touchControls: { default: true, type: Boolean },
-  gyroControls: { default: false, type: Boolean },
-  minHeight: { default: 200, type: Number },
-  minWidth: { default: 200, type: Number },
-  scale: { default: 1, type: Number },
-  scaleMobile: { default: 1, type: Number },
-  backgroundColor: { default: 0x07192f, type: Number },
-  color1: { default: 0xff0000, type: Number },
-  color2: { default: 0x00d1ff, type: Number },
-  colorMode: { default: "varianceGradient", type: String },
-  birdSize: { default: 1, type: Number },
-  wingSpan: { default: 30, type: Number },
-  speedLimit: { default: 5, type: Number },
-  separation: { default: 20, type: Number },
-  alignment: { default: 20, type: Number },
-  cohesion: { default: 20, type: Number },
-  quantity: { default: 5, type: Number },
+  options: {
+    /**
+     * @returns {object} default options
+     */
+    default: () => ({}),
+    type: Object,
+  },
 });
 const vantaRef = ref(null);
 let vantaEffect;
@@ -35,24 +24,7 @@ onMounted(() => {
     el: get(vantaRef).$el,
     THREE,
     p5,
-    mouseControls: props.mouseControls,
-    touchControls: props.touchControls,
-    gyroControls: props.gyroControls,
-    minHeight: props.minHeight,
-    minWidth: props.minWidth,
-    scale: props.scale,
-    scaleMobile: props.scaleMobile,
-    backgroundColor: props.backgroundColor,
-    color1: props.color1,
-    color2: props.color2,
-    colorMode: props.colorMode,
-    birdSize: props.birdSize,
-    wingSpan: props.wingSpan,
-    speedLimit: props.speedLimit,
-    separation: props.separation,
-    alignment: props.alignment,
-    cohesion: props.cohesion,
-    quantity: props.quantity,
+    ...props.options,
   });
 });
 useResizeObserver(vantaRef, () => {
