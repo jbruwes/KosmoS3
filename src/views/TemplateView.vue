1a8df30e9164a5dfe8a8fe87adb85d04ecd0a677
Jerry Bruwes <jbruwes@icloud.com>
2022-10-12 16:57:17 +0200

    Update TemplateView.vue

--- src/views/TemplateView.vue
+++ src/views/TemplateView.vue
@@ -1,10 +1,20 @@
-<template><div>TemplateView</div></template>
+<template>
+  <v-navigation-drawer
+    v-model="panel"
+    location="right"
+    :width="320"
+    :temporary="mobile"
+  ></v-navigation-drawer>
+  <div>TemplateView</div>
+</template>
 <script setup>
-import { set } from "@vueuse/core";
+import { set, get } from "@vueuse/core";
+import { useDisplay } from "vuetify";
 import { storeToRefs } from "pinia";
 import kosmos3 from "@/kosmos3";
 
 const store = kosmos3();
 const { panel } = storeToRefs(store);
-set(panel, null);
+const { mobile } = useDisplay();
+set(panel, !get(mobile));
 </script>
