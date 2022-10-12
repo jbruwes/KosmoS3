cce27ec882778c697e7fc4e6a2b69d7bc13516ff
Jerry Bruwes <jbruwes@icloud.com>
2022-10-12 16:56:15 +0200

    Update VSourceCode.vue

--- src/components/VSourceCode.vue
+++ src/components/VSourceCode.vue
@@ -1,18 +1,20 @@
 <template>
   <v-ace-editor
-    v-model:value="content"
-    lang="html"
+    :value="modelValue"
+    :lang="lang"
     theme="chrome"
     style="height: 100%"
+    @update:value="$emit('update:modelValue', $event)"
   ></v-ace-editor>
 </template>
 
 <script setup>
 import { VAceEditor } from "vue3-ace-editor";
 import "ace-builds/webpack-resolver";
-import { storeToRefs } from "pinia";
-import kosmos3 from "@/kosmos3";
 
-const store = kosmos3();
-const { content } = storeToRefs(store);
+defineProps({
+  lang: { default: "html", type: String },
+  modelValue: { default: "", type: String },
+});
+defineEmits(["update:modelValue"]);
 </script>
