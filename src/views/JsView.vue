ac59cf7b072747fb0bf2a5ac981e03b8ef73a27c
Jerry Bruwes <jbruwes@icloud.com>
2022-10-12 16:57:10 +0200

    Update JsView.vue

--- src/views/JsView.vue
+++ src/views/JsView.vue
@@ -1,10 +1,23 @@
-<template><div>JsView</div></template>
+<template>
+  <v-navigation-drawer
+    v-model="panel"
+    location="right"
+    :width="320"
+    :temporary="mobile"
+  ></v-navigation-drawer>
+  <div class="tox-tinymce fill-height">
+    <v-source-code v-model="javascript" lang="javascript"></v-source-code>
+  </div>
+</template>
 <script setup>
-import { set } from "@vueuse/core";
+import { set, get } from "@vueuse/core";
+import { useDisplay } from "vuetify";
 import { storeToRefs } from "pinia";
 import kosmos3 from "@/kosmos3";
+import VSourceCode from "@/components/VSourceCode.vue";
 
 const store = kosmos3();
-const { panel } = storeToRefs(store);
-set(panel, null);
+const { panel, javascript } = storeToRefs(store);
+const { mobile } = useDisplay();
+set(panel, !get(mobile));
 </script>
