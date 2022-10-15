8417b9e38358fa76896dbd6a51be365d206afd12
Jerry Bruwes <jbruwes@icloud.com>
2022-10-15 14:40:37 +0200

    Update ContentView.vue

--- src/views/ContentView.vue
+++ src/views/ContentView.vue
@@ -1,38 +1,27 @@
-<template>
-  <v-navigation-drawer
-    v-model="panel"
-    location="right"
-    :width="320"
-    :temporary="mobile"
-    ><v-tabs v-model="drawer" :grow="true" density="compact">
-      <v-tab value="1" prepend-icon="mdi-file-tree">Structure</v-tab>
-      <v-tab value="2" prepend-icon="mdi-card-bulleted-settings-outline"
-        >Attributes</v-tab
-      > </v-tabs
-    ><v-window v-model="drawer"
-      ><v-window-item value="1"
-        ><v-container fluid class="fill-height"
-          >tree</v-container
-        ></v-window-item
-      ><v-window-item value="2"
-        ><v-container fluid class="fill-height"
-          >attrs</v-container
-        ></v-window-item
-      ></v-window
-    ></v-navigation-drawer
-  >
-  <div class="tox-tinymce fill-height">
-    <v-tabs v-model="tab" show-arrows grow density="compact">
-      <v-tab prepend-icon="mdi-eye" value="1">Visual</v-tab
-      ><v-tab prepend-icon="mdi-code-tags" value="2">Source</v-tab>
-    </v-tabs>
-    <v-window v-model="tab" class="fill-height"
-      ><v-window-item class="fill-height" value="1">
-        <v-wysiwyg v-model.trim="content"></v-wysiwyg></v-window-item
-      ><v-window-item class="fill-height" value="2"
-        ><v-source-code v-model.trim="content"></v-source-code></v-window-item
-    ></v-window>
-  </div>
+<template lang="pug">
+v-navigation-drawer(
+  v-model="panel",
+  location="right",
+  :width="320",
+  :temporary="mobile"
+)
+  v-tabs(v-model="drawer", :grow="true", density="compact")
+    v-tab(value="1", prepend-icon="mdi-file-tree") Structure
+    v-tab(value="2", prepend-icon="mdi-card-bulleted-settings-outline") Attributes
+  v-window(v-model="drawer")
+    v-window-item(value="1")
+      v-container.fill-height(fluid) tree
+    v-window-item(value="2")
+      v-container.fill-height(fluid) attrs
+.tox-tinymce.fill-height
+  v-tabs(v-model="tab", show-arrows, grow, density="compact")
+    v-tab(value="1", prepend-icon="mdi-eye") Visual
+    v-tab(value="2", prepend-icon="mdi-code-tags") Source
+  v-window.fill-height(v-model="tab")
+    v-window-item.fill-height(value="1")
+      v-wysiwyg(v-model.trim="content")
+    v-window-item.fill-height(value="2")
+      v-source-code(v-model.trim="content")
 </template>
 
 <script setup>
