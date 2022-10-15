2881ec62b0cb603c715b353ebc133272a67af948
Jerry Bruwes <jbruwes@icloud.com>
2022-10-15 14:41:03 +0200

    Update JsView.vue

--- src/views/JsView.vue
+++ src/views/JsView.vue
@@ -1,55 +1,39 @@
-<template>
-  <div class="tox-tinymce fill-height">
-    <v-tabs v-model="tab" show-arrows grow density="compact">
-      <v-tab prepend-icon="mdi-format-list-bulleted" value="1"
-        >JavaScript File URLs</v-tab
-      ><v-tab prepend-icon="mdi-text-box-outline" value="2"
-        >Function and Global Variable Declaration</v-tab
-      ><v-tab prepend-icon="mdi-text-box-multiple-outline" value="3"
-        >Execute when Page Loads</v-tab
-      >
-    </v-tabs>
-    <v-window v-model="tab" class="fill-height"
-      ><v-window-item class="fill-height" value="1"
-        ><v-container fluid>
-          <v-text-field
-            v-for="(url, i) in jsJson"
-            :key="i"
-            v-model="jsJson[i]"
-            variant="underlined"
-            append-inner-icon="mdi-plus"
-            prepend-inner-icon="mdi-minus"
-            @click:prepend-inner="jsJson.length - 1 && jsJson.splice(i, 1)"
-            @click:append-inner="jsJson.splice(i + 1, 0, '')"
-            ><template #append
-              ><v-btn
-                icon="mdi-arrow-up"
-                size="x-small"
-                class="mr-1"
-                @click="i && jsJson.splice(i - 1, 0, ...jsJson.splice(i, 1))"
-              ></v-btn
-              ><v-btn
-                icon="mdi-arrow-down"
-                size="x-small"
-                class="ml-1"
-                @click="
-                  jsJson.length - (i + 1) &&
-                    jsJson.splice(i + 1, 0, ...jsJson.splice(i, 1))
-                "
-              ></v-btn
-            ></template> </v-text-field></v-container></v-window-item
-      ><v-window-item class="fill-height" value="2">
-        <v-source-code
-          v-model.trim="jsJs"
-          lang="javascript"
-        ></v-source-code></v-window-item
-      ><v-window-item class="fill-height" value="3">
-        <v-source-code
-          v-model.trim="indexJs"
-          lang="javascript"
-        ></v-source-code></v-window-item
-    ></v-window>
-  </div>
+<template lang="pug">
+.tox-tinymce.fill-height
+  v-tabs(v-model="tab", show-arrows, grow, density="compact")
+    v-tab(value="1", prepend-icon="mdi-format-list-bulleted") JavaScript File URLs
+    v-tab(value="2", prepend-icon="mdi-text-box-outline") Function and Global Variable Declaration
+    v-tab(value="3", prepend-icon="mdi-text-box-multiple-outline") Execute when Page Loads
+  v-window.fill-height(v-model="tab")
+    v-window-item.fill-height(value="1")
+      v-container(fluid)
+        v-text-field(
+          v-for="(url, i) in jsJson",
+          :key="i",
+          v-model="jsJson[i]",
+          variant="underlined",
+          append-inner-icon="mdi-plus",
+          prepend-inner-icon="mdi-minus",
+          @click:prepend-inner="jsJson.length - 1 && jsJson.splice(i, 1)",
+          @click:append-inner="jsJson.splice(i + 1, 0, '')"
+        )
+          template(#append)
+            v-btn.mr-1(
+              icon="mdi-arrow-up",
+              size="x-small",
+              :disabled="!i",
+              @click="jsJson.splice(i - 1, 0, ...jsJson.splice(i, 1))"
+            )
+            v-btn.ml-1(
+              icon="mdi-arrow-down",
+              size="x-small",
+              :disabled="jsJson.length === i + 1",
+              @click="jsJson.splice(i + 1, 0, ...jsJson.splice(i, 1))"
+            )
+    v-window-item.fill-height(value="2")
+      v-source-code(v-model.trim="jsJs", lang="javascript")
+    v-window-item.fill-height(value="3")
+      v-source-code(v-model.trim="indexJs", lang="javascript")
 </template>
 <script setup>
 import { ref } from "vue";
