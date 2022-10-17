14ec5c42f576699f7e3abcead050c5dc80775f68
Jerry Bruwes <jbruwes@icloud.com>
2022-10-17 17:32:35 +0200

    Update CssView.vue

--- src/views/CssView.vue
+++ src/views/CssView.vue
@@ -1,36 +1,36 @@
 <template lang="pug">
 .tox-tinymce.fill-height
   v-tabs(v-model="tab", show-arrows, grow, density="compact")
-    v-tab(value="1", prepend-icon="mdi-format-list-bulleted") JavaScript File URLs
-    v-tab(value="2", prepend-icon="mdi-text-box-outline") Function and Global Variable Declaration
+    v-tab(value="1", prepend-icon="mdi-format-list-bulleted") CSS File URLs
+    v-tab(value="2", prepend-icon="mdi-text-box-outline") Class Declaration
   v-window.fill-height(v-model="tab")
     v-window-item.fill-height(value="1")
       v-container(fluid)
         v-text-field(
-          v-for="(url, i) in cssCss",
+          v-for="(url, i) in stylesheets",
           :key="i",
-          v-model="cssCss[i]",
+          v-model="stylesheets[i]",
           variant="underlined",
           append-inner-icon="mdi-plus",
           prepend-inner-icon="mdi-minus",
-          @click:prepend-inner="cssCss.length - 1 && cssCss.splice(i, 1)",
-          @click:append-inner="cssCss.splice(i + 1, 0, '')"
+          @click:prepend-inner="stylesheets.length - 1 && stylesheets.splice(i, 1)",
+          @click:append-inner="stylesheets.splice(i + 1, 0, '')"
         )
           template(#append)
             v-btn.mr-1(
               icon="mdi-arrow-up",
               size="x-small",
               :disabled="!i",
-              @click="cssCss.splice(i - 1, 0, ...cssCss.splice(i, 1))"
+              @click="stylesheets.splice(i - 1, 0, ...stylesheets.splice(i, 1))"
             )
             v-btn.ml-1(
               icon="mdi-arrow-down",
               size="x-small",
-              :disabled="cssCss.length === i + 1",
-              @click="cssCss.splice(i + 1, 0, ...cssCss.splice(i, 1))"
+              :disabled="stylesheets.length === i + 1",
+              @click="stylesheets.splice(i + 1, 0, ...stylesheets.splice(i, 1))"
             )
     v-window-item.fill-height(value="2")
-      v-source-code(v-model.trim="indexCss", lang="css")
+      v-source-code(v-model.trim="stylesheet", lang="css")
 </template>
 <script setup>
 import { ref } from "vue";
@@ -40,7 +40,7 @@ import kosmos3 from "@/kosmos3";
 import VSourceCode from "@/components/VSourceCode.vue";
 
 const store = kosmos3();
-const { panel, cssCss, indexCss } = storeToRefs(store);
+const { panel, stylesheets, stylesheet } = storeToRefs(store);
 set(panel, null);
 const tab = ref(1);
 </script>
