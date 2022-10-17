f748eaf3f2bb3aaa775ba1fec81ceb1729e8f3ee
Jerry Bruwes <jbruwes@icloud.com>
2022-10-17 17:28:48 +0200

    Update VSingleBannerK3.vue

--- orbita/src/components/VSingleBannerK3.vue
+++ orbita/src/components/VSingleBannerK3.vue
@@ -1,23 +1,23 @@
-<template>
-  <v-single-k3 #="slot"
-    ><v-parallax
-      :height="height"
-      :width="width"
-      :src="slot.image"
-      :eager="true"
-      :aspect-ratio="1"
-      ><v-template-banner-k3
-        :inner-color="innerColor"
-        :animate="animate"
-        :classes="classes"
-        :title="slot.title"
-        :icon="slot.icon"
-        :href="slot.href"
-        :date="slot.date"
-        :description="slot.description"
-        :variant="variant"
-      ></v-template-banner-k3></v-parallax
-  ></v-single-k3>
+<template lang="pug">
+v-single-k3(#="slot")
+  v-parallax(
+    :height="height",
+    :width="width",
+    :src="slot.image",
+    :eager="true",
+    :aspect-ratio="1"
+  )
+    v-template-banner-k3(
+      :inner-color="innerColor",
+      :animate="animate",
+      :classes="classes",
+      :title="slot.title",
+      :icon="slot.icon",
+      :href="slot.href",
+      :date="slot.date",
+      :description="slot.description",
+      :variant="variant"
+    )
 </template>
 <script setup>
 import VSingleK3 from "./VSingleK3.vue";
