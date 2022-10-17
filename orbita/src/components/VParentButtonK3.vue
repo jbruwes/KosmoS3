dd8e85ae0d35e4afba6afe82b42c4465b4fc31a9
Jerry Bruwes <jbruwes@icloud.com>
2022-10-17 17:27:42 +0200

    Update VParentButtonK3.vue

--- orbita/src/components/VParentButtonK3.vue
+++ orbita/src/components/VParentButtonK3.vue
@@ -1,5 +1,5 @@
-<template>
-  <v-single-button-k3 selector="*[@id]" axe="../parent"></v-single-button-k3>
+<template lang="pug">
+v-single-button-k3(selector="*[@id]", axe="../parent")
 </template>
 <script setup>
 import VSingleButtonK3 from "./VSingleButtonK3.vue";
