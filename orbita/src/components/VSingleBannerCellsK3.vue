bc5d958b88768cbaf8938bea7f00cbea9ff6a9fe
Jerry Bruwes <jbruwes@icloud.com>
2022-10-17 17:28:04 +0200

    Update VSingleBannerCellsK3.vue

--- orbita/src/components/VSingleBannerCellsK3.vue
+++ orbita/src/components/VSingleBannerCellsK3.vue
@@ -1,35 +1,35 @@
-<template>
-  <v-single-k3 #="slot"
-    ><v-vanta-cells-k3
-      :height="height"
-      :width="width"
-      :mouse-controls="mouseControls"
-      :touch-controls="touchControls"
-      :gyro-controls="gyroControls"
-      :min-height="minHeight"
-      :min-width="minWidth"
-      :scale="scale"
-      :scale-mobile="scaleMobile"
-      :color1="color1"
-      :color2="color2"
-      :background-color="backgroundColor"
-      :amplitude-factor="amplitudeFactor"
-      :ring-factor="ringFactor"
-      :rotation-factor="rotationFactor"
-      :size="size"
-      :speed="speed"
-      ><v-template-banner-k3
-        :inner-color="innerColor"
-        :animate="animate"
-        :classes="classes"
-        :title="slot.title"
-        :icon="slot.icon"
-        :href="slot.href"
-        :date="slot.date"
-        :description="slot.description"
-        :variant="variant"
-      ></v-template-banner-k3></v-vanta-cells-k3
-  ></v-single-k3>
+<template lang="pug">
+v-single-k3(#="slot")
+  v-vanta-cells-k3(
+    :height="height",
+    :width="width",
+    :mouse-controls="mouseControls",
+    :touch-controls="touchControls",
+    :gyro-controls="gyroControls",
+    :min-height="minHeight",
+    :min-width="minWidth",
+    :scale="scale",
+    :scale-mobile="scaleMobile",
+    :color1="color1",
+    :color2="color2",
+    :background-color="backgroundColor",
+    :amplitude-factor="amplitudeFactor",
+    :ring-factor="ringFactor",
+    :rotation-factor="rotationFactor",
+    :size="size",
+    :speed="speed"
+  )
+    v-template-banner-k3(
+      :inner-color="innerColor",
+      :animate="animate",
+      :classes="classes",
+      :title="slot.title",
+      :icon="slot.icon",
+      :href="slot.href",
+      :date="slot.date",
+      :description="slot.description",
+      :variant="variant"
+    )
 </template>
 <script setup>
 import VSingleK3 from "./VSingleK3.vue";
