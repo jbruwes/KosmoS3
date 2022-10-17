a67d37a97810366f5fd3a26a39db14e9b98a6eff
Jerry Bruwes <jbruwes@icloud.com>
2022-10-17 17:29:55 +0200

    Update VSlideCardK3.vue

--- orbita/src/components/VSlideCardK3.vue
+++ orbita/src/components/VSlideCardK3.vue
@@ -1,16 +1,15 @@
-<template>
-  <v-slide-k3 #="slot" selector="*[@id][string(@image)]"
-    ><v-single-card-k3
-      :classes="classes"
-      :item="slot.item"
-      :animate="animate"
-      :date="slot.date"
-      :description="slot.description"
-      :variant="variant"
-      :height="height"
-      :width="width"
-    ></v-single-card-k3
-  ></v-slide-k3>
+<template lang="pug">
+v-slide-k3(#="slot", selector="*[@id][string(@image)]")
+  v-single-card-k3(
+    :classes="classes",
+    :item="slot.item",
+    :animate="animate",
+    :date="slot.date",
+    :description="slot.description",
+    :variant="variant",
+    :height="height",
+    :width="width"
+  )
 </template>
 <script setup>
 import VSlideK3 from "./VSlideK3.vue";
