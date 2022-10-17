b2506cb96c207ceecfba36b36e2aff328ec543a5
Jerry Bruwes <jbruwes@icloud.com>
2022-10-17 17:31:18 +0200

    Update VTemplateIconK3.vue

--- orbita/src/components/VTemplateIconK3.vue
+++ orbita/src/components/VTemplateIconK3.vue
@@ -1,38 +1,29 @@
-<template>
-  <v-card
-    v-animate-onscroll.repeat="animate"
-    :height="height"
-    :width="width"
-    :class="classes"
-    :href="href"
-    :variant="variant"
-  >
-    <div class="text-center">
-      <v-icon
-        :icon="icon"
-        style="font-size: calc(var(--v-icon-size-multiplier) * 4em)"
-      ></v-icon>
-    </div>
-    <v-card-item class="justify-center text-center">
-      <div>
-        <div v-if="title" class="text-h6 mb-1">
-          {{ title }}
-        </div>
-        <div v-if="date">
-          <v-chip
-            size="x-small"
-            label
-            variant="outlined"
-            prepend-icon="mdi-calendar"
-            :text="date"
-          ></v-chip>
-        </div>
-      </div>
-    </v-card-item>
-    <v-card-text v-if="description" class="text-center">{{
-      description
-    }}</v-card-text>
-  </v-card>
+<template lang="pug">
+v-card(
+  v-animate-onscroll.repeat="animate",
+  :height="height",
+  :width="width",
+  :class="classes",
+  :href="href",
+  :variant="variant"
+)
+  .text-center
+    v-icon(
+      :icon="icon",
+      style="font-size: calc(var(--v-icon-size-multiplier) * 4em)"
+    )
+  v-card-item.justify-center.text-center
+    div
+      .text-h6.mb-1(v-if="title") {{ title }}
+      div(v-if="date")
+        v-chip(
+          size="x-small",
+          label,
+          variant="outlined",
+          prepend-icon="mdi-calendar",
+          :text="date"
+        )
+  v-card-text.text-center(v-if="description") {{ description }}
 </template>
 <script>
 export default {
