68a76529918cdb06ce9b6f51727704c80ca0abe8
Jerry Bruwes <jbruwes@icloud.com>
2022-10-17 17:27:09 +0200

    Update VMenuK3.vue

--- orbita/src/components/VMenuK3.vue
+++ orbita/src/components/VMenuK3.vue
@@ -1,18 +1,14 @@
-<template>
-  <v-menu close-on-click location="start">
-    <template #activator="{ props }">
-      <v-btn icon="mdi-dots-vertical" v-bind="props"> </v-btn>
-    </template>
-    <v-list nav>
-      <template v-for="(item, i) in items" :key="i">
-        <v-list-subheader
-          v-if="item.visible"
-          :prepend-icon="`mdi-${item.icon}`"
-          :title="getTitle(item)"
-        >
-        </v-list-subheader></template
-    ></v-list>
-  </v-menu>
+<template lang="pug">
+v-menu(close-on-click, location="start")
+  template(#activator="{ props }")
+    v-btn(icon="mdi-dots-vertical", v-bind="props")
+  v-list(nav)
+    template(v-for="(item, i) in items", :key="i")
+      v-list-subheader(
+        v-if="item.visible",
+        :prepend-icon="`mdi-${item.icon}`",
+        :title="getTitle(item)"
+      )
 </template>
 <script>
 import { mapState, mapActions } from "pinia";
