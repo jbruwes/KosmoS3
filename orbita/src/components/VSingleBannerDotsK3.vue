e3c769eadd954c0be0910d00b7516c589abf64b7
Jerry Bruwes <jbruwes@icloud.com>
2022-10-17 17:28:24 +0200

    Update VSingleBannerDotsK3.vue

--- orbita/src/components/VSingleBannerDotsK3.vue
+++ orbita/src/components/VSingleBannerDotsK3.vue
@@ -1,33 +1,33 @@
-<template>
-  <v-single-k3 #="slot"
-    ><v-vanta-dots-k3
-      :height="height"
-      :width="width"
-      :mouse-controls="mouseControls"
-      :touch-controls="touchControls"
-      :gyro-controls="gyroControls"
-      :min-height="minHeight"
-      :min-width="minWidth"
-      :scale="scale"
-      :scale-mobile="scaleMobile"
-      :color="color"
-      :color2="color2"
-      :background-color="backgroundColor"
-      :size="size"
-      :spacing="spacing"
-      :show-lines="showLines"
-      ><v-template-banner-k3
-        :inner-color="innerColor"
-        :animate="animate"
-        :classes="classes"
-        :title="slot.title"
-        :icon="slot.icon"
-        :href="slot.href"
-        :date="slot.date"
-        :description="slot.description"
-        :variant="variant"
-      ></v-template-banner-k3></v-vanta-dots-k3
-  ></v-single-k3>
+<template lang="pug">
+v-single-k3(#="slot")
+  v-vanta-dots-k3(
+    :height="height",
+    :width="width",
+    :mouse-controls="mouseControls",
+    :touch-controls="touchControls",
+    :gyro-controls="gyroControls",
+    :min-height="minHeight",
+    :min-width="minWidth",
+    :scale="scale",
+    :scale-mobile="scaleMobile",
+    :color="color",
+    :color2="color2",
+    :background-color="backgroundColor",
+    :size="size",
+    :spacing="spacing",
+    :show-lines="showLines"
+  )
+    v-template-banner-k3(
+      :inner-color="innerColor",
+      :animate="animate",
+      :classes="classes",
+      :title="slot.title",
+      :icon="slot.icon",
+      :href="slot.href",
+      :date="slot.date",
+      :description="slot.description",
+      :variant="variant"
+    )
 </template>
 <script setup>
 import VSingleK3 from "./VSingleK3.vue";
