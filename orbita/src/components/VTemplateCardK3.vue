3b320cc876f20213c7e37ef2014aed345447d1e2
Jerry Bruwes <jbruwes@icloud.com>
2022-10-17 17:30:48 +0200

    Update VTemplateCardK3.vue

--- orbita/src/components/VTemplateCardK3.vue
+++ orbita/src/components/VTemplateCardK3.vue
@@ -1,51 +1,40 @@
-<template>
-  <v-hover v-slot="{ isHovering, props }">
-    <v-card
-      v-animate-onscroll.repeat="animate"
-      v-bind="props"
-      :height="height"
-      :width="width"
-      :elevation="isHovering ? 6 : undefined"
-      :class="classes"
-      :href="href"
-      :variant="variant"
-    >
-      <v-img :src="image" :aspect-ratio="4 / 3" cover
-        ><v-expand-transition
-          ><div
-            v-if="isHovering"
-            class="d-flex transition-fast-in-slow-out bg-white align-center justify-center position-absolute w-100"
-            style="height: 100%; bottom: 0; opacity: 0.9"
-          >
-            <v-btn
-              variant="outlined"
-              size="x-large"
-              color="grey"
-              :icon="icon"
-              :href="href"
-            ></v-btn></div></v-expand-transition
-      ></v-img>
-      <v-card-item>
-        <div>
-          <div v-if="date" class="mb-1">
-            <v-chip
-              size="x-small"
-              label
-              variant="outlined"
-              prepend-icon="mdi-calendar"
-              :text="date"
-            ></v-chip>
-          </div>
-          <div v-if="title" class="text-subtitle-1">
-            {{ title }}
-          </div>
-        </div>
-      </v-card-item>
-      <v-card-text v-if="description" class="text-caption">{{
-        description
-      }}</v-card-text>
-    </v-card>
-  </v-hover>
+<template lang="pug">
+v-hover(v-slot="{ isHovering, props }")
+  v-card(
+    v-animate-onscroll.repeat="animate",
+    v-bind="props",
+    :height="height",
+    :width="width",
+    :elevation="isHovering ? 6 : undefined",
+    :class="classes",
+    :href="href",
+    :variant="variant"
+  )
+    v-img(:src="image", :aspect-ratio="4 / 3", cover)
+      v-expand-transition
+        .d-flex.transition-fast-in-slow-out.bg-white.align-center.justify-center.position-absolute.w-100(
+          v-if="isHovering",
+          style="height: 100%; bottom: 0; opacity: 0.9"
+        )
+          v-btn(
+            variant="outlined",
+            size="x-large",
+            color="grey",
+            :icon="icon",
+            :href="href"
+          )
+    v-card-item
+      div
+        .mb-1(v-if="date")
+          v-chip(
+            size="x-small",
+            label,
+            variant="outlined",
+            prepend-icon="mdi-calendar",
+            :text="date"
+          )
+        .text-subtitle-1(v-if="title") {{ title }}
+    v-card-text.text-caption(v-if="description") {{ description }}
 </template>
 <script>
 export default {
