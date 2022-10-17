6cb8797f38699a3f058b4860582a4740396c5ea2
Jerry Bruwes <jbruwes@icloud.com>
2022-10-17 17:31:22 +0200

    Update VTemplateItemK3.vue

--- orbita/src/components/VTemplateItemK3.vue
+++ orbita/src/components/VTemplateItemK3.vue
@@ -1,24 +1,23 @@
-<template>
-  <v-list-item
-    v-animate-onscroll.repeat="animate"
-    :height="height"
-    :width="width"
-    :class="classes"
-    :href="href"
-    :prepend-icon="icon"
-    :title="title"
-    :subtitle="description"
-    :variant="variant"
-    ><v-chip
-      v-if="date"
-      size="x-small"
-      label
-      variant="outlined"
-      prepend-icon="mdi-calendar"
-      :text="date"
-      class="mr-1"
-    ></v-chip>
-  </v-list-item>
+<template lang="pug">
+v-list-item(
+  v-animate-onscroll.repeat="animate",
+  :height="height",
+  :width="width",
+  :class="classes",
+  :href="href",
+  :prepend-icon="icon",
+  :title="title",
+  :subtitle="description",
+  :variant="variant"
+)
+  v-chip.mr-1(
+    v-if="date",
+    size="x-small",
+    label,
+    variant="outlined",
+    prepend-icon="mdi-calendar",
+    :text="date"
+  )
 </template>
 <script>
 export default {
