12411e318d3ef06e50dfe3b36274c2454fb7c9e6
Jerry Bruwes <jbruwes@icloud.com>
2022-10-17 17:32:41 +0200

    Update JsView.vue

--- src/views/JsView.vue
+++ src/views/JsView.vue
@@ -3,37 +3,34 @@
   v-tabs(v-model="tab", show-arrows, grow, density="compact")
     v-tab(value="1", prepend-icon="mdi-format-list-bulleted") JavaScript File URLs
     v-tab(value="2", prepend-icon="mdi-text-box-outline") Function and Global Variable Declaration
-    v-tab(value="3", prepend-icon="mdi-text-box-multiple-outline") Execute when Page Loads
   v-window.fill-height(v-model="tab")
     v-window-item.fill-height(value="1")
       v-container(fluid)
         v-text-field(
-          v-for="(url, i) in jsJson",
+          v-for="(url, i) in javascripts",
           :key="i",
-          v-model="jsJson[i]",
+          v-model="javascripts[i]",
           variant="underlined",
           append-inner-icon="mdi-plus",
           prepend-inner-icon="mdi-minus",
-          @click:prepend-inner="jsJson.length - 1 && jsJson.splice(i, 1)",
-          @click:append-inner="jsJson.splice(i + 1, 0, '')"
+          @click:prepend-inner="javascripts.length - 1 && javascripts.splice(i, 1)",
+          @click:append-inner="javascripts.splice(i + 1, 0, '')"
         )
           template(#append)
             v-btn.mr-1(
               icon="mdi-arrow-up",
               size="x-small",
               :disabled="!i",
-              @click="jsJson.splice(i - 1, 0, ...jsJson.splice(i, 1))"
+              @click="javascripts.splice(i - 1, 0, ...javascripts.splice(i, 1))"
             )
             v-btn.ml-1(
               icon="mdi-arrow-down",
               size="x-small",
-              :disabled="jsJson.length === i + 1",
-              @click="jsJson.splice(i + 1, 0, ...jsJson.splice(i, 1))"
+              :disabled="javascripts.length === i + 1",
+              @click="javascripts.splice(i + 1, 0, ...javascripts.splice(i, 1))"
             )
     v-window-item.fill-height(value="2")
-      v-source-code(v-model.trim="jsJs", lang="javascript")
-    v-window-item.fill-height(value="3")
-      v-source-code(v-model.trim="indexJs", lang="javascript")
+      v-source-code(v-model.trim="javascript", lang="javascript")
 </template>
 <script setup>
 import { ref } from "vue";
@@ -43,7 +40,7 @@ import kosmos3 from "@/kosmos3";
 import VSourceCode from "@/components/VSourceCode.vue";
 
 const store = kosmos3();
-const { panel, jsJson, jsJs, indexJs } = storeToRefs(store);
+const { panel, javascripts, javascript } = storeToRefs(store);
 set(panel, null);
 const tab = ref(1);
 </script>
