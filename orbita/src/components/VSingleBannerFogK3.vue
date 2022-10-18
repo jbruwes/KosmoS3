17ff77987be098dde1a73bf341f0463bf3a26b08
Jerry Bruwes <jbruwes@icloud.com>
2022-10-18 08:12:14 +0200

    Update VSingleBannerFogK3.vue

--- orbita/src/components/VSingleBannerFogK3.vue
+++ orbita/src/components/VSingleBannerFogK3.vue
@@ -1,25 +1,8 @@
 <template lang="pug">
 v-single-k3(#="slot")
-  v-vanta-fog-k3(
-    :height="height",
-    :width="width",
-    :mouse-controls="mouseControls",
-    :touch-controls="touchControls",
-    :gyro-controls="gyroControls",
-    :min-height="minHeight",
-    :min-width="minWidth",
-    :scale="scale",
-    :scale-mobile="scaleMobile",
-    :highlight-color="highlightColor",
-    :midtone-color="midtoneColor",
-    :lowlight-color="lowlightColor",
-    :base-color="baseColor",
-    :blur-factor="blurFactor",
-    :speed="speed",
-    :zoom="zoom"
-  )
+  v-vanta-fog-k3(:height="height", :width="width", :options="options")
     v-template-banner-k3(
-      :inner-color="innerColor",
+      :color="color",
       :animate="animate",
       :classes="classes",
       :title="slot.title",
@@ -37,24 +20,11 @@ import VTemplateBannerK3 from "./VTemplateBannerK3.vue";
 
 defineProps({
   classes: { default: undefined, type: String },
-  innerColor: { default: undefined, type: String },
+  color: { default: undefined, type: String },
   animate: { default: undefined, type: String },
   width: { default: undefined, type: [String, Number] },
   height: { default: undefined, type: [String, Number] },
   variant: { default: undefined, type: String },
-  mouseControls: { default: undefined, type: Boolean },
-  touchControls: { default: undefined, type: Boolean },
-  gyroControls: { default: undefined, type: Boolean },
-  minHeight: { default: undefined, type: Number },
-  minWidth: { default: undefined, type: Number },
-  scale: { default: undefined, type: Number },
-  scaleMobile: { default: undefined, type: Number },
-  highlightColor: { default: undefined, type: Number },
-  midtoneColor: { default: undefined, type: Number },
-  lowlightColor: { default: undefined, type: Number },
-  baseColor: { default: undefined, type: Number },
-  blurFactor: { default: undefined, type: Number },
-  speed: { default: undefined, type: Number },
-  zoom: { default: undefined, type: Number },
+  options: { default: undefined, type: Object },
 });
 </script>
