4c262ba3b7d78f2768ce4353c40af6424d98b6f3
Jerry Bruwes <jbruwes@icloud.com>
2022-10-18 08:12:19 +0200

    Update VSingleBannerGlobeK3.vue

--- orbita/src/components/VSingleBannerGlobeK3.vue
+++ orbita/src/components/VSingleBannerGlobeK3.vue
@@ -1,26 +1,8 @@
 <template lang="pug">
 v-single-k3(#="slot")
-  v-vanta-globe-k3(
-    :height="height",
-    :width="width",
-    :mouse-controls="mouseControls",
-    :touch-controls="touchControls",
-    :gyro-controls="gyroControls",
-    :min-height="minHeight",
-    :min-width="minWidth",
-    :scale="scale",
-    :scale-mobile="scaleMobile",
-    :color="color",
-    :color2="color2",
-    :size="size",
-    :background-color="backgroundColor",
-    :points="points",
-    :max-distance="maxDistance",
-    :spacing="spacing",
-    :show-dots="showDots"
-  )
+  v-vanta-globe-k3(:height="height", :width="width", :options="options")
     v-template-banner-k3(
-      :inner-color="innerColor",
+      :color="color",
       :animate="animate",
       :classes="classes",
       :title="slot.title",
@@ -38,25 +20,11 @@ import VTemplateBannerK3 from "./VTemplateBannerK3.vue";
 
 defineProps({
   classes: { default: undefined, type: String },
-  innerColor: { default: undefined, type: String },
+  color: { default: undefined, type: String },
   animate: { default: undefined, type: String },
   width: { default: undefined, type: [String, Number] },
   height: { default: undefined, type: [String, Number] },
   variant: { default: undefined, type: String },
-  mouseControls: { default: undefined, type: Boolean },
-  touchControls: { default: undefined, type: Boolean },
-  gyroControls: { default: undefined, type: Boolean },
-  minHeight: { default: undefined, type: Number },
-  minWidth: { default: undefined, type: Number },
-  scale: { default: undefined, type: Number },
-  scaleMobile: { default: undefined, type: Number },
-  color: { default: undefined, type: Number },
-  color2: { default: undefined, type: Number },
-  size: { default: undefined, type: Number },
-  backgroundColor: { default: undefined, type: Number },
-  points: { default: undefined, type: Number },
-  maxDistance: { default: undefined, type: Number },
-  spacing: { default: undefined, type: Number },
-  showDots: { default: undefined, type: Boolean },
+  options: { default: undefined, type: Object },
 });
 </script>
