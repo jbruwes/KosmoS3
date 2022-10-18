34df8b3a441b14db340a0de7620255abe9380658
Jerry Bruwes <jbruwes@icloud.com>
2022-10-18 08:11:51 +0200

    Update VSingleBannerClouds2K3.vue

--- orbita/src/components/VSingleBannerClouds2K3.vue
+++ orbita/src/components/VSingleBannerClouds2K3.vue
@@ -1,24 +1,8 @@
 <template lang="pug">
 v-single-k3(#="slot")
-  v-vanta-clouds2-k3(
-    :height="height",
-    :width="width",
-    :mouse-controls="mouseControls",
-    :touch-controls="touchControls",
-    :gyro-controls="gyroControls",
-    :min-height="minHeight",
-    :min-width="minWidth",
-    :scale="scale",
-    :scale-mobile="scaleMobile",
-    :background-color="backgroundColor",
-    :sky-color="skyColor",
-    :cloud-color="cloudColor",
-    :light-color="lightColor",
-    :speed="speed",
-    :texture-path="texturePath"
-  )
+  v-vanta-clouds2-k3(:height="height", :width="width", :options="options")
     v-template-banner-k3(
-      :inner-color="innerColor",
+      :color="color",
       :animate="animate",
       :classes="classes",
       :title="slot.title",
@@ -36,23 +20,11 @@ import VTemplateBannerK3 from "./VTemplateBannerK3.vue";
 
 defineProps({
   classes: { default: undefined, type: String },
-  innerColor: { default: undefined, type: String },
+  color: { default: undefined, type: String },
   animate: { default: undefined, type: String },
   width: { default: undefined, type: [String, Number] },
   height: { default: undefined, type: [String, Number] },
   variant: { default: undefined, type: String },
-  mouseControls: { default: undefined, type: Boolean },
-  touchControls: { default: undefined, type: Boolean },
-  gyroControls: { default: undefined, type: Boolean },
-  minHeight: { default: undefined, type: Number },
-  minWidth: { default: undefined, type: Number },
-  scale: { default: undefined, type: Number },
-  scaleMobile: { default: undefined, type: Number },
-  backgroundColor: { default: undefined, type: Number },
-  skyColor: { default: undefined, type: Number },
-  cloudColor: { default: undefined, type: Number },
-  lightColor: { default: undefined, type: Number },
-  speed: { default: undefined, type: Number },
-  texturePath: { default: undefined, type: String },
+  options: { default: undefined, type: Object },
 });
 </script>
