a8f8e5607818d452817ed3cc768b7850f6a2eafb
Jerry Bruwes <jbruwes@icloud.com>
2022-10-18 08:10:39 +0200

    Update VListK3.vue

--- orbita/src/components/VListK3.vue
+++ orbita/src/components/VListK3.vue
@@ -3,40 +3,34 @@ v-list
   template(v-for="item in items")
     slot(:item="item", :date="date", :description="description")
 </template>
-<script>
-import { mapActions } from "pinia";
+<script setup>
+import { computed } from "vue";
 import orbita from "@/orbita";
 
-export default {
-  props: {
-    date: { default: false, type: [Boolean, String] },
-    description: { default: true, type: [Boolean, String] },
-    deep: { default: undefined, type: Boolean },
-    length: { default: undefined, type: Number },
-    reveal: { default: undefined, type: Boolean },
-    sort: { default: undefined, type: String },
-    path: { default: undefined, type: String },
-    children: { default: undefined, type: Boolean },
-    selector: { default: "*[@id]", type: String },
-    axe: { default: undefined, type: String },
-  },
-  computed: {
-    /**
-     * @returns {Array} Элементы списка
-     */
-    items() {
-      return this.getItems(
-        this.deep,
-        this.length,
-        this.reveal,
-        this.sort,
-        this.path,
-        this.children,
-        this.selector,
-        this.axe
-      );
-    },
-  },
-  methods: { ...mapActions(orbita, ["getItems"]) },
-};
+const props = defineProps({
+  date: { default: false, type: [Boolean, String] },
+  description: { default: true, type: [Boolean, String] },
+  deep: { default: undefined, type: Boolean },
+  length: { default: undefined, type: Number },
+  reveal: { default: undefined, type: Boolean },
+  sort: { default: undefined, type: String },
+  path: { default: undefined, type: String },
+  children: { default: undefined, type: Boolean },
+  selector: { default: "*[@id]", type: String },
+  axe: { default: undefined, type: String },
+});
+const store = orbita();
+const { getItems } = store;
+const items = computed(() =>
+  getItems(
+    props.deep,
+    props.length,
+    props.reveal,
+    props.sort,
+    props.path,
+    props.children,
+    props.selector,
+    props.axe
+  )
+);
 </script>
