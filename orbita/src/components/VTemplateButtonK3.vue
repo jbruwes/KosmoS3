d4af17641fc9ba5d246cde3f618d2ec178d018c8
Jerry Bruwes <jbruwes@icloud.com>
2022-10-18 08:15:02 +0200

    Update VTemplateButtonK3.vue

--- orbita/src/components/VTemplateButtonK3.vue
+++ orbita/src/components/VTemplateButtonK3.vue
@@ -9,17 +9,15 @@ v-btn.text-none(
   :prepend-icon="icon"
 ) {{ title }}
 </template>
-<script>
-export default {
-  props: {
-    animate: { default: "animate__animated animate__bounce", type: String },
-    classes: { default: undefined, type: String },
-    width: { default: undefined, type: [String, Number] },
-    height: { default: undefined, type: [String, Number] },
-    title: { default: undefined, type: String },
-    icon: { default: undefined, type: String },
-    href: { default: undefined, type: String },
-    variant: { default: "flat", type: String },
-  },
-};
+<script setup>
+defineProps({
+  animate: { default: "animate__animated animate__bounce", type: String },
+  classes: { default: undefined, type: String },
+  width: { default: undefined, type: [String, Number] },
+  height: { default: undefined, type: [String, Number] },
+  title: { default: undefined, type: String },
+  icon: { default: undefined, type: String },
+  href: { default: undefined, type: String },
+  variant: { default: "flat", type: String },
+});
 </script>
