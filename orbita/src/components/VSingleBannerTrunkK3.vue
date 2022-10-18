53ea912b59cd5da11527f8cda0a6fae4c47db61a
Jerry Bruwes <jbruwes@icloud.com>
2022-10-18 08:12:57 +0200

    Update VSingleBannerTrunkK3.vue

--- orbita/src/components/VSingleBannerTrunkK3.vue
+++ orbita/src/components/VSingleBannerTrunkK3.vue
@@ -1,22 +1,8 @@
 <template lang="pug">
 v-single-k3(#="slot")
-  v-vanta-trunk-k3(
-    :height="height",
-    :width="width",
-    :mouse-controls="mouseControls",
-    :touch-controls="touchControls",
-    :gyro-controls="gyroControls",
-    :min-height="minHeight",
-    :min-width="minWidth",
-    :scale="scale",
-    :scale-mobile="scaleMobile",
-    :color="color",
-    :background-color="backgroundColor",
-    :spacing="spacing",
-    :chaos="chaos"
-  )
+  v-vanta-trunk-k3(:height="height", :width="width", :options="options")
     v-template-banner-k3(
-      :inner-color="innerColor",
+      :color="color",
       :animate="animate",
       :classes="classes",
       :title="slot.title",
@@ -34,21 +20,11 @@ import VTemplateBannerK3 from "./VTemplateBannerK3.vue";
 
 defineProps({
   classes: { default: undefined, type: String },
-  innerColor: { default: undefined, type: String },
+  color: { default: undefined, type: String },
   animate: { default: undefined, type: String },
   width: { default: undefined, type: [String, Number] },
   height: { default: undefined, type: [String, Number] },
   variant: { default: undefined, type: String },
-  mouseControls: { default: undefined, type: Boolean },
-  touchControls: { default: undefined, type: Boolean },
-  gyroControls: { default: undefined, type: Boolean },
-  minHeight: { default: undefined, type: Number },
-  minWidth: { default: undefined, type: Number },
-  scale: { default: undefined, type: Number },
-  scaleMobile: { default: undefined, type: Number },
-  color: { default: undefined, type: Number },
-  backgroundColor: { default: undefined, type: Number },
-  spacing: { default: undefined, type: Number },
-  chaos: { default: undefined, type: Number },
+  options: { default: undefined, type: Object },
 });
 </script>
