225b453a8d1aa9c43019837842c9cb654c2be48a
Jerry Bruwes <jbruwes@icloud.com>
2022-10-21 16:55:03 +0200

    Update index.js

--- orbita/src/index.js
+++ orbita/src/index.js
@@ -28,7 +28,6 @@ import "glightbox/dist/css/glightbox.css";
 import "animate.css/animate.css";
 
 import "@mdi/font/css/materialdesignicons.css";
-import "tailwindcss/dist/tailwind.css";
 import "vuetify/styles";
 import "./styles/index.css";
 
