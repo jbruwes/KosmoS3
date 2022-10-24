38824b689e974a2a2c186fb69e540f9a43bc1a43
Jerry Bruwes <jbruwes@icloud.com>
2022-10-24 19:40:36 +0300

    Update VTemplateBannerK3.vue

--- orbita/src/components/VTemplateBannerK3.vue
+++ orbita/src/components/VTemplateBannerK3.vue
@@ -2,29 +2,29 @@
 v-row.h-100(align="center", justify="center")
   v-card.pa-16.ma-16.text-white(
     v-animate-onscroll.repeat="animate",
-    :href="href",
+    :href="params.href",
     :variant="variant",
     :color="color"
   )
     .text-center
       v-icon(
-        :icon="icon",
+        :icon="params.icon",
         style="font-size: calc(var(--v-icon-size-multiplier) * 4em)"
       )
     v-card-item.justify-center.text-center
       div
-        .text-h4.mb-1(v-if="title") {{ title }}
-        div(v-if="date")
+        .text-h4.mb-1(v-if="params.title") {{ params.title }}
+        div(v-if="params.date")
           v-chip(
             size="x-small",
             label,
             variant="outlined",
             prepend-icon="mdi-calendar",
-            :text="date"
+            :text="params.date"
           )
     v-alert.text-center(
-      v-if="description",
-      :title="description",
+      v-if="params.description",
+      :title="params.description",
       variant="text",
       prominent
     )
@@ -33,11 +33,7 @@ v-row.h-100(align="center", justify="center")
 defineProps({
   color: { default: "#00000033", type: String },
   animate: { default: "animate__animated animate__bounceIn", type: String },
-  title: { default: undefined, type: String },
-  date: { default: undefined, type: String },
-  description: { default: undefined, type: String },
-  icon: { default: undefined, type: String },
-  href: { default: undefined, type: String },
   variant: { default: undefined, type: String },
+  params: { default: undefined, type: Object },
 });
 </script>
