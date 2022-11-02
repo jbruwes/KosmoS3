5ae2bf7630354b40d574baa6a3662993509069d1
Jerry Bruwes <jbruwes@icloud.com>
2022-11-02 17:26:31 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
-  pages: {
-    index: {
-      entry: "src/main.js",
-      title: "kosmoS3",
-    },
-  },
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
     },
+  },
+  devServer: {
+    hot: false,
+    liveReload: true,
   },
 });
