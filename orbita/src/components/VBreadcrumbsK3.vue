322fcf62f496ede33b0ba738383aaea98c78cc01
Jerry Bruwes <jbruwes@icloud.com>
2022-11-10 07:43:13 +0200

    Update VBreadcrumbsK3.vue

--- orbita/src/components/VBreadcrumbsK3.vue
+++ orbita/src/components/VBreadcrumbsK3.vue
@@ -13,23 +13,23 @@ const props = defineProps({
   sort: { default: undefined, type: String },
   path: { default: undefined, type: String },
   children: { default: undefined, type: Boolean },
-  selector: { default: "*[@id]", type: String },
+  selector: { default: undefined, type: String },
   axe: { default: "ancestor-or-self", type: String },
   disabled: { default: true, type: Boolean },
 });
 const store = orbita();
 const { getItems, getTitle, getHref } = store;
 const items = computed(() =>
-  getItems(
-    props.deep,
-    props.length,
-    props.reveal,
-    props.sort,
-    props.path,
-    props.children,
-    props.selector,
-    props.axe
-  ).map((item, index, array) => ({
+  getItems({
+    deep: props.deep,
+    length: props.length,
+    reveal: props.reveal,
+    sort: props.sort,
+    path: props.path,
+    children: props.children,
+    selector: props.selector,
+    axe: props.axe,
+  }).map((item, index, array) => ({
     title: getTitle(item),
     href: getHref(item),
     disabled: (index === array.length - 1 && props.disabled) || !item.visible,
