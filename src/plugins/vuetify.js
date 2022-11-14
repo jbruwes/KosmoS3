8c3b89a9bae7c963cfd4617d467e5c01b993563a
Jerry Bruwes <jbruwes@icloud.com>
2022-11-14 16:00:27 +0200

    Update vuetify.js

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,6 +4,9 @@ import "vuetify/styles";
 
 // Vuetify
 import { createVuetify } from "vuetify";
+import { md1 } from "vuetify/blueprints";
 
-export default createVuetify();
+export default createVuetify({
+  blueprint: md1,
+});
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
