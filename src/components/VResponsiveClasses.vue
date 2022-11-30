8e27386d789086189b4bc97957c9c5570e5b9df5
Jerry Bruwes <jbruwes@icloud.com>
2022-11-30 16:35:33 +0200

    Create VResponsiveClasses.vue

---
+++ src/components/VResponsiveClasses.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+v-container.h-100(fluid)
+</template>
+
+<script setup>
+defineProps({
+  lang: { default: "html", type: String },
+  modelValue: { default: "", type: String },
+});
+</script>
