fa73be7dea9b7c47118347317cd09d788d62abfa
Jerry Bruwes <jbruwes@icloud.com>
2022-12-01 17:26:31 +0200

    Update VResponsiveClasses.vue

--- src/components/VResponsiveClasses.vue
+++ src/components/VResponsiveClasses.vue
@@ -1,10 +1,21 @@
 <template lang="pug">
-v-container.h-100(fluid)
+v-container(fluid)
+  .d-flex.flex-column.align-center.mb-4
+    v-btn-toggle(group, variant="outlined", divided)
+      v-btn(value="xs", size="small") xs
+      v-btn(value="sm", size="small") sm
+      v-btn(value="md", size="small") md
+      v-btn(value="lg", size="small") lg
+      v-btn(value="xl", size="small") xl
+  | margin
+  v-margins
+  v-divider.my-4
+  | padding
+  v-paddings
+  v-divider.my-4
 </template>
 
 <script setup>
-defineProps({
-  lang: { default: "html", type: String },
-  modelValue: { default: "", type: String },
-});
+import VMargins from "./VMargins.vue";
+import VPaddings from "./VPaddings.vue";
 </script>
