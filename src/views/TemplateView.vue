2c556646f644a552aa86b7a5db7766e81da864b0
Jerry Bruwes <jbruwes@icloud.com>
2022-12-02 15:09:26 +0200

    Update TemplateView.vue

--- src/views/TemplateView.vue
+++ src/views/TemplateView.vue
@@ -15,51 +15,19 @@ v-navigation-drawer(
   v-window(v-model="drawer")
     v-window-item(value="1")
       v-container.pa-0(fluid)
-        v-form(ref="form")
-          v-list
-            draggable(v-model="template", item-key="id")
-              template(#item="{ element, index }")
-                v-list-item.px-1.py-0(
-                  :value="element.id",
-                  :active="element.id === tplId",
-                  @click="clickRect(index)",
-                  @blur="element.edit = false"
-                )
-                  template(#prepend)
-                    v-list-item-action
-                      v-checkbox-btn(v-model="element.visible")
-                      v-icon(
-                        v-if="!(element.name === 'content' && template.filter(({ name }) => name === 'content').length === 1)",
-                        @click="delRect(index)"
-                      ) mdi-minus-circle-outline
-                      v-icon(
-                        v-if="element.name === 'content' && template.filter(({ name }) => name === 'content').length === 1"
-                      ) mdi-checkbox-blank-circle-outline
-                  v-text-field(
-                    v-model.trim="element.name",
-                    :readonly="element.id !== tplId || !element.edit",
-                    :disabled="element.name === 'content' && template.filter(({ name }) => name === 'content').length === 1",
-                    cur-prefix="#",
-                    variant="underlined",
-                    :rules="[(v) => !!v || 'Field is required', (v) => !(template.filter(({ name }) => name === v).length - 1) || 'Must be unique']",
-                    @blur="element.edit = false"
-                  )
-                  template(#append)
-                    v-list-item-action
-                      v-icon(@click="addRect(index)") mdi-plus-circle-outline
-                      v-icon mdi-drag-vertical
+        v-template-list
     v-window-item(value="2")
       v-responsive-classes
     v-window-item(value="3")
       v-container(fluid)
         v-switch(
-          v-model="tplItem.fluid",
-          :label="`Type: ${tplItem.fluid ? 'fluid' : 'responsive'}`",
+          v-model="item.fluid",
+          :label="`Type: ${item.fluid ? 'fluid' : 'responsive'}`",
           inset,
           hide-details
         )
         v-combobox(
-          v-model="tplItem.classes",
+          v-model="item.classes",
           label="classes",
           chips,
           closable-chips,
@@ -76,7 +44,7 @@ v-navigation-drawer(
   v-window.h-100(v-model="tab")
     v-window-item.h-100(value="1")
       v-overlay(
-        v-if="tplItem && tplItem.name === 'content'",
+        v-if="item && item.name === 'content'",
         :model-value="true",
         z-index="2",
         contained,
@@ -84,10 +52,10 @@ v-navigation-drawer(
         no-click-animation,
         content-class="w-100 h-100"
       )
-      v-wysiwyg(v-if="tplItem", v-model="tplItem.value")
+      v-wysiwyg(v-if="item", v-model="item.value")
     v-window-item.h-100(value="2")
       v-overlay(
-        v-if="tplItem && tplItem.name === 'content'",
+        v-if="item && item.name === 'content'",
         :model-value="true",
         z-index="2",
         contained,
@@ -95,59 +63,26 @@ v-navigation-drawer(
         no-click-animation,
         content-class="w-100 h-100"
       )
-      v-source-code(v-if="tplItem", v-model="tplItem.value")
+      v-source-code(v-if="item", v-model="item.value")
 </template>
 <script setup>
-import { ref, onMounted, watch, nextTick } from "vue";
+import { ref } from "vue";
 import { useDisplay } from "vuetify";
 import { get, set } from "@vueuse/core";
 import { storeToRefs } from "pinia";
-import draggable from "vuedraggable";
 import kosmos3 from "@/stores/kosmos3";
 import template3 from "@/stores/template3";
 import VWysiwyg from "@/components/VWysiwyg.vue";
 import VSourceCode from "@/components/VSourceCode.vue";
 import VResponsiveClasses from "@/components/VResponsiveClasses.vue";
+import VTemplateList from "@/components/VTemplateList.vue";
 
 const store = kosmos3();
 const localStore = template3();
-const { panel, template } = storeToRefs(store);
-const { calcLayer } = store;
-const { tplId, tplItem } = storeToRefs(localStore);
+const { panel } = storeToRefs(store);
+const { item } = storeToRefs(localStore);
 const { mobile } = useDisplay();
 set(panel, !get(mobile));
 const tab = ref(1);
 const drawer = ref(1);
-const form = ref();
-watch(
-  template,
-  () => {
-    get(form).validate();
-  },
-  { deep: true }
-);
-onMounted(async () => {
-  await nextTick();
-  get(form).validate();
-});
-/** @param {number} index индекс */
-const addRect = (index) => {
-  get(template).splice(index + 1, 0, calcLayer());
-};
-/** @param {number} index индекс */
-const delRect = (index) => {
-  const last = get(template).length - 1;
-  if (last) {
-    get(template).splice(index, 1);
-    const { id } = get(template, index === last ? index - 1 : index);
-    set(tplId, id);
-  }
-};
-/** @param {number} index индекс */
-const clickRect = (index) => {
-  const element = get(template)[index];
-  const { id } = element;
-  if (get(tplId) !== id) set(tplId, id);
-  else element.edit = true;
-};
 </script>
