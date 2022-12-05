a7386d62187473cbe63ba25d2a8bb5de2f01cf52
Jerry Bruwes <jbruwes@icloud.com>
2022-12-05 19:34:17 +0200

    Update VResponsiveClasses.vue

--- src/components/VResponsiveClasses.vue
+++ src/components/VResponsiveClasses.vue
@@ -13,6 +13,12 @@ v-container(fluid)
       v-btn(value="md", size="small") md
       v-btn(value="lg", size="small") lg
       v-btn(value="xl", size="small") xl
+  v-select.mt-4(
+    v-model="display",
+    :items="displays",
+    label="display",
+    clearable
+  )
   | margin
   v-margins
   v-divider.my-4
@@ -22,11 +28,49 @@ v-container(fluid)
 </template>
 
 <script setup>
+import { computed, reactive } from "vue";
 import { storeToRefs } from "pinia";
+import { get, isDefined } from "@vueuse/core";
 import template3 from "@/stores/template3";
 import VMargins from "./VMargins.vue";
 import VPaddings from "./VPaddings.vue";
 
 const localStore = template3();
-const { breakpoint } = storeToRefs(localStore);
+const { breakpoint, item } = storeToRefs(localStore);
+const displays = reactive([
+  "none",
+  "inline",
+  "inline-block",
+  "block",
+  "table",
+  "table-row",
+  "table-cell",
+  "flex",
+  "inline-flex",
+]);
+const display = computed({
+  /** @returns {string} значение параметра */
+  get() {
+    const k = "d";
+    const key = `${k}-${get(breakpoint) || ""}`.replace(/-$/, "");
+    const mask = `(${displays.join("|")})`;
+    return (
+      isDefined(item)
+        ? get(item).classes.find((element) =>
+            new RegExp(`^${key}-${mask}$`).test(element)
+          ) || ""
+        : ""
+    ).replace(new RegExp(`^${key}-`), "");
+  },
+  /** @param {string} value значение параметра */
+  set(value) {
+    const k = "d";
+    const key = `${k}-${get(breakpoint) || ""}`.replace(/-$/, "");
+    const mask = `(${displays.join("|")})`;
+    get(item).classes = get(item).classes.filter(
+      (aclass) => !new RegExp(`^${key}-${mask}$`).test(aclass)
+    );
+    if (value) get(item).classes.push(`${key}-${value}`);
+  },
+});
 </script>
