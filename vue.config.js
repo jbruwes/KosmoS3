56bf6cacf6d7d06b8725bc59e1c2b2513c225f08
Jerry Bruwes <jbruwes@icloud.com>
2022-12-05 19:34:24 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
+  publicPath: "",
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
