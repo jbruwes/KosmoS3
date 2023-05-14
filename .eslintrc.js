c547e8ff54353d0af2b91c19a8d90f018229d06f
Jerry Bruwes <jbruwes@icloud.com>
2023-05-14 14:33:59 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,47 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    browser: true,
+  },
+  globals: {
+    init: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
+    "eslint:recommended",
+    "airbnb-base",
+    "plugin:jsdoc/recommended",
+    "plugin:vue/vue3-recommended",
+    "plugin:vue-pug/vue3-recommended",
+    "plugin:prettier/recommended",
   ],
-}
+  rules: {
+    "jsdoc/require-jsdoc": [
+      "error",
+      {
+        require: {
+          FunctionDeclaration: true,
+          MethodDefinition: true,
+          ClassDeclaration: true,
+          ClassExpression: true,
+          ArrowFunctionExpression: true,
+          FunctionExpression: true,
+        },
+      },
+    ],
+  },
+  plugins: ["jsdoc", "vue"],
+  settings: {
+    "import/resolver": {
+      alias: {
+        map: [["@", "./src"]],
+      },
+    },
+    "import/core-modules": [
+      "vuetify/styles",
+      "vuetify/iconsets/fa",
+      "vuetify/iconsets/mdi",
+      "vuetify/components",
+      "vuetify/directives",
+    ],
+  },
+};
