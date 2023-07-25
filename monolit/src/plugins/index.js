05d085eaf7b45c8e85a808d1200d8e792c44cdbd
Jerry Bruwes <jbruwes@icloud.com>
2023-07-25 17:07:40 +0200

    Update index.js

--- monolit/src/plugins/index.js
+++ monolit/src/plugins/index.js
@@ -5,11 +5,12 @@
  */
 
 // Plugins
-import vuetify from './vuetify'
-import pinia from '../store'
+import Particles from "vue3-particles";
+import VueAnimateOnScroll from "vue3-animate-onscroll";
+import vuetify from "./vuetify";
+import pinia from "../store";
+import head from "../store/head";
 
-export function registerPlugins (app) {
-  app
-    .use(vuetify)
-    .use(pinia)
+export function registerPlugins(app) {
+  app.use(vuetify).use(pinia).use(head).use(Particles).use(VueAnimateOnScroll);
 }
