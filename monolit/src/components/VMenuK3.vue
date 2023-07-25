2846d4539b422276ca8728cea09f8924d9a52830
Jerry Bruwes <jbruwes@icloud.com>
2023-07-25 12:16:18 +0200

    Create VMenuK3.vue

---
+++ monolit/src/components/VMenuK3.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+v-menu(close-on-click, location="start")
+  template(#activator="{ props }")
+    v-btn(icon="mdi-dots-vertical", v-bind="props")
+  v-list(nav)
+    template(v-for="(item, i) in items", :key="i")
+      v-list-subheader(
+        v-if="item.visible",
+        :prepend-icon="`mdi-${item.icon}`",
+        :title="getTitle(item)"
+      )
+</template>
+<script setup>
+import { computed } from "vue";
+import { storeToRefs } from "pinia";
+import { get } from "@vueuse/core";
+import orbita from "@/orbita";
+
+const store = orbita();
+const { treeChildren, siblings, routePath } = storeToRefs(store);
+const { getTitle } = store;
+const items = computed(() =>
+  !get(routePath) || get(routePath) === "/" ? get(treeChildren) : get(siblings),
+);
+</script>
