97a843d04277aed8dd386f2801963d99ef8e5260
Jerry Bruwes <jbruwes@icloud.com>
2023-07-25 12:16:20 +0200

    Create VListK3.vue

---
+++ monolit/src/components/VListK3.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+v-list
+  template(v-for="item in items")
+    slot(:item="item", :date="date", :description="description")
+</template>
+<script setup>
+import { computed } from "vue";
+import orbita from "@/orbita";
+
+const props = defineProps({
+  date: { default: false, type: [Boolean, String] },
+  description: { default: true, type: [Boolean, String] },
+  deep: { default: undefined, type: Boolean },
+  length: { default: undefined, type: Number },
+  reveal: { default: undefined, type: Boolean },
+  sort: { default: undefined, type: String },
+  path: { default: undefined, type: String },
+  children: { default: undefined, type: Boolean },
+  selector: { default: undefined, type: String },
+  axe: { default: undefined, type: String },
+});
+const store = orbita();
+const { getItems } = store;
+const items = computed(() =>
+  getItems({
+    deep: props.deep,
+    length: props.length,
+    reveal: props.reveal,
+    sort: props.sort,
+    path: props.path,
+    children: props.children,
+    selector: props.selector,
+    axe: props.axe,
+  }),
+);
+</script>
