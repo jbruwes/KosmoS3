0a8437e408f59d0d7b935a778f73278c1a6e6309
Jerry Bruwes <jbruwes@icloud.com>
2023-07-25 12:15:51 +0200

    Create VVantaClouds2K3.vue

---
+++ monolit/src/components/VVantaClouds2K3.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+v-sheet(ref="vantaRef")
+  slot
+</template>
+<script setup>
+import * as THREE from "three";
+import p5 from "p5";
+import CLOUDS2 from "vanta/dist/vanta.clouds2.min";
+import { ref, onMounted, onBeforeUnmount } from "vue";
+import { get, useResizeObserver } from "@vueuse/core";
+
+const props = defineProps({
+  options: {
+    /** @returns {object} default options */
+    default: () => ({}),
+    type: Object,
+  },
+});
+const vantaRef = ref(null);
+let vantaEffect;
+onMounted(() => {
+  vantaEffect = CLOUDS2({
+    el: get(vantaRef).$el,
+    THREE,
+    p5,
+    ...props.options,
+  });
+});
+useResizeObserver(vantaRef, () => {
+  if (vantaEffect) vantaEffect.resize();
+});
+onBeforeUnmount(() => {
+  if (vantaEffect) vantaEffect.destroy();
+});
+</script>
