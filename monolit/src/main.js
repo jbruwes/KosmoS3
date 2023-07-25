91781912ed0e5bde826edcb8cecf70992372e89d
Jerry Bruwes <jbruwes@icloud.com>
2023-07-25 17:07:47 +0200

    Update main.js

--- monolit/src/main.js
+++ monolit/src/main.js
@@ -4,6 +4,37 @@
  * Bootstraps Vuetify and other plugins then mounts the App`
  */
 
+import "@fontsource/arsenal";
+import "@fontsource/bad-script";
+import "@fontsource/caveat";
+import "@fontsource/comfortaa";
+import "@fontsource/cormorant-garamond";
+import "@fontsource/cormorant-infant";
+import "@fontsource/cormorant-sc";
+import "@fontsource/cormorant-unicase";
+import "@fontsource/cormorant";
+import "@fontsource/jura";
+import "@fontsource/marck-script";
+import "@fontsource/montserrat";
+import "@fontsource/montserrat-alternates";
+import "@fontsource/open-sans-condensed";
+import "@fontsource/open-sans";
+import "@fontsource/oswald";
+import "@fontsource/pattaya";
+import "@fontsource/poiret-one";
+import "@fontsource/roboto-condensed";
+import "@fontsource/roboto-mono";
+import "@fontsource/roboto-slab";
+import "@fontsource/roboto";
+import "@fontsource/rubik-mono-one";
+import "@fontsource/rubik";
+import "@fontsource/tenor-sans";
+
+import "glightbox/dist/css/glightbox.css";
+import "animate.css/animate.css";
+
+import "./styles/index.css";
+
 // Components
 import App from './App.vue'
 
