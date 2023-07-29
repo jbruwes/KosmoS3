81a641f6abcc527ff12fcca9499489f53435e24a
Jerry Bruwes <jbruwes@icloud.com>
2023-07-29 13:59:15 +0200

    Update main.js

--- monolit/src/main.js
+++ monolit/src/main.js
@@ -36,16 +36,21 @@ import "animate.css/animate.css";
 import "./styles/index.css";
 
 // Components
-import App from './App.vue'
+import App from "./App.vue";
 
 // Composables
-import { createApp } from 'vue'
+import { createApp } from "vue";
 
 // Plugins
-import { registerPlugins } from '@/plugins'
+import { registerPlugins } from "@/plugins";
 
-const app = createApp(App)
+document.addEventListener("click", (event) => {
+  const element = event.target;
+  const href = element.getAttribute("href");
+  if (element.tagName.toLowerCase() === "a" && href && href.indexOf("#") === 0)
+    element.href = window.location.href.split("#")[0] + href;
+});
 
-registerPlugins(app)
-
-app.mount('#app')
+const app = createApp(App);
+registerPlugins(app);
+app.mount("#app");
