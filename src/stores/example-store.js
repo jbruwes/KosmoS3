c86775271fdb29cb7f5192a59b15c7e7e7a9e515
Jerry Bruwes <jbruwes@gmail.com>
2023-08-26 14:36:55 +0200

    lint --fix

--- src/stores/example-store.js
+++ src/stores/example-store.js
@@ -1,13 +1,23 @@
-import { defineStore } from 'pinia';
+import { defineStore } from "pinia";
 
-export const useCounterStore = defineStore('counter', {
+export const useCounterStore = defineStore("counter", {
+  /**
+   *
+   */
   state: () => ({
     counter: 0,
   }),
   getters: {
+    /**
+     *
+     * @param state
+     */
     doubleCount: (state) => state.counter * 2,
   },
   actions: {
+    /**
+     *
+     */
     increment() {
       this.counter++;
     },
