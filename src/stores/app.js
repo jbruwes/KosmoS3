632ff70415160924dbf152dfd4d4fbe76e8e7a96
Jerry Bruwes <jbruwes@gmail.com>
2023-09-01 09:20:19 +0200

    stores

---
+++ src/stores/app.js
@@ -0,0 +1,424 @@
+import { ref, computed } from "vue";
+import {
+  get,
+  set,
+  watchDebounced,
+  useFetch,
+  whenever,
+  isDefined,
+} from "@vueuse/core";
+import { logicAnd, logicNot } from "@vueuse/math";
+import { defineStore } from "pinia";
+import {
+  HeadObjectCommand,
+  PutObjectCommand,
+  GetObjectCommand,
+} from "@aws-sdk/client-s3";
+import Konva from "konva";
+import DOMPurify from "dompurify";
+
+export default defineStore("app", () => {
+  /**
+   * текст сообщения об ошибке
+   * @type {string}
+   */
+  const message = ref("");
+  /**
+   * переключатель видимости сообщения об ошибке
+   * @type {boolean}
+   */
+  const snackbar = ref(false);
+  /**
+   * переключатель видимости правой панели
+   * @type {boolean}
+   */
+  const panel = ref();
+  /**
+   * корзина в сервисе s3
+   * @type {string}
+   */
+  const bucket = ref("");
+  /**
+   * путь к сайту через сервис s3
+   * @type {string}
+   */
+  const wendpoint = ref("");
+  /**
+   * клиент к сервису s3
+   * @type {object}
+   */
+  const s3 = ref();
+  /**
+   * индекс сайта
+   * @type {object}
+   */
+  const index = ref();
+  /**
+   * считывание заголовка файла
+   * @param {string} Key имя файла
+   * @returns {object} заголовок файла
+   */
+  const headObject = async (Key) => {
+    const Bucket = get(bucket);
+    let head;
+    if (isDefined(s3))
+      head = await get(s3).send(new HeadObjectCommand({ Bucket, Key }));
+    return head;
+  };
+  /**
+   * Запись объекта
+   * @param {string} Key имя файла
+   * @param {string} ContentType тип mime
+   * @param {string | Uint8Array | Buffer} body тело файла
+   */
+  const putObject = async (Key, ContentType, body) => {
+    const Bucket = get(bucket);
+    const Body =
+      typeof body === "string" ? new TextEncoder().encode(body) : body;
+    if (isDefined(s3))
+      await get(s3).send(
+        new PutObjectCommand({ Bucket, Key, ContentType, Body }),
+      );
+  };
+  /**
+   * Запись файла
+   * @param {string} Key имя файла
+   * @param {string} ContentType тип mime
+   * @param {File} file файл
+   */
+  const putFile = async (Key, ContentType, file) => {
+    await putObject(
+      Key,
+      ContentType,
+      await new Promise((resolve) => {
+        const reader = new FileReader();
+        /** @returns {Buffer} загруженный файл */
+        reader.onloadend = () => resolve(reader.result);
+        reader.readAsArrayBuffer(file);
+      }),
+    );
+  };
+  /**
+   * Считывание объекта
+   * @param {string} Key Имя файла
+   * @returns {string} Тело файла
+   */
+  const getObject = async (Key) => {
+    const Bucket = get(bucket);
+    const ResponseCacheControl = "no-store";
+    let ret;
+    if (isDefined(s3)) {
+      const { Body } = await get(s3).send(
+        new GetObjectCommand({ ResponseCacheControl, Bucket, Key }),
+      );
+      ret = await new Promise((resolve) => {
+        const reader = Body.getReader();
+        const textDecoder = new TextDecoder();
+        (async function read(chunks) {
+          const { done, value } = await reader.read();
+          if (done) resolve(chunks.join(""));
+          else {
+            chunks.push(textDecoder.decode(value));
+            read(chunks);
+          }
+        })([]);
+      });
+    }
+    return ret;
+  };
+  const requiredFiles = ["index.html", "favicon.ico"];
+  const { data } = useFetch("monolit/manifest.json", {
+    /**
+     * Преводим в массив
+     * @param { object } ctx - возвращаемый объект
+     * @returns { object } - трансформируемый возвращаемый объект
+     */
+    afterFetch(ctx) {
+      ctx.data = [
+        ...requiredFiles,
+        ...Object.values(ctx.data).map(({ file }) => file),
+      ];
+      return ctx;
+    },
+  }).json();
+  const base = computed(
+    () =>
+      `${isDefined(wendpoint) ? get(wendpoint) : "htpps:/"}/${get(bucket)}/`,
+  );
+  const configDOMPurify = {
+    SAFE_FOR_TEMPLATES: true,
+    ADD_TAGS: ["iframe"],
+    ADD_ATTR: [
+      "target",
+      "allow",
+      "allowfullscreen",
+      "frameborder",
+      "scrolling",
+    ],
+    CUSTOM_ELEMENT_HANDLING: {
+      tagNameCheck: /^v-/,
+      attributeNameCheck: /\w+/,
+      allowCustomizedBuiltInElements: true,
+    },
+  };
+  /**
+   * проверка слоя
+   * @param {object} layer слой
+   * @param {string} layer.id уникальный идентификатор
+   * @param {string} layer.fill цвет заливки
+   * @param {string} layer.name имя
+   * @param {boolean} layer.visible видимость слоя
+   * @param {object} layer.params параметры
+   * @param {string} layer.params.position вид позиционирования
+   * @param {string} layer.params.responsive адаптивность
+   * @param {Array} layer.params.width параметры ширины
+   * @param {Array} layer.params.height параметры высоты
+   * @param {string} layer.params.value html
+   * @returns {object} слой
+   */
+  const calcLayer = ({
+    id = crypto.randomUUID(),
+    fill = Konva.Util.getRandomColor(),
+    name = "",
+    visible = true,
+    params: { position, responsive, width, height, value = "" } = {},
+  } = {}) => ({
+    opacity: 0.5,
+    draggable: true,
+    width: 1,
+    height: 1,
+    offsetX: 0.5,
+    offsetY: 0.5,
+    id,
+    fill,
+    name,
+    visible: !!visible,
+    params: {
+      edit: false,
+      position: +position || 0,
+      responsive: !!responsive,
+      width: Array.isArray(width)
+        ? width.map((element) => Math.round(element))
+        : [0, 100],
+      height: Array.isArray(height)
+        ? height.map((element) => Math.round(element))
+        : [0, 100],
+      value:
+        name === "content" ? "" : DOMPurify.sanitize(value, configDOMPurify),
+    },
+  });
+  /**
+   * проверка структуры сайта
+   * @param {object} index структура сайта
+   * @param {object} index.content контент
+   * @param {Array} index.template шаблон
+   * @param {Array} index.css ссылки на стили
+   * @param {string} index.style стили
+   * @param {Array} index.js ссылки на скрипты
+   * @param {string} index.script скрипт
+   * @param {object} index.settings настройки
+   * @returns {object} структура сайта
+   */
+  const calcIndex = ({
+    content: pContent = {},
+    template: pTemplate = [],
+    css: pCss = [],
+    style: pStyle = "",
+    js: pJs = [],
+    script: pScript = "",
+    settings: pSettings = {},
+  } = {}) => {
+    let content = { ...pContent };
+    let template = [...pTemplate];
+    let css = [...pCss];
+    let style = pStyle;
+    let js = [...pJs];
+    let script = pScript;
+    let settings = { ...pSettings };
+    template = Array.isArray(template) ? template.filter(Boolean) : [];
+    if (!template.find(({ name }) => name === "content"))
+      template.push({ name: "content" });
+    template = template.map((element) => calcLayer(element));
+    css = Array.isArray(css)
+      ? css
+          .map(({ id = crypto.randomUUID(), url = "" }) => ({ id, url }))
+          .filter(({ url }) => url)
+      : [];
+    if (!css.length) css.push({ id: crypto.randomUUID(), url: "" });
+    js = Array.isArray(js)
+      ? js
+          .map(({ id = crypto.randomUUID(), url = "" }) => ({ id, url }))
+          .filter(({ url }) => url)
+      : [];
+    if (!js.length) js.push({ id: crypto.randomUUID(), url: "" });
+    const {
+      id = crypto.randomUUID(),
+      value = get(bucket),
+      visible = true,
+    } = content;
+    content = {
+      ...content,
+      id,
+      value,
+      visible,
+    };
+    style = String(style) === style ? style : "";
+    script = String(script) === script ? script : "";
+    const { yandex, metrika, google, analytics } = settings;
+    settings = { yandex, metrika, google, analytics };
+    return { content, template, css, style, js, script, settings };
+  };
+  whenever(s3, async () => {
+    let json = {};
+    try {
+      json = JSON.parse(await getObject("data.json"));
+    } finally {
+      set(index, calcIndex(json));
+    }
+  });
+  const settings = computed({
+    /**
+     * чтение настроек
+     * @returns {object} настройки
+     */
+    get: () => get(index)?.settings,
+    /**
+     * запись настроек
+     * @param {object} value настройки
+     */
+    set(value) {
+      if (isDefined(index)) get(index).settings = value;
+    },
+  });
+  const script = computed({
+    /**
+     * чтение скрипта
+     * @returns {string} скрипт
+     */
+    get: () => get(index)?.script,
+    /**
+     * запись скрипта
+     * @param {string} value скрипт
+     */
+    set(value) {
+      if (isDefined(index)) get(index).script = value;
+    },
+  });
+  const style = computed({
+    /**
+     * чтение стилей
+     * @returns {string} стили
+     */
+    get: () => get(index)?.style,
+    /**
+     * запись стилей
+     * @param {string} value стили
+     */
+    set(value) {
+      if (isDefined(index)) get(index).style = value;
+    },
+  });
+  const js = computed({
+    /**
+     * чтение массива ссылок на скрипты
+     * @returns {Array} массив ссылок на скрипты
+     */
+    get: () => get(index)?.js,
+    /**
+     * запись массива ссылок на скрипты
+     * @param {Array} value массив ссылок на скрипты
+     */
+    set(value) {
+      if (isDefined(index)) get(index).js = value.filter(({ url }) => url);
+    },
+  });
+  const css = computed({
+    /**
+     * чтение массива ссылок на стили
+     * @returns {Array} массив ссылок на стили
+     */
+    get: () => get(index)?.css,
+    /**
+     * запись массива ссылок на стили
+     * @param {Array} value массив ссылок на стили
+     */
+    set(value) {
+      if (isDefined(index)) get(index).css = value.filter(({ url }) => url);
+    },
+  });
+  const template = computed({
+    /**
+     * чтение шаблона
+     * @returns {Array} шаблон
+     */
+    get: () => get(index)?.template,
+    /**
+     * запись шаблона
+     * @param {Array} value шаблон
+     */
+    set(value) {
+      if (isDefined(index)) get(index).template = value;
+    },
+  });
+  const content = computed({
+    /**
+     * чтение контента
+     * @returns {object} контент
+     */
+    get: () => get(index)?.content,
+    /**
+     * запись контента
+     * @param {object} value контент
+     */
+    set(value) {
+      if (isDefined(index)) get(index).content = value;
+    },
+  });
+  whenever(logicNot(s3), () => {
+    set(index, undefined);
+  });
+  whenever(logicAnd(s3, data), () => {
+    get(data).forEach((asset) => {
+      (async () => {
+        try {
+          const head = await headObject(asset);
+          if (requiredFiles.includes(asset))
+            throw new Error(head.ContentLength);
+        } catch (e) {
+          const byteLength = +e.message;
+          const { data: body } = await useFetch(`monolit/${asset}`).blob();
+          let lBody = null;
+          if (Number.isNaN(byteLength)) lBody = get(body);
+          else {
+            const blob = new Blob(
+              [(await get(body).text()).replace(/{{ domain }}/g, get(bucket))],
+              { type: get(body).type },
+            );
+            if (byteLength !== (await blob.arrayBuffer()).byteLength)
+              lBody = blob;
+          }
+          if (lBody) putObject(asset, lBody.type, lBody);
+        }
+      })();
+    });
+  });
+  watchDebounced(
+    index,
+    (value, oldValue) => {
+      if (value && oldValue)
+        putObject(
+          "data.json",
+          "application/json",
+          JSON.stringify(calcIndex(value)),
+        );
+    },
+    { deep: true, debounce: 1000, maxWait: 10000 },
+  );
+  return {
+    ...{ bucket, wendpoint, base },
+    ...{ panel, snackbar, message },
+    ...{ content, template, js, script, css, style, settings },
+    ...{ s3, putFile },
+    ...{ calcLayer },
+  };
+});
---
+++ src/stores/templateStore.js
@@ -0,0 +1,238 @@
+import { ref, computed } from "vue";
+import { get, useArrayFindIndex, useArrayFilter } from "@vueuse/core";
+import { defineStore, storeToRefs } from "pinia";
+import app from "./app";
+
+export default defineStore("templateStore", () => {
+  const store = app();
+  const { template } = storeToRefs(store);
+  const visibleTemplate = useArrayFilter(
+    template,
+    ({ params: { visible } = {} } = {}) => visible,
+  );
+  const visibleTemplateStatic = useArrayFilter(
+    visibleTemplate,
+    ({ params: { position } = {} } = {}) => !position,
+  );
+  const konvaWidth = ref();
+  const konvaHeight = ref();
+  const realResponsiveWidth = ref();
+  const scrollY = ref();
+  const itemId = ref();
+  const itemIndex = useArrayFindIndex(template, ({ id }) => id === get(itemId));
+
+  /** @constant {number} responsiveWidth рассчетная ширина адаптивного контейнера плюс ширина бордюра */
+  const responsiveWidth = computed(() => get(realResponsiveWidth) + 8);
+  const cntStatic = computed(() => get(visibleTemplateStatic).length || 1);
+  const fullHeight = computed(() => get(cntStatic) * get(konvaHeight));
+  /**
+   *
+   * @param {boolean} responsive адаптивность
+   * @returns {number} ширина контейнера
+   */
+  const containerWidth = (responsive) =>
+    get(responsive ? responsiveWidth : konvaWidth);
+  /**
+   *
+   * @param {number} position тип позиционирования
+   * @returns {number} высота контейнера
+   */
+  const containerHeight = (position) =>
+    get(position === 1 ? fullHeight : konvaHeight);
+  /**
+   *
+   * @param {boolean} responsive адаптивность
+   * @returns {number} адаптивный сдвиг по горизонтали
+   */
+  const calcOffsetX = (responsive) =>
+    (responsive > 0) * ((get(konvaWidth) - get(responsiveWidth)) / 2);
+  /**
+   *
+   * @param {number} pPosition тип позиционирования
+   * @param {number} pIndex порядковый номер в шаблоне
+   * @returns {number} адаптивный сдвиг по вертикали
+   */
+  const calcOffsetY = (pPosition, pIndex) =>
+    [
+      get(visibleTemplate).filter(
+        ({ params: { position } = {} }, index) => index < pIndex && !position,
+      ).length * get(konvaHeight),
+      0,
+      pIndex === undefined ? 0 : get(scrollY),
+    ][pPosition];
+  /**
+   * @param {number} value ширина px
+   * @param {boolean} responsive адаптивность
+   * @returns {number} ширина %
+   */
+  const calcXPct = (value, responsive) =>
+    (100 * (value - calcOffsetX(responsive))) / containerWidth(responsive);
+  /**
+   * @param {number} value высота px
+   * @param {number} position тип позиционирования
+   * @param {number} index порядковый номер в шаблоне
+   * @returns {number} высота %
+   */
+  const calcYPct = (value, position, index) =>
+    (100 * (value - calcOffsetY(position, index))) / containerHeight(position);
+  /**
+   * @param {number} value ширина %
+   * @param {boolean} responsive адаптивность
+   * @returns {number} ширина px
+   */
+  const calcXPx = (value, responsive) =>
+    calcOffsetX(responsive) + (value * containerWidth(responsive)) / 100;
+  /**
+   * @param {number} value высота %
+   * @param {number} position тип позиционирования
+   * @param {number} index порядковый номер в шаблоне
+   * @returns {number} высота px
+   */
+  const calcYPx = (value, position, index) =>
+    calcOffsetY(position, index) + (value * containerHeight(position)) / 100;
+
+  /**
+   *
+   * @param {object} value слой
+   * @returns {object} нормализованный слой
+   */
+  const normLayer = (value) => {
+    const layer = {
+      ...value,
+      params: {
+        ...value.params,
+        /** @returns {boolean} видимость x */
+        get visible() {
+          const { visible } = layer;
+          return visible;
+        },
+        /** @param {boolean} val видимость */
+        set visible(val) {
+          layer.visible = val;
+        },
+        /** @returns {number} сдвиг x */
+        get offsetX() {
+          const { width, offsetX, scaleX } = layer;
+          return (width - offsetX) * scaleX;
+        },
+        /** @returns {number} сдвиг y */
+        get offsetY() {
+          const { height, offsetY, scaleY } = layer;
+          return (height - offsetY) * scaleY;
+        },
+        /** @returns {number} позиция в массиве */
+        get index() {
+          const { id: layerId } = layer;
+          return get(visibleTemplate).findIndex(({ id }) => id === layerId);
+        },
+      },
+      /** @returns {number} отступ слева */
+      get x() {
+        const {
+          params: { width, responsive, offsetX },
+        } = this;
+        return calcXPx(width[0], responsive) + offsetX;
+      },
+      /** @param {number} val отступ слева px */
+      set x(val) {
+        const {
+          params: { width, responsive },
+        } = this;
+        width[0] = calcXPct(val, responsive);
+      },
+      /** @returns {number} отступ сверху */
+      get y() {
+        const {
+          params: { height, position, index, offsetY },
+        } = this;
+        return calcYPx(height[0], position, index) + offsetY;
+      },
+      /** @param {number} val отступ сверху px */
+      set y(val) {
+        const {
+          params: { height, position, index },
+        } = this;
+        height[0] = calcYPct(val, position, index);
+      },
+
+      /** @returns {number} масштаб по х */
+      get scaleX() {
+        const {
+          params: { width, responsive },
+        } = this;
+        return calcXPx(width[1] - width[0], -responsive);
+      },
+      /** @param {number} val масштаб по х */
+      set scaleX(val) {
+        const {
+          params: { width, responsive },
+        } = this;
+        width[1] = width[0] + calcXPct(val, -responsive);
+      },
+
+      /** @returns {number} масштаб по y */
+      get scaleY() {
+        const {
+          params: { height, position },
+        } = this;
+        return calcYPx(height[1] - height[0], position);
+      },
+      /** @param {number} val масштаб по y */
+      set scaleY(val) {
+        const {
+          params: { height, position },
+        } = this;
+        height[1] = height[0] + calcYPct(val, position);
+      },
+      /**
+       * проверка невыхода за габариты при изменении положения
+       * @param {object} pos новые координаты
+       * @param {number} pos.x новые координаты x
+       * @param {number} pos.y новые координаты y
+       * @returns {object} разрешенные координаты
+       */
+      dragBoundFunc({ x: posX, y: posY }) {
+        const {
+          attrs: {
+            scaleX,
+            scaleY,
+            params: { offsetX, offsetY, position, responsive, index },
+          },
+        } = this;
+        const top = calcYPct(posY - offsetY, position, index);
+        const bottom = top + calcYPct(scaleY, position);
+        const left = calcXPct(posX - offsetX, responsive);
+        const right = left + calcXPct(scaleX, -responsive);
+        let x = posX;
+        if (left.toFixed(2) < 0) x = offsetX + calcOffsetX(responsive);
+        if (right.toFixed(2) > 100)
+          x =
+            containerWidth(responsive) -
+            scaleX +
+            offsetX +
+            calcOffsetX(responsive);
+        let y = posY;
+        if (top.toFixed(2) < 0) y = offsetY + calcOffsetY(position, index);
+        if (bottom.toFixed(2) > 100)
+          y =
+            get(konvaHeight) - scaleY + offsetY + calcOffsetY(position, index);
+        return { x, y };
+      },
+    };
+    return layer;
+  };
+
+  return {
+    ...{ itemId, itemIndex },
+    ...{
+      konvaWidth,
+      konvaHeight,
+      realResponsiveWidth,
+      scrollY,
+      visibleTemplate,
+      cntStatic,
+      fullHeight,
+    },
+    ...{ calcXPct, calcYPct, normLayer },
+  };
+});
