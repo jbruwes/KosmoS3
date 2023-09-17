296c61c2ff205297be72d54923c712fd39ac2ca2
Jerry Bruwes <jbruwes@gmail.com>
2023-09-17 14:57:56 +0200

    HomePage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,46 +3,185 @@ q-page.column.justify-start
   .col.column.q-ma-md
     q-img.col.rounded-borders(src="~assets/bg.jpg")
       q-card.absolute-center
-        q-form
+        q-form(ref="form" v-model="valid")
           .row.q-col-gutter-x-md
             .col-12.col-md.min-w-300
               .text-overline WEBSITE
-              q-select.q-mb-md(dark=true standout label="saved credentials")
+              q-select(v-model="cred" dark filled clearable label="saved credentials" :options="creds" hint="")
                 template(#prepend)
                   q-icon(name="save")
-              q-input.q-mb-md(dark=true standout label="domain" error-message="item is required" placeholder="example.com")
+              q-input(v-model.trim="bucket" :rules="[(v) => !!v || 'Item is required']" dark filled label="domain" placeholder="example.com")
                 template(#prepend)
                   q-icon(name="language")
-              q-input.q-mb-md(dark=true standout label="access key id" error-message="item is required")
+              q-input(v-model.trim="accessKeyId" :rules="[(v) => !!v || 'Item is required']" dark filled label="access key id")
                 template(#prepend)
                   q-icon(name="key")
-              q-input.q-mb-md(dark=true standout label="secret access key" error-message="item is required" filled :type="visible ? 'password' : 'text'")
+              q-input(v-model.trim="secretAccessKey" :rules="[(v) => !!v || 'Item is required']" dark label="secret access key" filled :type="visible ? 'password' : 'text'")
                 template(#prepend)
                   q-icon(name="lock")
                 template(#append)
                   q-icon.cursor-pointer(:name="visible ? 'visibility_off' : 'visibility'" @click="visible = !visible")
             .col-12.col-md.min-w-300
               .text-overline CLOUD
-              q-select.q-mb-md(dark=true standout label="cloud provider")
+              q-select(v-model="provider" clearable :options="providers" dark filled label="cloud provider" hint="")
                 template(#prepend)
                   q-icon(name="cloud")
-              q-input.q-mb-md(dark=true standout label="region" error-message="item is required")
+              q-select(v-model.trim="region" use-input filled hide-selected fill-input input-debounce="0" :options="regions" label="region" :rules="[(v) => !!v || 'Item is required']" dark :disable="provider && provider.label === 'yandex'" @input-value="(val) => {region=val}")
                 template(#prepend)
                   q-icon(name="flag")
-              q-input.q-mb-md(dark=true standout label="endpoint url")
+              q-input(v-model.trim="endpoint" dark filled label="endpoint url" :disable="!!provider" hint="")
                 template(#prepend)
                   q-icon(name="link")
-              q-input.q-mb-md(dark=true standout label="website endpoint url")
+              q-input(v-model.trim="wendpoint" dark filled label="website endpoint url" :disable="!!provider" hint="")
                 template(#prepend)
                   q-icon(name="web")
-          q-toggle.q-mb-md(label="remember credentials")
+          q-toggle.q-mb-md(v-model="remember" label="remember credentials")
           div
-            q-btn.full-width(label="LogIn" type="submit" color="primary")
+            q-btn.full-width(label="LogIn" type="submit" color="primary" @click="login")
 </template>
 <script setup>
-import { ref } from "vue";
+import { HeadBucketCommand, S3Client } from "@aws-sdk/client-s3";
+import { get, set, useStorage } from "@vueuse/core";
+import { storeToRefs } from "pinia";
+import { computed, ref, watch } from "vue";
 
-const visible = ref(true);
+import app from "@/stores/app";
+
+const store = app();
+const { s3, bucket, wendpoint, snackbar, message } = storeToRefs(store);
+set(s3, null);
+set(bucket, "");
+set(wendpoint, "");
+const accessKeyId = ref("");
+const secretAccessKey = ref("");
+const region = ref("");
+const endpoint = ref("");
+const visible = ref(false);
+const remember = ref(true);
+const providers = ref([
+  {
+    label: "aws",
+    regions: [
+      "us-east-2",
+      "us-east-1",
+      "us-west-1",
+      "us-west-2",
+      "af-south-1",
+      "ap-east-1",
+      "ap-southeast-3",
+      "ap-south-1",
+      "ap-northeast-3",
+      "ap-northeast-2",
+      "ap-southeast-1",
+      "ap-southeast-2",
+      "ap-northeast-1",
+      "ca-central-1",
+      "cn-north-1",
+      "cn-northwest-1",
+      "eu-central-1",
+      "eu-west-1",
+      "eu-west-2",
+      "eu-south-1",
+      "eu-west-3",
+      "eu-north-1",
+      "me-south-1",
+      "sa-east-1",
+    ],
+    region: "us-east-1",
+    endpoint: "",
+    wendpoint: computed(() => `https://s3.${get(region)}.amazonaws.com`),
+  },
+  {
+    label: "yandex",
+    regions: ["ru-central1"],
+    region: "ru-central1",
+    endpoint: "https://storage.yandexcloud.net",
+    wendpoint: "https://website.yandexcloud.net",
+  },
+]);
+const provider = ref(null);
+const regions = ref([]);
+const valid = ref(true);
+const form = ref(null);
+const creds = useStorage("kosmos3", []);
+const cred = ref(null);
+watch(provider, (value) => {
+  if (value) {
+    set(regions, value.regions);
+    set(region, value.region);
+    set(endpoint, value.endpoint);
+  } else {
+    set(regions, []);
+    set(region, "");
+    set(endpoint, "");
+  }
+});
+watch(region, () => {
+  if (get(provider)) set(wendpoint, get(provider).wendpoint);
+  else set(wendpoint, "");
+});
+watch(cred, (value) => {
+  if (value) {
+    set(bucket, value.title);
+    set(accessKeyId, value.accessKeyId);
+    set(secretAccessKey, value.secretAccessKey);
+    set(
+      provider,
+      get(providers).find(({ title }) => title === value.provider),
+    );
+    set(region, value.region);
+    set(endpoint, value.endpoint);
+    set(wendpoint, value.wendpoint);
+  } else {
+    set(bucket, "");
+    set(accessKeyId, "");
+    set(secretAccessKey, "");
+    set(provider, null);
+    set(region, "");
+    set(endpoint, "");
+    set(wendpoint, "");
+  }
+});
+let s3Client = null;
+/**
+ *
+ */
+const login = async () => {
+  get(form).validate();
+  if (!s3Client && get(valid))
+    try {
+      s3Client = new S3Client({
+        region: get(region),
+        endpoint: get(endpoint),
+        credentials: {
+          accessKeyId: get(accessKeyId),
+          secretAccessKey: get(secretAccessKey),
+        },
+      });
+      const lCreds = get(creds).filter(({ title }) => title !== get(bucket));
+      if (get(remember))
+        lCreds.push({
+          title: get(bucket),
+          accessKeyId: get(accessKeyId),
+          secretAccessKey: get(secretAccessKey),
+          region: get(region),
+          endpoint: get(endpoint),
+          wendpoint: get(wendpoint),
+          provider: get(provider).title,
+        });
+      set(creds, lCreds);
+      await s3Client.send(
+        new HeadBucketCommand({
+          Bucket: get(bucket),
+        }),
+      );
+      set(s3, s3Client);
+    } catch (err) {
+      s3Client = null;
+      set(message, err.message);
+      set(snackbar, true);
+    }
+};
 </script>
 <style>
 .min-w-300 {
