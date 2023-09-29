d2b0ad878f68cb01d1c0ab3451ccb98cc0c71210
Jerry Bruwes <jbruwes@gmail.com>
2023-09-29 13:45:04 +0200

    ace

--- src/pages/ContentPage.vue
+++ src/pages/ContentPage.vue
@@ -1,12 +1,23 @@
 <template lang="pug">
 q-drawer(v-if="s3" v-model="rightDrawer" bordered side="right")
 q-page.column.full-height
-  q-editor.col.column.q-ma-md(v-model="qeditor" content-class="col")
+  q-tabs(v-model="tab" dense class="text-grey" active-color="primary" indicator-color="primary" align="justify" narrow-indicator)
+    q-tab(name="wysiwyg" label="wysiwyg")
+    q-tab(name="source" label="source")
+  q-separator
+  q-tab-panels.col.column(v-model="tab" animated)
+    q-tab-panel.col.column(name="wysiwyg")
+      q-editor.col.column.q-ma-md(v-model="qeditor" content-class="col")
+    q-tab-panel.col.column(name="source")
+      v-ace-editor.col(v-model="qeditor" lang="html" theme="chrome")
 </template>
 <script setup>
+import "ace-builds/esm-resolver";
+
 import { set } from "@vueuse/core";
 import { storeToRefs } from "pinia";
 import { ref } from "vue";
+import { VAceEditor } from "vue3-ace-editor";
 
 import app from "@/stores/app";
 
@@ -14,4 +25,5 @@ const store = app();
 const { s3, rightDrawer } = storeToRefs(store);
 set(rightDrawer, true);
 const qeditor = ref("");
+const tab = ref("wysiwyg");
 </script>
