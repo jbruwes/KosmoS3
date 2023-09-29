c7b7749685f479342d915582ecdfa235cc46c382
Jerry Bruwes <jbruwes@gmail.com>
2023-09-29 17:39:25 +0200

    tabs

--- src/pages/ContentPage.vue
+++ src/pages/ContentPage.vue
@@ -7,18 +7,19 @@ q-page.column.full-height
   q-separator
   q-tab-panels.col.column(v-model="tab" animated)
     q-tab-panel.col.column(name="wysiwyg")
-      q-editor.col.column.q-ma-md(v-model="qeditor" content-class="col")
+      q-editor.col.column(v-model="qeditor" content-class="col" flat)
     q-tab-panel.col.column(name="source")
-      v-ace-editor.col(v-model="qeditor" lang="html" theme="chrome")
+      v-ace-editor.col(v-model:value="source" lang="html")
 </template>
 <script setup>
+// eslint-disable-next-line simple-import-sort/imports
+import { VAceEditor } from "vue3-ace-editor";
 import "ace-builds/esm-resolver";
 
-import { set } from "@vueuse/core";
+import { set, get } from "@vueuse/core";
 import { storeToRefs } from "pinia";
-import { ref } from "vue";
-import { VAceEditor } from "vue3-ace-editor";
-
+import { computed, ref } from "vue";
+import { html_beautify as htmlBeautify } from "js-beautify";
 import app from "@/stores/app";
 
 const store = app();
@@ -26,4 +27,20 @@ const { s3, rightDrawer } = storeToRefs(store);
 set(rightDrawer, true);
 const qeditor = ref("");
 const tab = ref("wysiwyg");
+const source = computed({
+  /**
+   * Прихорашивание html
+   * @returns {string} - красивый исходный код
+   */
+  get() {
+    return htmlBeautify(get(qeditor));
+  },
+  /**
+   *
+   * @param {string} newValue - отредактрованное значение
+   */
+  set(newValue) {
+    set(qeditor, newValue);
+  },
+});
 </script>
