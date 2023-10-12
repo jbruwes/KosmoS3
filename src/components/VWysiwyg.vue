d68a177e07195b392f520f0c866a686cc36212a4
Jerry Bruwes <jbruwes@gmail.com>
2023-10-12 13:52:53 +0200

    Текущая ветка: dev Эта ветка соответствует «origin/dev».
    Изменения, которые будут включены в коммит:
    изменено:      package-lock.json
    изменено:      package.json
    изменено:      src/components/VWysiwyg.vue

--- src/components/VWysiwyg.vue
+++ src/components/VWysiwyg.vue
@@ -1,8 +1,34 @@
 <template lang="pug">
-q-editor( ref="editorRef" :dense="$q.screen.lt.md" :model-value="modelValue" :toolbar="editorTlb" :fonts="editorFnt" content-class="col" flat placeholder="Добавьте контент на вашу страницу..." :definitions="editorDef" @update:model-value="$emit('update:modelValue', $event)" @paste="pasteCapture" @drop="dropCapture")
+q-editor( ref="editorRef" :dense="$q.screen.lt.md" :model-value="modelValue" :toolbar="editorTlb" :fonts="editorFnt" content-class="col" flat placeholder="Добавьте контент на вашу страницу..." :definitions="editorDef" @update:model-value="$emit('update:modelValue', $event)" @paste="capture" @drop="capture")
 </template>
 
 <script setup>
+import "@fontsource/arsenal";
+import "@fontsource/bad-script";
+import "@fontsource/caveat";
+import "@fontsource/comfortaa";
+import "@fontsource/cormorant-garamond";
+import "@fontsource/cormorant-infant";
+import "@fontsource/cormorant-sc";
+import "@fontsource/cormorant-unicase";
+import "@fontsource/cormorant";
+import "@fontsource/jura";
+import "@fontsource/marck-script";
+import "@fontsource/montserrat";
+import "@fontsource/montserrat-alternates";
+import "@fontsource/open-sans-condensed";
+import "@fontsource/open-sans";
+import "@fontsource/oswald";
+import "@fontsource/pattaya";
+import "@fontsource/poiret-one";
+import "@fontsource/roboto-condensed";
+import "@fontsource/roboto-mono";
+import "@fontsource/roboto-slab";
+import "@fontsource/roboto";
+import "@fontsource/rubik-mono-one";
+import "@fontsource/rubik";
+import "@fontsource/tenor-sans";
+
 import { get, useFileDialog } from "@vueuse/core";
 import * as mime from "mime-types";
 import { useQuasar } from "quasar";
@@ -16,7 +42,9 @@ const $q = useQuasar();
 const store = app();
 const { putFile, base } = store;
 const editorRef = ref();
-
+/**
+ * { @link https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types#image_types }
+ */
 const mTypes = [
   "image/apng",
   "image/avif",
@@ -26,51 +54,53 @@ const mTypes = [
   "image/svg+xml",
   "image/webp",
 ];
-
-const { files, open } = useFileDialog({
-  multiple: false,
-  accept: "image/*",
-});
 /**
- *
- * @param item
+ * @param { object } file - файл
  */
 const putImage = async (file) => {
   try {
     const { type } = file;
     if (mTypes.includes(type)) {
-      const filePath = `image/${crypto.randomUUID()}.${mime.extension(type)}`;
+      const filePath = `images/${crypto.randomUUID()}.${mime.extension(type)}`;
       await putFile(filePath, type, file);
       get(editorRef).runCmd("insertImage", `${base}${filePath}`);
     } else
       throw new Error(
-        "Тип файла не подходит для использования в сети интернет",
+        "Тип графического файла не подходит для использования в сети интернет",
       );
   } catch (err) {
     const { message } = err;
     $q.notify({ message });
   }
 };
-watch(files, (newFiles) => {
-  if (newFiles.length) {
-    const [file] = newFiles;
-    putImage(file);
-  }
-});
 /**
- *
+ * @param {object} evt - объект события
  */
+const capture = (evt) => {
+  const { files = [] } =
+    evt?.dataTransfer ||
+    evt?.originalEvent?.clipboardData ||
+    evt?.clipboardData ||
+    window?.clipboardData ||
+    {};
+  const lFiles = [...files];
+  if (lFiles.length) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    lFiles.forEach(putImage);
+  }
+};
+const { files, open } = useFileDialog({ accept: "image/*" });
+watch(files, (newFiles) => {
+  [...newFiles].forEach(putImage);
+});
 const editorDef = reactive({
   upload: {
     tip: "Загрузка картинки",
     icon: "cloud_upload",
-    /** */
-    handler() {
-      open();
-    },
+    handler: open,
   },
 });
-
 const editorTlb = reactive([
   ["left", "center", "right", "justify"],
   ["bold", "italic", "strike", "underline", "subscript", "superscript"],
@@ -114,6 +144,32 @@ const editorTlb = reactive([
         "lucida_grande",
         "times_new_roman",
         "verdana",
+
+        "arsenal",
+        "bad_script",
+        "caveat",
+        "comfortaa",
+        "cormorant_garamond",
+        "cormorant_infant",
+        "cormorant_sc",
+        "cormorant_unicase",
+        "cormorant",
+        "jura",
+        "marck_script",
+        "montserrat",
+        "montserrat_alternates",
+        "open_sans_condensed",
+        "open_sans",
+        "oswald",
+        "pattaya",
+        "poiret_one",
+        "roboto_condensed",
+        "roboto_mono",
+        "roboto_slab",
+        "roboto",
+        "rubik_mono_one",
+        "rubik",
+        "tenor_sans",
       ],
     },
     "removeFormat",
@@ -132,34 +188,31 @@ const editorFnt = reactive({
   lucida_grande: "Lucida Grande",
   times_new_roman: "Times New Roman",
   verdana: "Verdana",
+
+  arsenal: "Arsenal",
+  bad_script: "Bad Script",
+  caveat: "Caveat",
+  comfortaa: "Comfortaa",
+  cormorant_garamond: "Cormorant Garamond",
+  cormorant_infant: "Cormorant Infant",
+  cormorant_sc: "Cormorant SC",
+  cormorant_unicase: "Cormorant Unicase",
+  cormorant: "Cormorant",
+  jura: "Jura",
+  marck_script: "Marck Script",
+  montserrat: "Montserrat",
+  montserrat_alternates: "Montserrat Alternates",
+  open_sans_condensed: "Open Sans Condensed",
+  open_sans: "Open Sans",
+  oswald: "Oswald",
+  pattaya: "Pattaya",
+  poiret_one: "Poiret One",
+  roboto_condensed: "Roboto Condensed",
+  roboto_mono: "Roboto Mono",
+  roboto_slab: "Roboto Slab",
+  roboto: "Roboto",
+  rubik_mono_one: "Rubik Mono One",
+  rubik: "Rubik",
+  tenor_sans: "Tenor Sans",
 });
-
-/**
- * { @link https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types#image_types }
- * @param {object} evt - объект события
- */
-const pasteCapture = (evt) => {
-  const clipboardData =
-    evt?.originalEvent?.clipboardData ||
-    evt?.clipboardData ||
-    window?.clipboardData;
-
-  if (!clipboardData.getData("text")) {
-    evt.preventDefault();
-    evt.stopPropagation();
-    const { items = [] } = clipboardData;
-    [...items].forEach((item) => {
-      putImage(item.getAsFile());
-    });
-  }
-};
-/**
- *
- * @param evt
- */
-const dropCapture = (evt) => {
-  evt.preventDefault();
-  evt.stopPropagation();
-  console.log("dropCapture", evt);
-};
 </script>
