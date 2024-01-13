e5cf275d744fb244d8caa03419d49393c1797a68
Jerry Bruwes <jbruwes@gmail.com>
2024-01-13 17:04:40 +0200

    изменено:      jsconfig.json 	изменено:      monolit/vite.config.js

--- monolit/vite.config.js
+++ monolit/vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
-      vue: "vue/dist/vue.esm-bundler.js",
     },
     extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
   },
