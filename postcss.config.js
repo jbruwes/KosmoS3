666da7ae29960d26ef7069f8502fcd0559769fc5
Jerry Bruwes <jbruwes@gmail.com>
2024-02-08 18:58:55 +0200

    изменено:      package-lock.json 	изменено:      package.json 	изменено:      postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
-/* eslint-disable */
-// https://github.com/michael-ciniawsky/postcss-load-config
 import autoprefixer from "autoprefixer";
+
 export default {
   plugins: [
-    // https://github.com/postcss/autoprefixer
     autoprefixer({
       overrideBrowserslist: [
         "last 4 Chrome versions",
@@ -16,12 +14,5 @@ export default {
         "last 4 iOS versions",
       ],
     }),
-
-    // https://github.com/elchininet/postcss-rtlcss
-    // If you want to support RTL css, then
-    // 1. yarn/npm install postcss-rtlcss
-    // 2. optionally set quasar.config.js > framework > lang to an RTL language
-    // 3. uncomment the following line:
-    // require('postcss-rtlcss')
   ],
 };
