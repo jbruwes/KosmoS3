e0698f78011db90677bffc339830ab601befb13f
Jerry Bruwes <jbruwes@gmail.com>
2024-02-17 18:11:53 +0200

    изменено:      package-lock.json 	изменено:      package.json 	изменено:      uno.config.js

--- uno.config.js
+++ uno.config.js
@@ -1,6 +1,16 @@
+import presetAttributify from "@unocss/preset-attributify";
+import presetTagify from "@unocss/preset-tagify";
 import presetTypography from "@unocss/preset-typography";
 import presetUno from "@unocss/preset-uno";
+import transformerDirectives from "@unocss/transformer-directives";
+import transformerVariantGroup from "@unocss/transformer-variant-group";
 
 export default {
-  presets: [presetUno(), presetTypography()],
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetTypography(),
+    presetTagify(),
+  ],
+  transformers: [transformerDirectives(), transformerVariantGroup()],
 };
