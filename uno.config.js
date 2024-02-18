27b638a495baa9b3fe82d652ae17a3493056a531
Jerry Bruwes <jbruwes@gmail.com>
2024-02-18 15:03:31 +0200

    изменено:      package-lock.json 	изменено:      package.json 	изменено:      uno.config.js

--- uno.config.js
+++ uno.config.js
@@ -2,6 +2,7 @@ import presetAttributify from "@unocss/preset-attributify";
 import presetTagify from "@unocss/preset-tagify";
 import presetTypography from "@unocss/preset-typography";
 import presetUno from "@unocss/preset-uno";
+// import presetWebFonts from "@unocss/preset-web-fonts";
 import transformerDirectives from "@unocss/transformer-directives";
 import transformerVariantGroup from "@unocss/transformer-variant-group";
 
@@ -11,6 +12,7 @@ export default {
     presetAttributify(),
     presetTypography(),
     presetTagify(),
+    // presetWebFonts({ fonts }),
   ],
   transformers: [transformerDirectives(), transformerVariantGroup()],
 };
