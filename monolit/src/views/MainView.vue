a76efa7ac25e3954b5899375d603c92352575c54
Jerry Bruwes <jbruwes@gmail.com>
2024-03-09 22:47:36 +0200

    изменено:      monolit/src/views/MainView.vue 	изменено:      package-lock.json 	изменено:      package.json

--- monolit/src/views/MainView.vue
+++ monolit/src/views/MainView.vue
@@ -178,7 +178,7 @@ const cmpTemplates = computed(fncTemplates);
  * @type {string}
  * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/IntersectionObserver/rootMargin} см. документацию
  */
-const rootMargin = "-1px 0px -100%";
+const rootMargin = "-0.1% 0px -99.9%";
 
 /**
  * Процент площади объекта, который должен попасть в область видимости
